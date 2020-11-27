481c81e7f9c01833d2cc26f40d0f00e916db2c2e
Jan Keim <jan.keim@kit.edu>
2020-11-27 17:30:38 +0100

    further refactoring and killing code smells

--- src/main/java/modelconnector/recommendationGenerator/analyzers/RecommendationAnalyzer.java
+++ src/main/java/modelconnector/recommendationGenerator/analyzers/RecommendationAnalyzer.java
@@ -17,34 +17,40 @@ import modelconnector.textExtractor.state.TextExtractionState;
  */
 public abstract class RecommendationAnalyzer extends Analyzer {
 
-	protected IGraph graph;
-	protected IArcType depArcType;
-	protected IArcType relArcType;
-	protected TextExtractionState textExtractionState;
-	protected ModelExtractionState modelExtractionState;
-	protected RecommendationState recommendationState;
+    protected IGraph graph;
+    protected IArcType depArcType;
+    protected IArcType relArcType;
+    protected TextExtractionState textExtractionState;
+    protected ModelExtractionState modelExtractionState;
+    protected RecommendationState recommendationState;
 
-	/**
-	 * Creates a new analyzer.
-	 *
-	 * @param dependencyType       the dependencies of the analyzer
-	 * @param graph                the PARSE graph to look up
-	 * @param textExtractionState  the text extraction state to look up
-	 * @param modelExtractionState the model extraction state to look up
-	 * @param recommendationState  the model extraction state to work with
-	 */
-	public RecommendationAnalyzer(//
-			DependencyType dependencyType, IGraph graph, TextExtractionState textExtractionState, ModelExtractionState modelExtractionState, RecommendationState recommendationState) {
-		super(dependencyType);
-		this.graph = graph;
-		depArcType = graph.getArcType("typedDependency");
-		relArcType = graph.getArcType("relation");
-		this.textExtractionState = textExtractionState;
-		this.modelExtractionState = modelExtractionState;
-		this.recommendationState = recommendationState;
-	}
+    /**
+     * Creates a new analyzer.
+     *
+     * @param dependencyType
+     *            the dependencies of the analyzer
+     * @param graph
+     *            the PARSE graph to look up
+     * @param textExtractionState
+     *            the text extraction state to look up
+     * @param modelExtractionState
+     *            the model extraction state to look up
+     * @param recommendationState
+     *            the model extraction state to work with
+     */
+    protected RecommendationAnalyzer(//
+            DependencyType dependencyType, IGraph graph, TextExtractionState textExtractionState,
+            ModelExtractionState modelExtractionState, RecommendationState recommendationState) {
+        super(dependencyType);
+        this.graph = graph;
+        depArcType = graph.getArcType("typedDependency");
+        relArcType = graph.getArcType("relation");
+        this.textExtractionState = textExtractionState;
+        this.modelExtractionState = modelExtractionState;
+        this.recommendationState = recommendationState;
+    }
 
-	@Override
-	public abstract void exec(INode node);
+    @Override
+    public abstract void exec(INode node);
 
 }
--- src/main/java/modelconnector/textExtractor/analyzers/ArticleTypeNameAnalyzer.java
+++ src/main/java/modelconnector/textExtractor/analyzers/ArticleTypeNameAnalyzer.java
@@ -15,73 +15,70 @@ import modelconnector.textExtractor.state.TextExtractionState;
  */
 public class ArticleTypeNameAnalyzer extends TextExtractionAnalyzer {
 
-	private double probability = ModelConnectorConfiguration.ARTICLE_TYPE_NAME_ANALYZER_PROBABILITY;
+    private double probability = ModelConnectorConfiguration.ARTICLE_TYPE_NAME_ANALYZER_PROBABILITY;
 
-	/**
-	 * Creates a new article type name analyzer.
-	 *
-	 * @param graph
-	 * @param textExtractionState the text extraction state
-	 */
-	public ArticleTypeNameAnalyzer(IGraph graph, TextExtractionState textExtractionState) {
-		super(DependencyType.TEXT, graph, textExtractionState);
-	}
+    /**
+     * Creates a new article type name analyzer.
+     *
+     * @param graph
+     * @param textExtractionState
+     *            the text extraction state
+     */
+    public ArticleTypeNameAnalyzer(IGraph graph, TextExtractionState textExtractionState) {
+        super(DependencyType.TEXT, graph, textExtractionState);
+    }
 
-	@Override
-	public void exec(INode n) {
+    @Override
+    public void exec(INode n) {
 
-		if (checkIfNodeIsName(n)) {
-			return;
-		} else {
-			checkIfNodeIsType(n);
-		}
-	}
+        if (!checkIfNodeIsName(n)) {
+            checkIfNodeIsType(n);
+        }
+    }
 
-	/**
-	 * If the current node is contained by name-or-type mappings, the previous node
-	 * is contained by type nodes and the preprevious an article the node is added
-	 * as a name mapping.
-	 *
-	 * @param n node to check
-	 */
-	private boolean checkIfNodeIsName(INode n) {
-		if (textExtractionState.isNodeContainedByNameOrTypeNodes(n)) {
+    /**
+     * If the current node is contained by name-or-type mappings, the previous node is contained by type nodes and the
+     * preprevious an article the node is added as a name mapping.
+     *
+     * @param n
+     *            node to check
+     */
+    private boolean checkIfNodeIsName(INode n) {
+        if (textExtractionState.isNodeContainedByNameOrTypeNodes(n)) {
 
-			INode prevNode = GraphUtils.getPreviousNode(n, relArcType);
-			if (prevNode != null && textExtractionState.isNodeContainedByTypeNodes(prevNode)) {
+            INode prevNode = GraphUtils.getPreviousNode(n, relArcType);
+            if (prevNode != null && textExtractionState.isNodeContainedByTypeNodes(prevNode)
+                    && GraphUtils.checkIfPrevNodeIsDt(prevNode, relArcType)) {
 
-				if (GraphUtils.checkIfPrevNodeIsDt(prevNode, relArcType)) {
-					textExtractionState.addName(n, GraphUtils.getNodeValue(n), probability);
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+                textExtractionState.addName(n, GraphUtils.getNodeValue(n), probability);
+                return true;
 
-	/**
-	 * If the current node is contained by name-or-type mappings, the previous node
-	 * is contained by name nodes and the preprevious an article the node is added
-	 * as a type mapping.
-	 *
-	 * @param n node to check
-	 */
-	private boolean checkIfNodeIsType(INode n) {
-		if (textExtractionState.isNodeContainedByNameOrTypeNodes(n)) {
+            }
+        }
+        return false;
+    }
 
-			INode prevNode = GraphUtils.getPreviousNode(n, relArcType);
-			if (prevNode != null && textExtractionState.isNodeContainedByNameNodes(prevNode)) {
+    /**
+     * If the current node is contained by name-or-type mappings, the previous node is contained by name nodes and the
+     * preprevious an article the node is added as a type mapping.
+     *
+     * @param n
+     *            node to check
+     */
+    private boolean checkIfNodeIsType(INode n) {
+        if (textExtractionState.isNodeContainedByNameOrTypeNodes(n)) {
 
-				if (GraphUtils.checkIfPrevNodeIsDt(prevNode, relArcType)) {
-					textExtractionState.addType(n, GraphUtils.getNodeValue(n), probability);
-					return true;
-				}
+            INode prevNode = GraphUtils.getPreviousNode(n, relArcType);
+            if (prevNode != null && textExtractionState.isNodeContainedByNameNodes(prevNode)
+                    && GraphUtils.checkIfPrevNodeIsDt(prevNode, relArcType)) {
 
-			}
+                textExtractionState.addType(n, GraphUtils.getNodeValue(n), probability);
+                return true;
+            }
 
-		}
-		return false;
+        }
+        return false;
 
-	}
+    }
 
 }
