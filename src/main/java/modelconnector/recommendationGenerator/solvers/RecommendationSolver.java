ab7211e48004ab704bf45a895330beec7d59362d
Jan Keim <jan.keim@kit.edu>
2020-11-27 17:30:38 +0100

    further refactoring and killing code smells

--- src/main/java/modelconnector/recommendationGenerator/solvers/RecommendationSolver.java
+++ src/main/java/modelconnector/recommendationGenerator/solvers/RecommendationSolver.java
@@ -16,34 +16,39 @@ import modelconnector.textExtractor.state.TextExtractionState;
  */
 public abstract class RecommendationSolver extends Solver {
 
-	protected IGraph graph;
-	protected IArcType depArcType;
-	protected IArcType relArcType;
-	protected TextExtractionState textExtractionState;
-	protected ModelExtractionState modelExtractionState;
-	protected RecommendationState recommendationState;
+    protected IGraph graph;
+    protected IArcType depArcType;
+    protected IArcType relArcType;
+    protected TextExtractionState textExtractionState;
+    protected ModelExtractionState modelExtractionState;
+    protected RecommendationState recommendationState;
 
-	/**
-	 * Creates a new solver.
-	 *
-	 * @param dependencyType       the dependencies of the analyzer
-	 * @param graph                the PARSE graph to look up
-	 * @param textExtractionState  the text extraction state to look up
-	 * @param modelExtractionState the model extraction state to look up
-	 * @param recommendationState  the model extraction state to work with
-	 */
-	public RecommendationSolver(//
-			DependencyType dependencyType, IGraph graph, TextExtractionState textExtractionState, //
-			ModelExtractionState modelExtractionState, RecommendationState recommendationState) {
-		super(dependencyType);
-		this.graph = graph;
-		depArcType = graph.getArcType("typedDependency");
-		relArcType = graph.getArcType("relation");
-		this.textExtractionState = textExtractionState;
-		this.modelExtractionState = modelExtractionState;
-		this.recommendationState = recommendationState;
-	}
+    /**
+     * Creates a new solver.
+     *
+     * @param dependencyType
+     *            the dependencies of the analyzer
+     * @param graph
+     *            the PARSE graph to look up
+     * @param textExtractionState
+     *            the text extraction state to look up
+     * @param modelExtractionState
+     *            the model extraction state to look up
+     * @param recommendationState
+     *            the model extraction state to work with
+     */
+    protected RecommendationSolver(//
+            DependencyType dependencyType, IGraph graph, TextExtractionState textExtractionState, //
+            ModelExtractionState modelExtractionState, RecommendationState recommendationState) {
+        super(dependencyType);
+        this.graph = graph;
+        depArcType = graph.getArcType("typedDependency");
+        relArcType = graph.getArcType("relation");
+        this.textExtractionState = textExtractionState;
+        this.modelExtractionState = modelExtractionState;
+        this.recommendationState = recommendationState;
+    }
 
-	@Override
-	public abstract void exec();
+    @Override
+    public abstract void exec();
 }
--- src/main/java/modelconnector/textExtractor/solvers/TextExtractionSolver.java
+++ src/main/java/modelconnector/textExtractor/solvers/TextExtractionSolver.java
@@ -14,26 +14,30 @@ import modelconnector.textExtractor.state.TextExtractionState;
  */
 public abstract class TextExtractionSolver extends Solver {
 
-	protected IGraph graph;
-	protected IArcType depArcType;
-	protected IArcType relArcType;
-	protected TextExtractionState textExtractionState;
+    protected IGraph graph;
+    protected IArcType depArcType;
+    protected IArcType relArcType;
+    protected TextExtractionState textExtractionState;
 
-	/**
-	 * Creates a new solver.
-	 *
-	 * @param dependencyType      the dependencies of the analyzer
-	 * @param graph               the PARSE graph to look up
-	 * @param textExtractionState the text extraction state to work with
-	 */
-	public TextExtractionSolver(DependencyType dependencyType, IGraph graph, TextExtractionState textExtractionState) {
-		super(dependencyType);
-		this.graph = graph;
-		depArcType = graph.getArcType("typedDependency");
-		relArcType = graph.getArcType("relation");
-		this.textExtractionState = textExtractionState;
-	}
+    /**
+     * Creates a new solver.
+     *
+     * @param dependencyType
+     *            the dependencies of the analyzer
+     * @param graph
+     *            the PARSE graph to look up
+     * @param textExtractionState
+     *            the text extraction state to work with
+     */
+    protected TextExtractionSolver(DependencyType dependencyType, IGraph graph,
+            TextExtractionState textExtractionState) {
+        super(dependencyType);
+        this.graph = graph;
+        depArcType = graph.getArcType("typedDependency");
+        relArcType = graph.getArcType("relation");
+        this.textExtractionState = textExtractionState;
+    }
 
-	@Override
-	public abstract void exec();
+    @Override
+    public abstract void exec();
 }
