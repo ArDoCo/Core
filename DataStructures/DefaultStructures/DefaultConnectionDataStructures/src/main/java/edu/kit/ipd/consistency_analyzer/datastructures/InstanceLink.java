1c3c8a5c0916c8b0bff817ae2b55bbb3181bf2f0
Dominik Fuchß <dominik.fuchss@kit.edu>
2021-01-27 13:57:38 +0100

    Removed unused class and fixed "equals" mudholes :)

--- DataStructures/DefaultStructures/DefaultConnectionDataStructures/src/main/java/edu/kit/ipd/consistency_analyzer/datastructures/InstanceLink.java
+++ DataStructures/DefaultStructures/DefaultConnectionDataStructures/src/main/java/edu/kit/ipd/consistency_analyzer/datastructures/InstanceLink.java
@@ -4,154 +4,140 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 /**
- * Represents a trace link between an instance of the extracted model and a
- * recommended instance.
+ * Represents a trace link between an instance of the extracted model and a recommended instance.
  *
  * @author Sophie
  *
  */
 public class InstanceLink implements IInstanceLink {
 
-	private IRecommendedInstance textualInstance;
-	private IInstance modelInstance;
-	private double probability;
+    private IRecommendedInstance textualInstance;
+    private IInstance modelInstance;
+    private double probability;
 
-	@Override
-	public IInstanceLink createCopy() {
-		return new InstanceLink(textualInstance.createCopy(), modelInstance.createCopy(), probability);
-	}
+    @Override
+    public IInstanceLink createCopy() {
+        return new InstanceLink(textualInstance.createCopy(), modelInstance.createCopy(), probability);
+    }
 
-	/**
-	 * Creates a new instance link.
-	 *
-	 * @param textualInstance the recommended instance
-	 * @param modelInstance   the extracted instance
-	 * @param probability     the probability of this link
-	 */
-	public InstanceLink(IRecommendedInstance textualInstance, IInstance modelInstance, double probability) {
-		this.textualInstance = textualInstance;
-		this.modelInstance = modelInstance;
-		this.probability = probability;
-	}
+    /**
+     * Creates a new instance link.
+     *
+     * @param textualInstance
+     *            the recommended instance
+     * @param modelInstance
+     *            the extracted instance
+     * @param probability
+     *            the probability of this link
+     */
+    public InstanceLink(IRecommendedInstance textualInstance, IInstance modelInstance, double probability) {
+        this.textualInstance = textualInstance;
+        this.modelInstance = modelInstance;
+        this.probability = probability;
+    }
 
-	/**
-	 * Returns the probability of the correctness of this link.
-	 *
-	 * @return the probability of this link
-	 */
-	@Override
-	public double getProbability() {
-		return probability;
-	}
+    /**
+     * Returns the probability of the correctness of this link.
+     *
+     * @return the probability of this link
+     */
+    @Override
+    public double getProbability() {
+        return probability;
+    }
 
-	/**
-	 * Sets the probability to the given probability.
-	 *
-	 * @param probability the new probability
-	 */
-	@Override
-	public void setProbability(double probability) {
-		this.probability = probability;
-	}
+    /**
+     * Sets the probability to the given probability.
+     *
+     * @param probability
+     *            the new probability
+     */
+    @Override
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
 
-	/**
-	 * Returns the recommended instance.
-	 *
-	 * @return the textual instance
-	 */
-	@Override
-	public IRecommendedInstance getTextualInstance() {
-		return textualInstance;
-	}
+    /**
+     * Returns the recommended instance.
+     *
+     * @return the textual instance
+     */
+    @Override
+    public IRecommendedInstance getTextualInstance() {
+        return textualInstance;
+    }
 
-	/**
-	 * Returns the model instance.
-	 *
-	 * @return the extracted instance
-	 */
-	@Override
-	public IInstance getModelInstance() {
-		return modelInstance;
-	}
+    /**
+     * Returns the model instance.
+     *
+     * @return the extracted instance
+     */
+    @Override
+    public IInstance getModelInstance() {
+        return modelInstance;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((modelInstance == null) ? 0 : modelInstance.hashCode());
-		result = prime * result + ((textualInstance == null) ? 0 : textualInstance.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(modelInstance, textualInstance);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		IInstanceLink other = (IInstanceLink) obj;
-		if (modelInstance == null) {
-			if (other.getModelInstance() != null) {
-				return false;
-			}
-		} else if (!modelInstance.equals(other.getModelInstance())) {
-			return false;
-		}
-		if (textualInstance == null) {
-			if (other.getTextualInstance() != null) {
-				return false;
-			}
-		} else if (!textualInstance.equals(other.getTextualInstance())) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        InstanceLink other = (InstanceLink) obj;
+        return Objects.equals(modelInstance, other.modelInstance)
+                && Objects.equals(textualInstance, other.textualInstance);
+    }
 
-	/**
-	 * Returns all occurrences of all recommended instance names as string.
-	 *
-	 * @return all names of the recommended instances
-	 */
-	@Override
-	public String getNameOccurencesAsString() {
-		Set<String> names = new HashSet<>();
-		List<Integer> namePositions = new ArrayList<>();
-		for (INounMapping nameMapping : textualInstance.getNameMappings()) {
-			names.addAll(nameMapping.getOccurrences());
-			namePositions.addAll(nameMapping.getMappingSentenceNo());
-		}
+    /**
+     * Returns all occurrences of all recommended instance names as string.
+     *
+     * @return all names of the recommended instances
+     */
+    @Override
+    public String getNameOccurencesAsString() {
+        Set<String> names = new HashSet<>();
+        List<Integer> namePositions = new ArrayList<>();
+        for (INounMapping nameMapping : textualInstance.getNameMappings()) {
+            names.addAll(nameMapping.getOccurrences());
+            namePositions.addAll(nameMapping.getMappingSentenceNo());
+        }
 
-		return "name=" + textualInstance.getName() + "occurrences= " + "NameVariants: " + names.size() + ": " + names.toString() + //
-				" sentences{" + Arrays.toString(namePositions.toArray()) + "}";
-	}
+        return "name=" + textualInstance.getName() + "occurrences= " + "NameVariants: " + names.size() + ": "
+                + names.toString() + //
+                " sentences{" + Arrays.toString(namePositions.toArray()) + "}";
+    }
 
-	@Override
-	public String toString() {
-		Set<String> names = new HashSet<>();
-		List<Integer> namePositions = new ArrayList<>();
-		Set<String> types = new HashSet<>();
-		List<Integer> typePositions = new ArrayList<>();
+    @Override
+    public String toString() {
+        Set<String> names = new HashSet<>();
+        List<Integer> namePositions = new ArrayList<>();
+        Set<String> types = new HashSet<>();
+        List<Integer> typePositions = new ArrayList<>();
 
-		for (INounMapping nameMapping : textualInstance.getNameMappings()) {
-			names.addAll(nameMapping.getOccurrences());
-			namePositions.addAll(nameMapping.getMappingSentenceNo());
-		}
-		for (INounMapping typeMapping : textualInstance.getTypeMappings()) {
-			types.addAll(typeMapping.getOccurrences());
-			typePositions.addAll(typeMapping.getMappingSentenceNo());
-		}
-		return "InstanceMapping [ uid=" + modelInstance.getUid() + ", name=" + modelInstance.getLongestName() + //
-				", as=" + String.join(", ", modelInstance.getLongestType()) + ", probability=" + probability + ", FOUND: " + //
-				textualInstance.getName() + " : " + getTextualInstance().getType() + ", occurrences= " + //
-				"NameVariants: " + names.size() + ": " + names.toString() + " sentences{" + Arrays.toString(namePositions.toArray()) + "}" + //
-				", TypeVariants: " + types.size() + ": " + types.toString() + "sentences{" + Arrays.toString(typePositions.toArray()) + "}" + "]";
-	}
+        for (INounMapping nameMapping : textualInstance.getNameMappings()) {
+            names.addAll(nameMapping.getOccurrences());
+            namePositions.addAll(nameMapping.getMappingSentenceNo());
+        }
+        for (INounMapping typeMapping : textualInstance.getTypeMappings()) {
+            types.addAll(typeMapping.getOccurrences());
+            typePositions.addAll(typeMapping.getMappingSentenceNo());
+        }
+        return "InstanceMapping [ uid=" + modelInstance.getUid() + ", name=" + modelInstance.getLongestName() + //
+                ", as=" + String.join(", ", modelInstance.getLongestType()) + ", probability=" + probability
+                + ", FOUND: " + //
+                textualInstance.getName() + " : " + getTextualInstance().getType() + ", occurrences= " + //
+                "NameVariants: " + names.size() + ": " + names.toString() + " sentences{"
+                + Arrays.toString(namePositions.toArray()) + "}" + //
+                ", TypeVariants: " + types.size() + ": " + types.toString() + "sentences{"
+                + Arrays.toString(typePositions.toArray()) + "}" + "]";
+    }
 }
--- DataStructures/DefaultStructures/DefaultRecommendationDataStructures/src/main/java/edu/kit/ipd/consistency_analyzer/datastructures/RecommendedInstance.java
+++ DataStructures/DefaultStructures/DefaultRecommendationDataStructures/src/main/java/edu/kit/ipd/consistency_analyzer/datastructures/RecommendedInstance.java
@@ -2,248 +2,243 @@ package edu.kit.ipd.consistency_analyzer.datastructures;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
- * This class represents recommended instances. These instances should be
- * contained by the model. The likelihood is measured by the probability. Every
- * recommended instance has a unique name.
+ * This class represents recommended instances. These instances should be contained by the model. The likelihood is
+ * measured by the probability. Every recommended instance has a unique name.
  *
  * @author Sophie
  *
  */
 public class RecommendedInstance implements IRecommendedInstance {
 
-	private String type;
-	private String name;
-	private double probability;
-	private List<INounMapping> typeMappings;
-	private List<INounMapping> nameMappings;
+    private String type;
+    private String name;
+    private double probability;
+    private List<INounMapping> typeMappings;
+    private List<INounMapping> nameMappings;
 
-	@Override
-	public IRecommendedInstance createCopy() {
-		return new RecommendedInstance(name, type, probability, nameMappings.stream().map(INounMapping::createCopy).collect(Collectors.toList()),
-				typeMappings.stream().map(INounMapping::createCopy).collect(Collectors.toList()));
-	}
+    @Override
+    public IRecommendedInstance createCopy() {
+        return new RecommendedInstance(name, type, probability, nameMappings.stream()
+                                                                            .map(INounMapping::createCopy)
+                                                                            .collect(Collectors.toList()),
+                typeMappings.stream()
+                            .map(INounMapping::createCopy)
+                            .collect(Collectors.toList()));
+    }
 
-	/**
-	 * Creates a new recommended instance.
-	 *
-	 * @param name        the name of the instance
-	 * @param type        the type of the instance
-	 * @param probability the probability that this instance should be found in the
-	 *                    model
-	 * @param nameNodes   the involved name mappings
-	 * @param typeNodes   the involved type mappings
-	 */
-	public RecommendedInstance(String name, String type, double probability, List<INounMapping> nameNodes, List<INounMapping> typeNodes) {
-		this.type = type;
-		this.name = name;
-		this.probability = probability;
-		nameMappings = new ArrayList<>(nameNodes);
-		typeMappings = new ArrayList<>(typeNodes);
-	}
+    /**
+     * Creates a new recommended instance.
+     *
+     * @param name
+     *            the name of the instance
+     * @param type
+     *            the type of the instance
+     * @param probability
+     *            the probability that this instance should be found in the model
+     * @param nameNodes
+     *            the involved name mappings
+     * @param typeNodes
+     *            the involved type mappings
+     */
+    public RecommendedInstance(String name, String type, double probability, List<INounMapping> nameNodes,
+            List<INounMapping> typeNodes) {
+        this.type = type;
+        this.name = name;
+        this.probability = probability;
+        nameMappings = new ArrayList<>(nameNodes);
+        typeMappings = new ArrayList<>(typeNodes);
+    }
 
-	/**
-	 * Returns the involved name mappings.
-	 *
-	 * @return the name mappings of this recommended instance
-	 */
-	@Override
-	public List<INounMapping> getNameMappings() {
-		return nameMappings;
-	}
+    /**
+     * Returns the involved name mappings.
+     *
+     * @return the name mappings of this recommended instance
+     */
+    @Override
+    public List<INounMapping> getNameMappings() {
+        return nameMappings;
+    }
 
-	/**
-	 * Returns the involved type mappings.
-	 *
-	 * @return the type mappings of this recommended instance
-	 */
-	@Override
-	public List<INounMapping> getTypeMappings() {
-		return typeMappings;
-	}
+    /**
+     * Returns the involved type mappings.
+     *
+     * @return the type mappings of this recommended instance
+     */
+    @Override
+    public List<INounMapping> getTypeMappings() {
+        return typeMappings;
+    }
 
-	/**
-	 * Returns the probability being an instance of the model.
-	 *
-	 * @return the probability to be found in the model
-	 */
-	@Override
-	public double getProbability() {
-		return probability;
-	}
+    /**
+     * Returns the probability being an instance of the model.
+     *
+     * @return the probability to be found in the model
+     */
+    @Override
+    public double getProbability() {
+        return probability;
+    }
 
-	/**
-	 * Removes nameMappings from this recommended instance.
-	 *
-	 * @param nameMappings the name mappings to remove
-	 */
-	@Override
-	public void removeNounNodeMappingsFromName(List<INounMapping> nameMappings) {
-		this.nameMappings.removeAll(nameMappings);
-	}
+    /**
+     * Removes nameMappings from this recommended instance.
+     *
+     * @param nameMappings
+     *            the name mappings to remove
+     */
+    @Override
+    public void removeNounNodeMappingsFromName(List<INounMapping> nameMappings) {
+        this.nameMappings.removeAll(nameMappings);
+    }
 
-	/**
-	 * Adds a name and type mapping to this recommended instance.
-	 *
-	 * @param nameMapping the name mapping to add
-	 * @param typeMapping the type mapping to add
-	 */
-	@Override
-	public void addMappings(INounMapping nameMapping, INounMapping typeMapping) {
-		addName(nameMapping);
-		addType(typeMapping);
-	}
+    /**
+     * Adds a name and type mapping to this recommended instance.
+     *
+     * @param nameMapping
+     *            the name mapping to add
+     * @param typeMapping
+     *            the type mapping to add
+     */
+    @Override
+    public void addMappings(INounMapping nameMapping, INounMapping typeMapping) {
+        addName(nameMapping);
+        addType(typeMapping);
+    }
 
-	/**
-	 * Adds name and type mappings to this recommended instance.
-	 *
-	 * @param nameMapping the name mappings to add
-	 * @param typeMapping the type mappings to add
-	 */
-	@Override
-	public void addMappings(List<INounMapping> nameMapping, List<INounMapping> typeMapping) {
-		nameMapping.forEach(this::addName);
-		typeMapping.forEach(this::addType);
-	}
+    /**
+     * Adds name and type mappings to this recommended instance.
+     *
+     * @param nameMapping
+     *            the name mappings to add
+     * @param typeMapping
+     *            the type mappings to add
+     */
+    @Override
+    public void addMappings(List<INounMapping> nameMapping, List<INounMapping> typeMapping) {
+        nameMapping.forEach(this::addName);
+        typeMapping.forEach(this::addType);
+    }
 
-	/**
-	 * Adds a name mapping to this recommended instance.
-	 *
-	 * @param nameMapping the name mapping to add
-	 */
-	@Override
-	public void addName(INounMapping nameMapping) {
-		if (!nameMappings.contains(nameMapping)) {
-			nameMappings.add(nameMapping);
-		}
-	}
+    /**
+     * Adds a name mapping to this recommended instance.
+     *
+     * @param nameMapping
+     *            the name mapping to add
+     */
+    @Override
+    public void addName(INounMapping nameMapping) {
+        if (!nameMappings.contains(nameMapping)) {
+            nameMappings.add(nameMapping);
+        }
+    }
 
-	/**
-	 * Adds a type mapping to this recommended instance.
-	 *
-	 * @param typeMapping the type mapping to add
-	 */
-	@Override
-	public void addType(INounMapping typeMapping) {
-		if (!typeMappings.contains(typeMapping)) {
-			typeMappings.add(typeMapping);
-		}
-	}
+    /**
+     * Adds a type mapping to this recommended instance.
+     *
+     * @param typeMapping
+     *            the type mapping to add
+     */
+    @Override
+    public void addType(INounMapping typeMapping) {
+        if (!typeMappings.contains(typeMapping)) {
+            typeMappings.add(typeMapping);
+        }
+    }
 
-	/**
-	 * Sets the probability to a given probability.
-	 *
-	 * @param probability the new probability
-	 */
-	@Override
-	public void setProbability(double probability) {
-		this.probability = probability;
-	}
+    /**
+     * Sets the probability to a given probability.
+     *
+     * @param probability
+     *            the new probability
+     */
+    @Override
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
 
-	/**
-	 * Returns the type as string from this recommended instance.
-	 *
-	 * @return the type as string
-	 */
-	@Override
-	public String getType() {
-		return type;
-	}
+    /**
+     * Returns the type as string from this recommended instance.
+     *
+     * @return the type as string
+     */
+    @Override
+    public String getType() {
+        return type;
+    }
 
-	/**
-	 * Returns the name as string from this recommended instance.
-	 *
-	 * @return the name as string
-	 */
-	@Override
-	public String getName() {
-		return name;
-	}
+    /**
+     * Returns the name as string from this recommended instance.
+     *
+     * @return the name as string
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * Sets the type of this recommended instance to the given type.
-	 *
-	 * @param type the new type
-	 */
-	@Override
-	public void setType(String type) {
-		this.type = type;
-	}
+    /**
+     * Sets the type of this recommended instance to the given type.
+     *
+     * @param type
+     *            the new type
+     */
+    @Override
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	/**
-	 * Sets the name of this recommended instance to the given name.
-	 *
-	 * @param name the new name
-	 */
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * Sets the name of this recommended instance to the given name.
+     *
+     * @param name
+     *            the new name
+     */
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public String toString() {
-		String separator = "\n\t\t\t\t\t";
-		List<String> typeNodeVals = new ArrayList<>();
-		List<String> typeOccurrences = new ArrayList<>();
-		List<Integer> typePositions = new ArrayList<>();
-		for (INounMapping typeMapping : typeMappings) {
-			typeNodeVals.add(typeMapping.toString());
-			typeOccurrences.addAll(typeMapping.getOccurrences());
-			typePositions.addAll(typeMapping.getMappingSentenceNo());
-		}
+    @Override
+    public String toString() {
+        String separator = "\n\t\t\t\t\t";
+        List<String> typeNodeVals = new ArrayList<>();
+        List<String> typeOccurrences = new ArrayList<>();
+        List<Integer> typePositions = new ArrayList<>();
+        for (INounMapping typeMapping : typeMappings) {
+            typeNodeVals.add(typeMapping.toString());
+            typeOccurrences.addAll(typeMapping.getOccurrences());
+            typePositions.addAll(typeMapping.getMappingSentenceNo());
+        }
 
-		List<String> nameNodeVals = new ArrayList<>();
-		List<String> nameOccurrences = new ArrayList<>();
-		List<Integer> namePositions = new ArrayList<>();
-		for (INounMapping nameMapping : nameMappings) {
-			nameNodeVals.add(nameMapping.toString());
-			nameOccurrences.addAll(nameMapping.getOccurrences());
-			namePositions.addAll(nameMapping.getMappingSentenceNo());
-		}
-		return "RecommendationInstance [" + " name=" + name + ", type=" + type + ", probability=" + probability + //
-				", mappings:]= " + separator + String.join(separator, nameNodeVals) + separator + String.join(separator, typeNodeVals) + "\n";
-	}
+        List<String> nameNodeVals = new ArrayList<>();
+        List<String> nameOccurrences = new ArrayList<>();
+        List<Integer> namePositions = new ArrayList<>();
+        for (INounMapping nameMapping : nameMappings) {
+            nameNodeVals.add(nameMapping.toString());
+            nameOccurrences.addAll(nameMapping.getOccurrences());
+            namePositions.addAll(nameMapping.getMappingSentenceNo());
+        }
+        return "RecommendationInstance [" + " name=" + name + ", type=" + type + ", probability=" + probability + //
+                ", mappings:]= " + separator + String.join(separator, nameNodeVals) + separator
+                + String.join(separator, typeNodeVals) + "\n";
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		RecommendedInstance other = (RecommendedInstance) obj;
-		if (name == null) {
-			if (other.name != null) {
-				return false;
-			}
-		} else if (!name.equals(other.name)) {
-			return false;
-		}
-
-		if (type == null) {
-			if (other.type != null) {
-				return false;
-			}
-
-		} else if (!type.equals(other.type)) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        RecommendedInstance other = (RecommendedInstance) obj;
+        return Objects.equals(name, other.name) && Objects.equals(type, other.type);
+    }
 
 }
