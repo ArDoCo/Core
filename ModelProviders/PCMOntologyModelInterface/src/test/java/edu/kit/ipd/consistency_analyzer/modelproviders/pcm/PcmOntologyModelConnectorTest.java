0c296b69d984f8d2ab401f5da8b672d375b9c961
Jan Keim <jan.keim@kit.edu>
2021-04-07 15:19:10 +0200

    add teastore to tests for PCMOntologyModelConnector

--- ModelProviders/PCMOntologyModelInterface/src/test/java/edu/kit/ipd/consistency_analyzer/modelproviders/pcm/PcmOntologyModelConnectorTest.java
+++ ModelProviders/PCMOntologyModelInterface/src/test/java/edu/kit/ipd/consistency_analyzer/modelproviders/pcm/PcmOntologyModelConnectorTest.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
@@ -19,30 +17,32 @@ import edu.kit.ipd.consistency_analyzer.datastructures.IInstance;
 class PcmOntologyModelConnectorTest {
     private static Logger logger = LogManager.getLogger();
 
-    private static PcmOntologyModelConnector connector;
+    private static PcmOntologyModelConnector connectorMediaStore;
 
-    @BeforeAll
-    static void setup() {
+    static PcmOntologyModelConnector setupMediaStore() {
         File file = new File("src/test/resources/mediastore.owl");
 
         String absolutePath = file.getAbsolutePath();
-        connector = new PcmOntologyModelConnector(absolutePath);
+        return new PcmOntologyModelConnector(absolutePath);
     }
 
     @Test
     @DisplayName("Get all instances from MediaStore ontology")
     void getInstancesFromMediaStoreTest() {
-        if (connector == null) {
+        PcmOntologyModelConnector connectorMediaStore = setupMediaStore();
+        if (connectorMediaStore == null) {
             logger.debug("connector is null");
         }
-        List<IInstance> instances = connector.getInstances();
+        List<IInstance> instances = connectorMediaStore.getInstances();
 
         if (logger.isDebugEnabled()) {
+            logger.debug("Listing MediaStore insances:");
             for (IInstance instance : instances) {
                 String info = instance.toString();
                 logger.debug(info);
                 logger.debug(instance.getNames());
             }
+            logger.debug("\n");
         }
 
         int expectedNumberOfInstances = 14;
@@ -56,10 +56,39 @@ class PcmOntologyModelConnectorTest {
             Assertions.assertTrue(expectedInstancesNames.contains(name), "Found instance does not match one of the expected instances!");
         }
 
+        connectorMediaStore = null;
     }
 
-    @AfterAll
-    static void cleanUp() {
-        connector = null;
+    static PcmOntologyModelConnector setupTeaStore() {
+        File file = new File("src/test/resources/teastore.owl");
+
+        String absolutePath = file.getAbsolutePath();
+        return new PcmOntologyModelConnector(absolutePath);
     }
+
+    @Test
+    @DisplayName("Get all instances from TeaStore ontology")
+    void getInstancesFromTeaStoreTest() {
+        PcmOntologyModelConnector connectorTeaStore = setupTeaStore();
+        if (connectorTeaStore == null) {
+            logger.debug("connector is null");
+        }
+        List<IInstance> instances = connectorTeaStore.getInstances();
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Listing TeaStore insances:");
+            for (IInstance instance : instances) {
+                String info = instance.toString();
+                logger.debug(info);
+                logger.debug(instance.getNames());
+            }
+            logger.debug("\n");
+        }
+
+        int expectedNumberOfInstances = 13;
+        Assertions.assertEquals(expectedNumberOfInstances, instances.size(), "The number of expected and found instances differs!");
+
+        connectorTeaStore = null;
+    }
+
 }
