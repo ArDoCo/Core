53082c63d6c6b7071cd94af4364c787e36a91560
Jan Keim <jan.keim@kit.edu>
2021-05-03 11:42:37 +0200

    cleanup TextExtractor

--- text-extractor/src/main/java/edu/kit/kastel/mcse/ardoco/core/textextractor/TextExtractorConfig.java
+++ text-extractor/src/main/java/edu/kit/kastel/mcse/ardoco/core/textextractor/TextExtractorConfig.java
@@ -8,6 +8,11 @@ import edu.kit.kastel.mcse.ardoco.core.util.SystemParameters;
 
 public class TextExtractorConfig extends Configuration {
 
+    private static final String CONFIGS_TEXT_EXTRACTOR_PROPERTIES = "/configs/TextExtractor.properties";
+    private static final String DELIMITER = " ";
+    private static final String SIMILARITY_PERCENTAGE = "similarityPercentage";
+    private static final String TEXT_AGENTS = "Text_Agents";
+
     public static final TextExtractorConfig DEFAULT_CONFIG = new TextExtractorConfig();
 
     /**
@@ -18,21 +23,21 @@ public class TextExtractorConfig extends Configuration {
     public final double similarityPercentage;
 
     public TextExtractorConfig() {
-        SystemParameters config = new SystemParameters("/configs/TextExtractor.properties", true);
-        textAgents = config.getPropertyAsList("Text_Agents");
-        similarityPercentage = config.getPropertyAsDouble("similarityPercentage");
+        SystemParameters config = new SystemParameters(CONFIGS_TEXT_EXTRACTOR_PROPERTIES, true);
+        textAgents = config.getPropertyAsList(TEXT_AGENTS);
+        similarityPercentage = config.getPropertyAsDouble(SIMILARITY_PERCENTAGE);
     }
 
     public TextExtractorConfig(Map<String, String> configs) {
-        textAgents = getPropertyAsList("Text_Agents", configs);
-        similarityPercentage = getPropertyAsDouble("similarityPercentage", configs);
+        textAgents = getPropertyAsList(TEXT_AGENTS, configs);
+        similarityPercentage = getPropertyAsDouble(SIMILARITY_PERCENTAGE, configs);
     }
 
     @Override
     protected Map<String, String> getAllProperties() {
         return Map.of(//
-                "Text_Agents", String.join(" ", textAgents), //
-                "similarityPercentage", String.valueOf(similarityPercentage) //
+                TEXT_AGENTS, String.join(DELIMITER, textAgents), //
+                SIMILARITY_PERCENTAGE, String.valueOf(similarityPercentage) //
         );
     }
 }
