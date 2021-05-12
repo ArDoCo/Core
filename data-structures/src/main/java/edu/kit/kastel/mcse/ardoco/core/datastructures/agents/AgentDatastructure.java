2695cfb3f793bb0e18183f39278a6393478d1ce0
Jan Keim <jan.keim@kit.edu>
2021-05-12 17:02:23 +0200

    add base for inconsistency-detection

--- data-structures/src/main/java/edu/kit/kastel/mcse/ardoco/core/datastructures/agents/AgentDatastructure.java
+++ data-structures/src/main/java/edu/kit/kastel/mcse/ardoco/core/datastructures/agents/AgentDatastructure.java
@@ -3,6 +3,7 @@ package edu.kit.kastel.mcse.ardoco.core.datastructures.agents;
 import java.util.logging.Logger;
 
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IConnectionState;
+import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IInconsistencyState;
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IModelState;
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IRecommendationState;
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IText;
@@ -10,122 +11,140 @@ import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.ITextState;
 
 public class AgentDatastructure {
 
-	private Logger logger = Logger.getLogger(this.getClass().getName());
+    private Logger logger = Logger.getLogger(this.getClass().getName());
 
-	private IText text;
-	private ITextState textState;
-	private IModelState modelState;
-	private IRecommendationState recommendationState;
-	private IConnectionState connectionState;
+    private IText text;
+    private ITextState textState;
+    private IModelState modelState;
+    private IRecommendationState recommendationState;
+    private IConnectionState connectionState;
+    private IInconsistencyState inconsistencyState;
 
-	public AgentDatastructure() {
-	}
+    public AgentDatastructure() {
+    }
 
-	public AgentDatastructure createCopy() {
-		AgentDatastructure data = new AgentDatastructure();
-		data.text = text;
-		data.textState = textState == null ? null : textState.createCopy();
-		data.modelState = modelState == null ? null : modelState.createCopy();
-		data.recommendationState = recommendationState == null ? null : recommendationState.createCopy();
-		data.connectionState = connectionState == null ? null : connectionState.createCopy();
-		return data;
-	}
+    public AgentDatastructure createCopy() {
+        AgentDatastructure data = new AgentDatastructure();
+        data.text = text;
+        data.textState = textState == null ? null : textState.createCopy();
+        data.modelState = modelState == null ? null : modelState.createCopy();
+        data.recommendationState = recommendationState == null ? null : recommendationState.createCopy();
+        data.connectionState = connectionState == null ? null : connectionState.createCopy();
+        return data;
+    }
 
-	public AgentDatastructure(IText text, ITextState textState, IModelState modelState, IRecommendationState recommendationState,
-			IConnectionState connectionState) {
-		this.text = text;
-		this.textState = textState;
-		this.modelState = modelState;
-		this.recommendationState = recommendationState;
-		this.connectionState = connectionState;
-	}
+    public AgentDatastructure(IText text, ITextState textState, IModelState modelState, IRecommendationState recommendationState,
+            IConnectionState connectionState, IInconsistencyState inconsistencyState) {
+        this.text = text;
+        this.textState = textState;
+        this.modelState = modelState;
+        this.recommendationState = recommendationState;
+        this.connectionState = connectionState;
+        this.inconsistencyState = inconsistencyState;
+    }
 
-	public IText getText() {
-		return text;
-	}
+    public IText getText() {
+        return text;
+    }
 
-	public void setText(IText text) {
-		this.text = text;
-	}
+    public void setText(IText text) {
+        this.text = text;
+    }
 
-	public ITextState getTextState() {
-		return textState;
-	}
+    public ITextState getTextState() {
+        return textState;
+    }
 
-	public void setTextState(ITextState textState) {
-		this.textState = textState;
-	}
+    public void setTextState(ITextState textState) {
+        this.textState = textState;
+    }
 
-	public IModelState getModelState() {
-		return modelState;
-	}
+    public IModelState getModelState() {
+        return modelState;
+    }
 
-	public void setModelState(IModelState modelState) {
-		this.modelState = modelState;
-	}
+    public void setModelState(IModelState modelState) {
+        this.modelState = modelState;
+    }
 
-	public IRecommendationState getRecommendationState() {
-		return recommendationState;
-	}
+    public IRecommendationState getRecommendationState() {
+        return recommendationState;
+    }
 
-	public void setRecommendationState(IRecommendationState recommendationState) {
-		this.recommendationState = recommendationState;
-	}
+    public void setRecommendationState(IRecommendationState recommendationState) {
+        this.recommendationState = recommendationState;
+    }
 
-	public IConnectionState getConnectionState() {
-		return connectionState;
-	}
+    public IConnectionState getConnectionState() {
+        return connectionState;
+    }
 
-	public void setConnectionState(IConnectionState connectionState) {
-		this.connectionState = connectionState;
-	}
+    public void setConnectionState(IConnectionState connectionState) {
+        this.connectionState = connectionState;
+    }
 
-	public void overwrite(AgentDatastructure newData) {
-		text = newData.text;
-		textState = newData.textState;
-		modelState = newData.modelState;
-		recommendationState = newData.recommendationState;
-		connectionState = newData.connectionState;
-	}
+    public IInconsistencyState getInconsistencyState() {
+        return inconsistencyState;
+    }
 
-	public void extend(IModelState modelState) {
-		if (null != this.modelState) {
-			logger.warning("ModelState was overwritten.");
-		} else {
-			this.modelState = modelState;
-		}
-	}
+    public void setInconsistencyState(IInconsistencyState inconsistencyState) {
+        this.inconsistencyState = inconsistencyState;
+    }
 
-	public void extend(IText text) {
-		if (null != this.text) {
-			logger.warning("Text was overwritten.");
-		} else {
-			this.text = text;
-		}
-	}
+    public void overwrite(AgentDatastructure newData) {
+        text = newData.text;
+        textState = newData.textState;
+        modelState = newData.modelState;
+        recommendationState = newData.recommendationState;
+        connectionState = newData.connectionState;
+    }
 
-	public void extend(ITextState textState) {
-		if (null != this.textState) {
-			logger.warning("TextState was overwritten.");
-		} else {
-			this.textState = textState;
-		}
-	}
+    public void extend(IModelState modelState) {
+        if (null != this.modelState) {
+            logger.warning("ModelState was overwritten.");
+        } else {
+            this.modelState = modelState;
+        }
+    }
 
-	public void extend(IRecommendationState recommendationState) {
-		if (null != this.recommendationState) {
-			logger.warning("RecommendationState was overwritten.");
-		} else {
-			this.recommendationState = recommendationState;
-		}
-	}
+    public void extend(IText text) {
+        if (null != this.text) {
+            logger.warning("Text was overwritten.");
+        } else {
+            this.text = text;
+        }
+    }
 
-	public void extend(IConnectionState connectionState) {
-		if (null != this.connectionState) {
-			logger.warning("ConnectionState was overwritten.");
-		} else {
-			this.connectionState = connectionState;
-		}
-	}
+    public void extend(ITextState textState) {
+        if (null != this.textState) {
+            logger.warning("TextState was overwritten.");
+        } else {
+            this.textState = textState;
+        }
+    }
+
+    public void extend(IRecommendationState recommendationState) {
+        if (null != this.recommendationState) {
+            logger.warning("RecommendationState was overwritten.");
+        } else {
+            this.recommendationState = recommendationState;
+        }
+    }
+
+    public void extend(IConnectionState connectionState) {
+        if (null != this.connectionState) {
+            logger.warning("ConnectionState was overwritten.");
+        } else {
+            this.connectionState = connectionState;
+        }
+    }
+
+    public void extend(IInconsistencyState inconsistencyState) {
+        if (null != this.inconsistencyState) {
+            logger.warning("InconsistencyState was overwritten.");
+        } else {
+            this.inconsistencyState = inconsistencyState;
+        }
+    }
 
 }
