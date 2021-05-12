bb6267aeafdadb11d6a904f3d0b0ebb12135d54f
Jan Keim <jan.keim@kit.edu>
2021-05-12 17:02:23 +0200

    add base for inconsistency-detection

---
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/datastructures/InconsistencyState.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package edu.kit.kastel.mcse.ardoco.core.inconsistency.datastructures;
+
+import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IInconsistencyState;
+
+/**
+ * @author Jan Keim
+ *
+ */
+public class InconsistencyState implements IInconsistencyState {
+
+}
