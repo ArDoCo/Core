dfc4061c988e0e397faf361dc83470d38d09597a
Jan Keim <jan.keim@kit.edu>
2021-05-17 15:59:31 +0200

    implement actual detection and creation of Inconsistencies

---
+++ data-structures/src/main/java/edu/kit/kastel/mcse/ardoco/core/datastructures/definitions/IInconsistency.java
@@ -0,0 +1,12 @@
+package edu.kit.kastel.mcse.ardoco.core.datastructures.definitions;
+
+public interface IInconsistency {
+
+    /**
+     * Returns the reason why there is an inconsistency
+     *
+     * @return The reason of inconsistency
+     */
+    String getReason();
+
+}
--- data-structures/src/main/java/edu/kit/kastel/mcse/ardoco/core/datastructures/definitions/IInconsistencyState.java
+++ data-structures/src/main/java/edu/kit/kastel/mcse/ardoco/core/datastructures/definitions/IInconsistencyState.java
@@ -1,5 +1,7 @@
 package edu.kit.kastel.mcse.ardoco.core.datastructures.definitions;
 
+import java.util.List;
+
 import edu.kit.kastel.mcse.ardoco.core.datastructures.modules.IState;
 
 /**
@@ -7,5 +9,20 @@ import edu.kit.kastel.mcse.ardoco.core.datastructures.modules.IState;
  *
  */
 public interface IInconsistencyState extends IState {
+
+    /**
+     * Returns a list of inconsistencies held by this state
+     *
+     * @return list of inconsistencies
+     */
+    List<IInconsistency> getInconsistencies();
+
+    /**
+     * Add an Inconsistency to this state
+     *
+     * @param inconsistency the inconsistency to add
+     * @return true if added successfully
+     */
+    boolean addInconsistency(IInconsistency inconsistency);
 
 }
