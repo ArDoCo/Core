603bdf0d8f1252b8ec66505492a7934807e1106c
Jan Keim <jan.keim@kit.edu>
2021-05-17 15:59:31 +0200

    implement actual detection and creation of Inconsistencies

--- inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/NameInconsistencyAgent.java
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/NameInconsistencyAgent.java
@@ -17,9 +17,12 @@ import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IRecommendatio
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IRecommendedInstance;
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IText;
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.ITextState;
+import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IWord;
+import edu.kit.kastel.mcse.ardoco.core.inconsistency.datastructures.NameInconsistency;
 
 @MetaInfServices(InconsistencyAgent.class)
 public class NameInconsistencyAgent extends InconsistencyAgent {
+    private static final String REGEX_DIVIDER = "[\\.:]";
 
     public NameInconsistencyAgent() {
         super(GenericInconsistencyConfig.class);
@@ -41,25 +44,55 @@ public class NameInconsistencyAgent extends InconsistencyAgent {
 
     @Override
     public void exec() {
-        // TODO Auto-generated method stub
-        System.out.println("Executing NameInconsistencyAgent");
         List<IInstanceLink> tracelinks = connectionState.getInstanceLinks();
         for (IInstanceLink tracelink : tracelinks) {
             IInstance modelInstance = tracelink.getModelInstance();
-            String modelName = modelInstance.getLongestName();
-            System.out.println("\tTracelink for " + modelName);
             IRecommendedInstance recommendationInstance = tracelink.getTextualInstance();
             List<INounMapping> nameMappings = recommendationInstance.getNameMappings();
             for (INounMapping nameMapping : nameMappings) {
-                String nameMappingReference = nameMapping.getReference();
-                System.out.println("\t\tReference: " + nameMappingReference);
-                List<String> occurences = nameMapping.getOccurrences();
-                for (String occurence : occurences) {
-                    System.out.println("\t\t\t Occurence: " + occurence);
-                    // TODO: idea: if the occurence does not match the modelName, generate a inconsistency warning
+                List<IWord> words = nameMapping.getWords();
+                for (IWord word : words) {
+                    analyseWord(modelInstance, word);
                 }
             }
         }
     }
 
+    private void analyseWord(IInstance modelInstance, IWord word) {
+        if (!equalTextOrLemma(modelInstance, word)) {
+            if (partOfDividerEqualsModelInstance(modelInstance, word)) {
+                return;
+            }
+            var nameInconsistency = new NameInconsistency(modelInstance, word);
+            inconsistencyState.addInconsistency(nameInconsistency);
+        }
+    }
+
+    /**
+     * Return if text or lemma of provided word matches to the model instance
+     *
+     * @param modelInstance the model instance
+     * @param word          the word
+     * @return <code>true</code>, if there is a match, otherwise <code>false</code>
+     */
+    private boolean equalTextOrLemma(IInstance modelInstance, IWord word) {
+        String modelName = modelInstance.getLongestName();
+        String text = word.getText();
+        String lemma = word.getLemma();
+
+        // TODO employ other/better String comparison instead of exact equals?
+        return modelName.equalsIgnoreCase(text) && modelName.equalsIgnoreCase(lemma);
+    }
+
+    private boolean partOfDividerEqualsModelInstance(IInstance modelInstance, IWord word) {
+        String text = word.getText();
+        String modelName = modelInstance.getLongestName();
+        for (String part : text.split(REGEX_DIVIDER)) {
+            // TODO employ other/better String comparison instead of exact equals?
+            if (part.equalsIgnoreCase(modelName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
