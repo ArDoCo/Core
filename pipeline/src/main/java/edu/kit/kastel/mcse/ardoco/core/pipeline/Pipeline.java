9bc1a62a90f1eb6accce1afd7276551138519022
Jan Keim <jan.keim@kit.edu>
2021-06-14 16:18:42 +0200

    exchangeSys.err with logger output

--- pipeline/src/main/java/edu/kit/kastel/mcse/ardoco/core/pipeline/Pipeline.java
+++ pipeline/src/main/java/edu/kit/kastel/mcse/ardoco/core/pipeline/Pipeline.java
@@ -108,7 +108,7 @@ public class Pipeline {
     }
 
     private static void printUsage() {
-        System.err.println(
+        logger.info(
                 """
                         Usage: java -jar ardoco-core-pipeline.jar
 
