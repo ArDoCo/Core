c63761e41998eed48bc6909a048f1bce53bfe086
Jan Keim <jan.keim@kit.edu>
2021-07-01 12:38:04 +0200

    add some more functionality into OntologyConnector

--- ontology-connector/src/main/java/edu/kit/kastel/mcse/ardoco/core/ontology/OntologyConnector.java
+++ ontology-connector/src/main/java/edu/kit/kastel/mcse/ardoco/core/ontology/OntologyConnector.java
@@ -17,11 +17,21 @@ import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.ontology.Ontology;
+import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.reasoner.ValidityReport;
+import org.apache.jena.reasoner.ValidityReport.Report;
 import org.apache.jena.riot.Lang;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.XSD;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.util.UuidUtil;
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.MutableList;
 
@@ -37,11 +47,60 @@ public class OntologyConnector {
 
     private String pathToOntology;
     private Ontology ontology;
+    private InfModel infModel;
 
     public OntologyConnector(String ontologyUrl) {
         pathToOntology = ontologyUrl;
         ontModel = loadOntology(pathToOntology);
         ontology = getBaseOntology().orElseThrow(() -> new IllegalArgumentException("Could not load ontology: No base ontology found"));
+    }
+
+    private OntologyConnector() {
+        pathToOntology = null;
+        ontModel = ModelFactory.createOntologyModel(modelSpec);
+    }
+
+    /**
+     * Creates an OntologyConnector based on no existing ontology, so creates an empty ontology.
+     *
+     * @param defaultNameSpaceUri The defaul namespace URI
+     * @return An OntologyConnector based on no existing ontology
+     */
+    public static OntologyConnector createWithEmptyOntology(String defaultNameSpaceUri) {
+        var ontologyConnector = new OntologyConnector();
+        ontologyConnector.ontology = ontologyConnector.ontModel.createOntology(defaultNameSpaceUri);
+        ontologyConnector.ontModel.setNsPrefix("", defaultNameSpaceUri);
+        ontologyConnector.ontModel.setNsPrefix("xsd", XSD.NS);
+        return ontologyConnector;
+    }
+
+    /**
+     * Validates the ontology. A logger will put out warnings iff there are conflicts.
+     *
+     * @return <code>true</code> if the ontology is valid and has no conflicts, <code>false</code> if there are
+     *         conflicts
+     */
+    public boolean validateOntology() {
+        var validationInfModel = ModelFactory.createRDFSModel(ontModel);
+        ValidityReport validity = validationInfModel.validate();
+        if (validity.isValid()) {
+            return true;
+        }
+        Iterator<Report> i = validity.getReports();
+        while (i.hasNext()) {
+            logger.warn("Conflict in ontology: {}", i.next());
+        }
+        return false;
+    }
+
+    /**
+     * Add a namespace prefix
+     *
+     * @param prefix the new prefix that should be able to use
+     * @param uri    the URI that the prefix should be resolved to
+     */
+    public void addNsPrefix(String prefix, String uri) {
+        ontModel.setNsPrefix(prefix, uri);
     }
 
     /**
@@ -134,6 +193,19 @@ public class OntologyConnector {
         return ontModel;
     }
 
+    /**
+     * Returns the {@link InfModel} for inference reasons.
+     *
+     * @return The InfModel for the ontology
+     */
+    private synchronized InfModel getInfModel() {
+        if (infModel == null) {
+            var reasoner = ReasonerRegistry.getOWLReasoner();
+            infModel = ModelFactory.createInfModel(reasoner, ontModel);
+        }
+        return infModel;
+    }
+
     private String createUri(String prefix, String suffix) {
         String encodedSuffix = suffix;
         try {
@@ -163,26 +235,29 @@ public class OntologyConnector {
 
     }
 
-    private Property getIdProperty() {
-        Optional<Property> optionalProperty = getProperty("id_-_Identifier");
-        if (optionalProperty.isEmpty()) {
-            throw new IllegalStateException("Cannot find the \"id\" property!");
-        }
-        return optionalProperty.get();
-    }
-
-    private Property getEntityNameProperty() {
-        Optional<Property> optionalProperty = getProperty("entityName_-_NamedElement");
-        if (optionalProperty.isEmpty()) {
-            throw new IllegalStateException("Cannot find the \"entityName\" property!");
-        }
-        return optionalProperty.get();
-    }
-
+    /* CLASSES */
+    /**
+     * Looks for an {@link OntClass} that contains the given name as either label or uses the name as (local) Iri.
+     *
+     * @param className Label or Iri of the wanted class
+     * @return Optional containing the given class. Returns an empty optional, if the class could not be found
+     */
     public Optional<OntClass> getClass(String className) {
+        // first look for usage of className as label
+        var stmts = ontModel.listStatements(null, RDFS.label, className, null);
+        if (stmts.hasNext()) {
+            var resource = stmts.next().getSubject();
+            if (resource.canAs(OntClass.class)) {
+                var clazz = ontModel.createClass(resource.getURI());
+                return Optional.of(clazz);
+            }
+        }
+
+        // if the className was not a label, looks for usage of the className in the Iris
         var prefixes = ontModel.getNsPrefixMap().keySet();
         for (var prefix : prefixes) {
-            var optClass = getClass(className, prefix);
+            var uri = createUri(prefix, className);
+            var optClass = getClassByIri(uri);
             if (optClass.isPresent()) {
                 return optClass;
             }
@@ -190,25 +265,262 @@ public class OntologyConnector {
         return Optional.empty();
     }
 
+    /**
+     * Looks for a class with the given name and the given prefix. The name can be either a local iri or a given label.
+     * If it is a label, this method will only return classes that can be found within the namespace of the given prefix
+     *
+     * @param className label or iri of the class that should be returned
+     * @param prefix    Prefix of the namespace the class should be contained in.
+     * @return {@link Optional} containing the wanted class if it was found. Else, returns an empty {@link Optional}
+     */
     public Optional<OntClass> getClass(String className, String prefix) {
         if (prefix == null || prefix.isEmpty()) {
             prefix = DEFAULT_PREFIX;
         }
+        var prefixUri = ontModel.getNsPrefixURI(prefix);
+        if (prefixUri == null) {
+            return Optional.empty();
+        }
+
         var uri = createUri(prefix, className);
-        return getClassByIri(uri);
+        var clazz = getClassByIri(uri);
+        if (clazz.isPresent()) {
+            return clazz;
+        }
+        var stmts = ontModel.listStatements(null, RDFS.label, className, null);
+        while (stmts.hasNext()) {
+            var resource = stmts.next().getSubject();
+            var namespace = resource.getNameSpace();
+            if (prefixUri.equals(namespace)) {
+                return Optional.ofNullable(ontModel.createClass(resource.getURI()));
+            }
+        }
+        return Optional.empty();
     }
 
+    /**
+     * Returns an {@link Optional} containing the {@link OntClass} that corresponds to the given uri. The uri might be
+     * in prefix-notation (e.g., "owl:Thing"). If no class is found, the returned {@link Optional} will be empty
+     *
+     * @param iri the iri of the class (can be prefix notation or simple uri notation)
+     * @return {@link Optional} containing the {@link OntClass} that corresponds to the given iri. Empty Optional if no
+     *         class exists.
+     */
     public Optional<OntClass> getClassByIri(String iri) {
+        var expandedUri = ontModel.expandPrefix(iri);
+        return Optional.ofNullable(ontModel.getOntClass(expandedUri));
+    }
+
+    /**
+     * Adds a class with the given name to the ontology. If the class exists already, returns the existing class.
+     *
+     * @param className Name of the class that should be created
+     * @return created class
+     */
+    public OntClass addClass(String className) {
+        Optional<OntClass> clazzOpt = getClass(className);
+        if (clazzOpt.isPresent()) {
+            return clazzOpt.get();
+        }
+
+        var uri = generateRandomURI(DEFAULT_PREFIX);
+        var clazz = ontModel.createClass(uri);
+        clazz.addProperty(RDFS.label, className);
+        return clazz;
+    }
+
+    /**
+     * Adds a class with the given Iri and returns this class. If a class with that Iri already exists, returns the
+     * existing class.
+     *
+     * @param iri Iri of the class that should be added
+     * @return {@link OntClass} with the given Iri
+     */
+    public OntClass addClassByIri(String iri) {
+        Optional<OntClass> clazz = getClassByIri(iri);
+        if (clazz.isPresent()) {
+            return clazz.get();
+        }
         String uri = ontModel.expandPrefix(iri);
-        var clazz = ontModel.getOntClass(uri);
-
-        return Optional.ofNullable(clazz);
+        return ontModel.createClass(uri);
     }
 
-    private MutableList<Individual> getInstancesOfClass(OntClass clazz) {
-        return createMutableListFromIterator(ontModel.listIndividuals(clazz));
+    /**
+     * Add superclass relation between the given arguments.
+     *
+     * @param subClass   class that should be subclass of the other given class/resource
+     * @param superClass class/resource that should be superclass of the other given class
+     */
+    public void addSuperClass(OntClass subClass, Resource superClass) {
+        subClass.addSuperClass(superClass);
     }
 
+    /**
+     * Sets exclusive superclassing for the given sub-class to the given super-class. Overwrites previous super-classes.
+     *
+     * @param subClass   class that should be subclass of the other given class/resource
+     * @param superClass class/resource that should be superclass of the other given class
+     */
+    public void addSuperClassExclusive(OntClass subClass, Resource superClass) {
+        subClass.setSuperClass(superClass);
+    }
+
+    /**
+     * Add superclass relation between the given arguments.
+     *
+     * @param className      name of the class that should be subclass of the other given class/resource
+     * @param superClassName name of the class that should be superclass of the other given class
+     */
+    public OntClass addSuperClass(String className, String superClassName) {
+        Optional<OntClass> superClassOpt = getClass(superClassName);
+        if (superClassOpt.isPresent()) {
+            OntClass superClass = superClassOpt.get();
+            return addSubClass(className, superClass);
+        } else {
+            return addClass(className);
+        }
+    }
+
+    /**
+     * Add superclass relation between the given arguments.
+     *
+     * @param className  name of the class that should be subclass of the other given class/resource
+     * @param superClass class that should be superclass of the other given class
+     */
+    public OntClass addSubClass(String className, OntClass superClass) {
+        OntClass clazz = addClass(className);
+        superClass.addSubClass(clazz);
+        return clazz;
+    }
+
+    /**
+     * Add superclass relation between the given arguments.
+     *
+     * @param subClass   class that should be subclass of the other given class/resource
+     * @param superClass class that should be superclass of the other given class
+     */
+    public void addSubClass(OntClass subClass, OntClass superClass) {
+        superClass.addSubClass(subClass);
+    }
+
+    /**
+     * Checks whether there is a sub/super-class relation between the given classes (order is important!)
+     *
+     * @param clazz      sub-class
+     * @param superClass super-class
+     * @return True if sub-class and super-class are related correspondingly
+     */
+    public boolean classIsSubClassOf(OntClass clazz, OntClass superClass) {
+        return clazz.hasSuperClass(superClass) && superClass.hasSubClass(clazz);
+    }
+
+    /**
+     * Removes sub/super-classing relation between given classes.
+     *
+     * @param clazz      previous sub-class
+     * @param superClass previous super-class
+     */
+    public void removeSubClassing(OntClass clazz, OntClass superClass) {
+        clazz.removeSuperClass(superClass);
+        superClass.removeSubClass(clazz);
+    }
+
+    /**
+     * Returns whether there is a class in the ontology with the given name. See also: {@link #getClass(String)}.
+     *
+     * @param className class to look for
+     * @return True if there is a class with the given name. Else, False
+     */
+    public boolean containsClass(String className) {
+        return getClass(className).isPresent();
+    }
+
+    /**
+     * Returns whether there is a class in the ontology with the given name and the given prefix. See also:
+     * {@link #getClass(String, String)}.
+     *
+     * @param className class to look for
+     * @param prefix    prefix of the class
+     * @return True if there is a class with the given name and prefix. Else, False
+     */
+    public boolean containsClass(String className, String prefix) {
+        return getClass(className, prefix).isPresent();
+    }
+
+    /* INDIVIDUALS */
+
+    /**
+     * Returns an {@link Optional} that contains a named individual with the given uri. If no individual with that uri
+     * exists, returns empty {@link Optional}.
+     *
+     * @param uri uri of the individual
+     * @return Optional with the individual if it exists. Otherwise, empty Optional.
+     */
+    public Optional<Individual> getIndividualByUri(String uri) {
+        return Optional.ofNullable(ontModel.getIndividual(uri));
+    }
+
+    /**
+     * Returns the Individuals that have a class that corresponds to the given class name
+     *
+     * @param className Name of the class
+     * @return List of Individuals for the given class (name)
+     */
+    public List<Individual> getInstancesOfClass(String className) {
+        Optional<OntClass> optClass = getClass(className);
+        if (!optClass.isPresent()) {
+            return Lists.mutable.empty();
+        }
+        OntClass clazz = optClass.get();
+        return getInstancesOfClass(clazz);
+    }
+
+    private List<Individual> getInstancesOfClass(OntClass clazz) {
+        return ontModel.listIndividuals(clazz).toList();
+    }
+
+    /**
+     * Similar to {@link #getInstancesOfClass(String)}, but also checks for inferred instances.
+     *
+     * @param className name of the class to retrieve individuals from
+     * @return List of Individuals for the given class (name), including inferred ones
+     */
+    public List<Individual> getInferredInstancesOfClass(String className) {
+        Optional<OntClass> optClass = getClass(className);
+        if (!optClass.isPresent()) {
+            return Lists.mutable.empty();
+        }
+        OntClass clazz = optClass.get();
+
+        StmtIterator stmts = getInfModel().listStatements(null, RDF.type, clazz);
+        return createMutableIndividualListFromStatementIterator(stmts);
+    }
+
+    private MutableList<Individual> createMutableIndividualListFromStatementIterator(StmtIterator stmts) {
+        MutableList<Individual> individuals = Lists.mutable.empty();
+        while (stmts.hasNext()) {
+            var stmt = stmts.nextStatement();
+            var res = stmt.getSubject();
+            if (res.canAs(Individual.class)) {
+                individuals.add(res.as(Individual.class));
+            }
+        }
+        return individuals;
+    }
+
+    /**
+     * Adds an Individual to the given class
+     *
+     * @param name  name of the individual that should be added
+     * @param clazz Class the individual should be added to
+     * @return the created individual
+     */
+    public Individual addInstanceToClass(String name, OntClass clazz) {
+        String uri = createUri(DEFAULT_PREFIX, name);
+        return ontModel.createIndividual(uri, clazz);
+    }
+
+    /* PROPERTIES */
     public Optional<Property> getProperty(String dataPropertyLocalName) {
         var prefixes = ontModel.getNsPrefixMap().keySet();
         for (var prefix : prefixes) {
@@ -233,8 +545,26 @@ public class OntologyConnector {
         return Optional.ofNullable(datatypeProperty);
     }
 
-    private static <T> MutableList<T> createMutableListFromIterator(Iterator<T> iterator) {
-        return Lists.mutable.ofAll(() -> iterator);
+    // TODO move getIdProperty and getEntityName into model-provider-owl
+    private Property getIdProperty() {
+        Optional<Property> optionalProperty = getProperty("id_-_Identifier");
+        if (optionalProperty.isEmpty()) {
+            throw new IllegalStateException("Cannot find the \"id\" property!");
+        }
+        return optionalProperty.get();
     }
 
+    private Property getEntityNameProperty() {
+        Optional<Property> optionalProperty = getProperty("entityName_-_NamedElement");
+        if (optionalProperty.isEmpty()) {
+            throw new IllegalStateException("Cannot find the \"entityName\" property!");
+        }
+        return optionalProperty.get();
+    }
+
+    /* Convenience Methods */
+    private String generateRandomURI(String prefix) {
+        var uuid = UuidUtil.getTimeBasedUuid().toString();
+        return createUri(prefix, uuid);
+    }
 }
--- ontology-connector/src/test/java/edu/kit/kastel/mcse/ardoco/core/ontology/OntologyConnectorTest.java
+++ ontology-connector/src/test/java/edu/kit/kastel/mcse/ardoco/core/ontology/OntologyConnectorTest.java
@@ -14,8 +14,15 @@ import org.junit.runner.RunWith;
 @RunWith(JUnitPlatform.class)
 class OntologyConnectorTest {
     private static Logger logger = LogManager.getLogger();
-
     private static String ontologyPath = "src/test/resources/mediastore.owl";
+
+    private static final String NONEXISTENT = "><$NONEXISTENT!><";
+    private static final String LABEL_E_CLASS = "EClass";
+    private static final String URI_E_CLASS = "https://informalin.github.io/knowledgebases/informalin_base_ecore.owl#OWLClass_EClass";
+    private static final String LOCAL_IRI_NAMED_ELEMENT = "NamedElement";
+    private static final String URI_NAMED_ELEMENT = "https://informalin.github.io/knowledgebases/informalin_base_pcm.owl#NamedElement";
+    private static final String LABEL_SYSTEM = "defaultSystem";
+    private static final String URI_SYSTEM = "https://informalin.github.io/knowledgebases/examples/mediastore.owl#System_oPwBYHDhEeSqnN80MQ2uGw";
 
     private OntologyConnector ontologyConnector;
 
@@ -46,6 +53,49 @@ class OntologyConnectorTest {
         var baseOntology = baseOntologyOpt.get();
         var assumedBaseOntologyUri = "https://informalin.github.io/knowledgebases/examples/mediastore.owl#";
         Assertions.assertEquals(assumedBaseOntologyUri, baseOntology.getURI());
+    }
+
+    @Test
+    @DisplayName("Test getClass() with existing and correct labels")
+    void getClassWithLabelTest() {
+        var clazz = ontologyConnector.getClass(LABEL_E_CLASS);
+        Assertions.assertTrue(clazz.isPresent(), "Could not find class with name in label.");
+        Assertions.assertTrue(clazz.get().hasURI(URI_E_CLASS), "Found class has invalid URI.");
+    }
+
+    @Test
+    @DisplayName("Test getClass() with existing and correct URIs")
+    void getClassWithUriTest() {
+        var clazz = ontologyConnector.getClass(LOCAL_IRI_NAMED_ELEMENT);
+        Assertions.assertTrue(clazz.isPresent(), "Could not find class with name in Iri.");
+        Assertions.assertTrue(clazz.get().hasURI(URI_NAMED_ELEMENT), "Found class has invalid URI.");
+    }
+
+    @Test
+    @DisplayName("Test getClass() with non-existing classes.")
+    void getNonexistentClassTest() {
+        var clazz = ontologyConnector.getClass(NONEXISTENT);
+        Assertions.assertTrue(clazz.isEmpty(), "Found a class although it should be non-existent.");
+    }
+
+    @Test
+    @DisplayName("Test getClass() with details for non-class entities like individuals.")
+    void getClassWithNonClassDetailsTest() {
+        var clazz = ontologyConnector.getClass(LABEL_SYSTEM);
+        Assertions.assertTrue(clazz.isEmpty(), "Found a class although it should be an individual.");
+    }
+
+    @Test
+    @DisplayName("")
+    void getClassByIriTest() {
+        var clazz = ontologyConnector.getClassByIri(URI_E_CLASS);
+        Assertions.assertTrue(clazz.isPresent(), "Could not find class with Iri/Uri.");
+
+        clazz = ontologyConnector.getClassByIri(URI_NAMED_ELEMENT);
+        Assertions.assertTrue(clazz.isPresent(), "Could not find class with Iri/Uri.");
+
+        clazz = ontologyConnector.getClassByIri(NONEXISTENT);
+        Assertions.assertTrue(clazz.isEmpty(), "Found a class although it should be non-existent.");
     }
 
 }
