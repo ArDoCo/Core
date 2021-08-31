bfc0de689457934fbb3f9c65c5dd5c03cc94cce0
Jan Keim <jan.keim@kit.edu>
2021-08-31 09:40:01 +0200

    add new datastructures around MissingElementInconsistency
    added a Candidate-Class that holds recommended instances that might indicate an inconsistency regarding missing elements.

--- inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/MissingModelElementInconsistencyAgent.java
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/MissingModelElementInconsistencyAgent.java
@@ -1,13 +1,11 @@
 package edu.kit.kastel.mcse.ardoco.core.inconsistency.agents;
 
 import java.util.List;
-import java.util.Set;
 
 import org.eclipse.collections.api.factory.Lists;
-import org.eclipse.collections.api.factory.Maps;
+import org.eclipse.collections.api.factory.Sets;
 import org.eclipse.collections.api.list.MutableList;
-import org.eclipse.collections.api.map.MutableMap;
-import org.eclipse.collections.impl.set.mutable.UnifiedSet;
+import org.eclipse.collections.api.set.MutableSet;
 import org.kohsuke.MetaInfServices;
 
 import edu.kit.kastel.mcse.ardoco.core.datastructures.agents.Configuration;
@@ -19,6 +17,8 @@ import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IRecommendatio
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IRecommendedInstance;
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IText;
 import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.ITextState;
+import edu.kit.kastel.mcse.ardoco.core.inconsistency.datastructures.MissingElementInconsistencyCandidate;
+import edu.kit.kastel.mcse.ardoco.core.inconsistency.datastructures.MissingElementSupport;
 import edu.kit.kastel.mcse.ardoco.core.inconsistency.datastructures.MissingModelInstanceInconsistency;
 
 @MetaInfServices(InconsistencyAgent.class)
@@ -26,7 +26,7 @@ public class MissingModelElementInconsistencyAgent extends InconsistencyAgent {
     // TODO add some structure like "support" and filter inconsistency with low support (<2)
     // support is gained if one test identifies this as inconsistency. If another test confirms it, support is increased
 
-    private MutableMap<IRecommendedInstance, Set<SupportKind>> inconsistencyCandidates = Maps.mutable.empty();
+    private MutableSet<MissingElementInconsistencyCandidate> candidates = Sets.mutable.empty();
     private double minSupport = 1;
     private double threshold = 0.75d;
 
@@ -53,7 +53,7 @@ public class MissingModelElementInconsistencyAgent extends InconsistencyAgent {
 
         List<IRecommendedInstance> candidatesLikelyElements = findLikelyTextElementsWithNoTraceLinks(recommendedInstances);
         for (var candidate : candidatesLikelyElements) {
-            inconsistencyCandidates.computeIfAbsent(candidate, k -> new UnifiedSet<>()).add(SupportKind.TextElementWithNoTraceLink);
+            candidates.add(new MissingElementInconsistencyCandidate(candidate, MissingElementSupport.ELEMENT_WITH_NO_TRACE_LINK));
         }
 
         // TODO methods for other kinds of support
@@ -62,29 +62,23 @@ public class MissingModelElementInconsistencyAgent extends InconsistencyAgent {
     }
 
     private void createInconsistencies() {
-        for (var candidateEntry : inconsistencyCandidates.entrySet()) {
-            var candidate = candidateEntry.getKey();
-            var support = candidateEntry.getValue().size();
+        for (var candidate : candidates) {
+            var support = candidate.getAmountOfSupport();
             if (support >= minSupport) {
-                inconsistencyState.addInconsistency(new MissingModelInstanceInconsistency(candidate));
+                inconsistencyState.addInconsistency(new MissingModelInstanceInconsistency(candidate.getRecommendedInstance()));
             }
         }
     }
 
     private MutableList<IRecommendedInstance> findLikelyTextElementsWithNoTraceLinks(List<IRecommendedInstance> recommendedInstances) {
-        var<IRecommendedInstance> candidates = Lists.mutable.ofAll(recommendedInstances);
+        var<IRecommendedInstance> potentialCandidates = Lists.mutable.ofAll(recommendedInstances);
 
         // remove all recommended instances that were used in an instanceLink (trace link)
         for (var tracelink : connectionState.getInstanceLinks()) {
             var textualInstance = tracelink.getTextualInstance();
-            candidates.remove(textualInstance);
+            potentialCandidates.remove(textualInstance);
         }
 
-        return candidates.select(c -> c.getProbability() >= threshold);
-    }
-
-    private enum SupportKind {
-        TextElementWithNoTraceLink, DependencyWithTracedElement;
-        // TODO further
+        return potentialCandidates.select(c -> c.getProbability() >= threshold);
     }
 }
---
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/datastructures/MissingElementInconsistencyCandidate.java
@@ -0,0 +1,72 @@
+package edu.kit.kastel.mcse.ardoco.core.inconsistency.datastructures;
+
+import java.util.Objects;
+
+import org.eclipse.collections.api.factory.Sets;
+import org.eclipse.collections.api.set.MutableSet;
+
+import edu.kit.kastel.mcse.ardoco.core.datastructures.definitions.IRecommendedInstance;
+
+public class MissingElementInconsistencyCandidate {
+
+    private IRecommendedInstance recommendedInstance;
+    private MutableSet<MissingElementSupport> supports = Sets.mutable.empty();
+
+    public MissingElementInconsistencyCandidate(IRecommendedInstance recommendedInstance) {
+        super();
+        this.recommendedInstance = recommendedInstance;
+    }
+
+    public MissingElementInconsistencyCandidate(IRecommendedInstance recommendedInstance, MissingElementSupport support) {
+        super();
+        this.recommendedInstance = recommendedInstance;
+        supports.add(support);
+    }
+
+    /**
+     * Adds support for this candidate. If the support was already added before, it will not add it again.
+     *
+     * @param support the support that should be added
+     * @return true if this candidate did not already contain the specified support
+     */
+    public boolean addSupport(MissingElementSupport support) {
+        return supports.add(support);
+    }
+
+    /**
+     * Returns an integer to show how much support this candidate has
+     *
+     * @return the amount of support
+     */
+    public int getAmountOfSupport() {
+        return supports.size();
+    }
+
+    /**
+     * @return the recommendedInstance
+     */
+    public IRecommendedInstance getRecommendedInstance() {
+        return recommendedInstance;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recommendedInstance);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MissingElementInconsistencyCandidate other = (MissingElementInconsistencyCandidate) obj;
+        return Objects.equals(recommendedInstance, other.recommendedInstance);
+    }
+
+}
