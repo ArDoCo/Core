ed3cc92c1969b7af07efc2f4f4a5e7eb8638f0d7
Jan Keim <jan.keim@kit.edu>
2021-11-16 16:23:05 +0100

    refactorings

--- inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/datastructures/MissingElementSupport.java
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/datastructures/MissingElementSupport.java
@@ -2,5 +2,4 @@ package edu.kit.kastel.mcse.ardoco.core.inconsistency.datastructures;
 
 public enum MissingElementSupport {
     ELEMENT_WITH_NO_TRACE_LINK, TRACED_ELEMENT_IN_SAME_SENTENCE, DEPENDENCY_TO_TRACED_ELEMENT;
-    // TODO further
 }
