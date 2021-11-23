8c864f8e5dd57f5d405ad1be0c3de03390ffd970
Jan Keim <jan.keim@kit.edu>
2021-11-23 18:17:17 +0100

    add handling of typeMappings for Phrases
    Not fully done yet. Currently has no impact in our case studies

--- recommendation-generator/src/main/java/edu/kit/kastel/mcse/ardoco/core/recommendationgenerator/agents/PhraseRecommendationAgent.java
+++ recommendation-generator/src/main/java/edu/kit/kastel/mcse/ardoco/core/recommendationgenerator/agents/PhraseRecommendationAgent.java
@@ -1,15 +1,23 @@
 package edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.agents;
 
+import java.util.Set;
+
 import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Sets;
 import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.set.MutableSet;
 import org.kohsuke.MetaInfServices;
 
 import edu.kit.kastel.mcse.ardoco.core.common.Configuration;
+import edu.kit.kastel.mcse.ardoco.core.common.util.CommonUtilities;
+import edu.kit.kastel.mcse.ardoco.core.common.util.SimilarityUtils;
 import edu.kit.kastel.mcse.ardoco.core.model.IModelState;
 import edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.GenericRecommendationConfig;
 import edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.IRecommendationState;
 import edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.RecommendationAgent;
 import edu.kit.kastel.mcse.ardoco.core.text.IText;
+import edu.kit.kastel.mcse.ardoco.core.text.IWord;
 import edu.kit.kastel.mcse.ardoco.core.textextraction.INounMapping;
 import edu.kit.kastel.mcse.ardoco.core.textextraction.ITextState;
 
@@ -47,11 +55,56 @@ public class PhraseRecommendationAgent extends RecommendationAgent {
             // TODO Look if we can extract type information because type is within the compound but was filtered
             if (nounMapping.isPhrase()) {
                 var nounMappings = Lists.immutable.of(nounMapping);
-                ImmutableList<INounMapping> typeMappings = Lists.immutable.empty();
-                String type = "";
-                recommendationState.addRecommendedInstance(nounMapping.getReference(), type, CONFIDENCE, nounMappings, typeMappings);
+                var typeMappings = getRelatedTypeMappings(nounMapping);
+                var types = getSimilarModelTypes(typeMappings);
+                if (types.isEmpty()) {
+                    String type = "";
+                    recommendationState.addRecommendedInstance(nounMapping.getReference(), type, CONFIDENCE, nounMappings, typeMappings);
+                } else {
+                    for (var type : types) {
+                        recommendationState.addRecommendedInstance(nounMapping.getReference(), type, CONFIDENCE, nounMappings, typeMappings);
+                    }
+                }
             }
         }
+    }
+
+    private ImmutableList<String> getSimilarModelTypes(ImmutableList<INounMapping> typeMappings) {
+        MutableSet<String> similarModelTypes = Sets.mutable.empty();
+        Set<String> typeIdentifiers = CommonUtilities.getTypeIdentifiers(modelState);
+        for (var typeMapping : typeMappings) {
+            var currSimilarTypes = Lists.immutable
+                    .fromStream(typeIdentifiers.stream().filter(typeId -> SimilarityUtils.areWordsSimilar(typeId, typeMapping.getReference())));
+            similarModelTypes.addAll(currSimilarTypes.toList());
+            for (var word : typeMapping.getWords()) {
+                currSimilarTypes = Lists.immutable
+                        .fromStream(typeIdentifiers.stream().filter(typeId -> SimilarityUtils.areWordsSimilar(typeId, word.getLemma())));
+                similarModelTypes.addAll(currSimilarTypes.toList());
+            }
+        }
+        return similarModelTypes.toList().toImmutable();
+    }
+
+    private ImmutableList<INounMapping> getRelatedTypeMappings(INounMapping nounMapping) {
+        MutableList<INounMapping> typeMappings = Lists.mutable.empty();
+        // find TypeMappings that come from the Phrase Words within the Compound Word
+        var phrase = getPhraseWordsFromNounMapping(nounMapping);
+        for (var word : phrase) {
+            typeMappings.addAll(textState.getTypeMappingsByWord(word).toList());
+        }
+        // TODO find further TypeMappings around, if possible!
+        return typeMappings.toImmutable();
+    }
+
+    private static ImmutableList<IWord> getPhraseWordsFromNounMapping(INounMapping nounMapping) {
+        ImmutableList<IWord> phrase = Lists.immutable.empty();
+        for (var word : nounMapping.getWords()) {
+            var currPhrase = CommonUtilities.getCompoundPhrases(word);
+            if (currPhrase.size() > phrase.size()) {
+                phrase = currPhrase;
+            }
+        }
+        return phrase;
     }
 
 }
