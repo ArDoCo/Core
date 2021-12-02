6eed6684cf6ef4bad8c86d3ed77e9b2457a6d1c8
Jan Keim <jan.keim@kit.edu>
2021-12-02 11:15:05 +0100

    add locale for Inconsistency Eval output

--- tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/inconsistencies/eval/PRF1.java
+++ tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/inconsistencies/eval/PRF1.java
@@ -1,6 +1,8 @@
 /* Licensed under MIT 2021. */
 package edu.kit.kastel.mcse.ardoco.core.tests.inconsistencies.eval;
 
+import java.util.Locale;
+
 public class PRF1 implements EvaluationResult {
     private final double precision;
     private final double recall;
@@ -14,7 +16,7 @@ public class PRF1 implements EvaluationResult {
 
     @Override
     public String toString() {
-        return String.format("P: %.2f, R: %.2f, F1: %.2f", precision, recall, f1);
+        return String.format(Locale.US, "P: %.2f, R: %.2f, F1: %.2f", precision, recall, f1);
     }
 
     /**
