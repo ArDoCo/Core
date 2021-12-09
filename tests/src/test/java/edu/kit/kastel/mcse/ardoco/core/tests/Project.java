0ade7e853b3feccb58e2101df00fb64b851d2bb7
Jan Keim <jan.keim@kit.edu>
2021-12-09 17:27:35 +0100

    add bbb to eval with models, owls and in the enum

--- tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/Project.java
+++ tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/Project.java
@@ -39,7 +39,14 @@ public enum Project {
             "src/test/resources/teastore/teastore.txt", //
             "src/test/resources/teastore/goldstandard.csv", //
             new EvaluationResults(.94, .57, .71), //
-            new EvaluationResults(.94, .58, .72));
+            new EvaluationResults(.94, .58, .72)),
+    BIGBLUEBUTTON( //
+            "src/test/resources/bigbluebutton/bbb.owl", //
+            "src/test/resources/bigbluebutton/bbb_w_text.owl", //
+            "src/test/resources/bigbluebutton/bigbluebutton.txt", //
+            "src/test/resources/bigbluebutton/goldstandard.csv", //
+            new EvaluationResults(.0, .0, .0), //
+            new EvaluationResults(.0, .0, .0));
 
     private final String model;
     private final String textOntology;
