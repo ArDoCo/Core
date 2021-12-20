d229f992c0b1ccb048438d521f2d02132e4631fd
Jan Keim <jan.keim@kit.edu>
2021-12-20 15:33:42 +0100

    add Sentence class

---
+++ data-structures/src/main/java/edu/kit/kastel/mcse/ardoco/core/text/ISentence.java
@@ -0,0 +1,35 @@
+/* Licensed under MIT 2021. */
+package edu.kit.kastel.mcse.ardoco.core.text;
+
+import org.eclipse.collections.api.list.ImmutableList;
+
+/**
+ * Represents a sentence within the document.
+ *
+ * @author Jan Keim
+ *
+ */
+public interface ISentence {
+
+    /**
+     * Returns the sentence number
+     *
+     * @return the sentence number
+     */
+    int getSentenceNumber();
+
+    /**
+     * Returns the words contained by this sentence
+     *
+     * @return the words contained by this sentence
+     */
+    ImmutableList<IWord> getWords();
+
+    /**
+     * Returns the text of this sentence
+     *
+     * @return the text of this sentence
+     */
+    String getText();
+
+}
