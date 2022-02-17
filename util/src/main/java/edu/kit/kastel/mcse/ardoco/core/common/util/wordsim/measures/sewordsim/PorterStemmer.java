38e1d15f1a14ed1df11483c92582b7e4cb0ece2f
Kevin Werber <werberkevin@gmail.com>
2022-02-17 15:47:47 +0100

    Begin work on SEWordSimMeasure

---
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/sewordsim/PorterStemmer.java
@@ -0,0 +1,9 @@
+package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim;
+
+public class PorterStemmer {
+
+    public static String stem(String string) {
+        return string; // TODO: Implement a proper stemmer
+    }
+
+}
