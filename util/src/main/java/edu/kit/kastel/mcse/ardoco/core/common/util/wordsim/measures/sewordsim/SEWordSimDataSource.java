536564e7964b29a9d36f89fadcbffa9241d6d8bd
Kevin Werber <werberkevin@gmail.com>
2022-02-20 11:28:02 +0100

    Finish implementation of SEWordSimMeasure

--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/sewordsim/SEWordSimDataSource.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/sewordsim/SEWordSimDataSource.java
@@ -1,27 +1,55 @@
 package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim;
 
+import org.sqlite.SQLiteConfig;
+import org.sqlite.SQLiteOpenMode;
+
 import java.nio.file.Path;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Provides access to the SEWordSim sqlite database.
+ * Instances of this class keep an open connection to the sqlite file until {@link #close()} is called.
+ */
 public class SEWordSimDataSource implements AutoCloseable {
 
-    private static final String EXISTS_QUERY = "SELECT EXISTS(SELECT * FROM `Word_Similarity` WHERE `term_1` = ?) AS `ex`;";
-    private static final String SELECT_QUERY = "SELECT `similarity` FROM `Word_Similarity` WHERE `term_1` = ? AND `term_2` = ?;";
-    private static final String SELECT_ALL_QUERY = "SELECT DISTINCT `term_1` FROM `Word_Similarity`;";
+    private static final String EXISTS_QUERY = "SELECT EXISTS(SELECT * FROM `wsim` WHERE `term_1` = ?) AS `ex`;";
+    private static final String SELECT_QUERY = "SELECT `similarity` FROM `wsim` WHERE `term_1` = ? AND `term_2` = ?;";
+    private static final String SELECT_ALL_QUERY = "SELECT DISTINCT `term_1` FROM `wsim`;";
 
     private final Connection connection;
 
+    /**
+     * Construct a new {@link SEWordSimDataSource}.
+     * Once instantiated, a connection to the file will be kept open until {@link #close()} is called on this instance.
+     *
+     * @param sqliteFile the path to the sqlite database file
+     * @throws SQLException if connecting to the sqlite fails
+     */
     public SEWordSimDataSource(Path sqliteFile) throws SQLException {
         Objects.requireNonNull(sqliteFile);
-        this.connection = DriverManager.getConnection("jdbc:sqlite:" + sqliteFile);
+
+        var cfg = new SQLiteConfig();
+        cfg.setReadOnly(true);
+        cfg.setLockingMode(SQLiteConfig.LockingMode.EXCLUSIVE);
+        cfg.setJournalMode(SQLiteConfig.JournalMode.OFF);
+        cfg.setSynchronous(SQLiteConfig.SynchronousMode.OFF);
+        cfg.setOpenMode(SQLiteOpenMode.NOMUTEX);
+
+        this.connection = cfg.createConnection("jdbc:sqlite:" + sqliteFile);
     }
 
+    /**
+     * Checks whether the stemmed version of the given word is contained in the database.
+     *
+     * @param word the word to be checked
+     * @return {@code true} if the stemmed version of the given word is contained in the database
+     * @throws SQLException if a database access error occurs
+     */
     public boolean containsWord(String word) throws SQLException {
         word = PorterStemmer.stem(word);
 
@@ -38,6 +66,14 @@ public class SEWordSimDataSource implements AutoCloseable {
         return false;
     }
 
+    /**
+     * Attempts to retrieve the similarity score for the given pair of words.
+     *
+     * @param firstWord  the first word
+     * @param secondWord the second word
+     * @return the similarity score, ranging from {@code 0.0} to {@code 1.1}, or {@link Optional#empty()} if the database does not contain the given word pair
+     * @throws SQLException if a database access error occurs
+     */
     public Optional<Double> getSimilarity(String firstWord, String secondWord) throws SQLException {
         firstWord = PorterStemmer.stem(firstWord);
         secondWord = PorterStemmer.stem(secondWord);
@@ -57,6 +93,12 @@ public class SEWordSimDataSource implements AutoCloseable {
         return Optional.empty();
     }
 
+    /**
+     * Gets all words stored in the database.
+     *
+     * @return a list of all words stored in the database
+     * @throws SQLException if a database access error occurs
+     */
     public List<String> getAllWords() throws SQLException {
         var words = new ArrayList<String>();
 
@@ -72,8 +114,12 @@ public class SEWordSimDataSource implements AutoCloseable {
         return words;
     }
 
-    @Override
-    public void close() throws SQLException {
+    /**
+     * Closes the connection to the sqlite file.
+     *
+     * @throws SQLException if a database access error occurs
+     */
+    @Override public void close() throws SQLException {
         this.connection.close();
     }
 
--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/sewordsim/SEWordSimMeasure.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/sewordsim/SEWordSimMeasure.java
@@ -1,12 +1,58 @@
 package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim;
 
+import edu.kit.kastel.mcse.ardoco.core.common.util.CommonTextToolsConfig;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.ComparisonContext;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.WordSimMeasure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+import java.sql.SQLException;
+
+/**
+ * This word similarity measures utilizes the SEWordSim database from Tian et al. 2014
+ */
 public class SEWordSimMeasure implements WordSimMeasure {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SEWordSimMeasure.class);
+
+    private final SEWordSimDataSource dataSource;
+    private final double similarityThreshold;
+
+    /**
+     * Constructs a new {@link SEWordSimMeasure} using the settings provided by {@link CommonTextToolsConfig}.
+     *
+     * @throws SQLException if establishing the connection to the data source fails
+     */
+    public SEWordSimMeasure() throws SQLException {
+        this(new SEWordSimDataSource(Path.of(CommonTextToolsConfig.SEWORDSIM_DB_FILE_PATH)), CommonTextToolsConfig.SEWORDSIM_SIMILARITY_THRESHOLD);
+    }
+
+    /**
+     * Constructs a new {@link SEWordSimMeasure} instance.
+     *
+     * @param dataSource          the data source from which word similarities are loaded
+     * @param similarityThreshold the threshold above which words are considered similar
+     */
+    public SEWordSimMeasure(SEWordSimDataSource dataSource, double similarityThreshold) {
+        this.dataSource = dataSource;
+        this.similarityThreshold = similarityThreshold;
+    }
+
     @Override public boolean areWordsSimilar(ComparisonContext ctx) {
-        return false; // TODO
+        try {
+            double similarity = this.dataSource.getSimilarity(ctx.firstTerm(), ctx.secondTerm()).orElse(Double.NaN);
+
+            if (Double.isNaN(similarity)) {
+                return false; // words are probably missing in the database
+            }
+
+            return similarity >= this.similarityThreshold;
+
+        } catch (SQLException e) {
+            LOGGER.error("Failed to query the SEWordSim database for word comparison: " + ctx, e);
+            return false;
+        }
     }
 
 }
