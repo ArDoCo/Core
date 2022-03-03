543da8e70cbffa03cd9f7ae9baacc26d6ae68fd6
Kevin Werber <werberkevin@gmail.com>
2022-03-03 17:07:51 +0100

    Restructure & cleanup stats classes

---
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/stats/MeasureResult.java
@@ -0,0 +1,7 @@
+package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.stats;
+
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.WordSimMeasure;
+
+public record MeasureResult(WordSimMeasure measure,
+                            boolean accepted) {
+}
