859bc7294daf902c9487f1ab53d100a2c96e920e
Kevin Werber <werberkevin@gmail.com>
2022-03-12 20:11:58 +0100

    Clean up fastText classes

--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/fastText/FastTextMeasure.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/fastText/FastTextMeasure.java
@@ -4,17 +4,13 @@ package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.fastText;
 import edu.kit.kastel.mcse.ardoco.core.common.util.CommonTextToolsConfig;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.ComparisonContext;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.WordSimMeasure;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A word similarity measure based on the fastText neural network.
  */
 public class FastTextMeasure implements WordSimMeasure {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FastTextMeasure.class);
-
-    private final FastTextDataSource dataSource;
+    private final DL4JFastTextDataSource dataSource;
     private final double similarityThreshold;
 
     /**
@@ -22,33 +18,29 @@ public class FastTextMeasure implements WordSimMeasure {
      *
      * @param dataSource the data source to use for word similarity computation
      */
-    public FastTextMeasure(FastTextDataSource dataSource) {
+    public FastTextMeasure(DL4JFastTextDataSource dataSource) {
         this(dataSource, CommonTextToolsConfig.FASTTEXT_SIMILARITY_THRESHOLD);
     }
 
     /**
      * Constructs a new {@link FastTextMeasure} instance.
      *
+     * @param dataSource the data source to use for word similarity computation
      * @param similarityThreshold the threshold above which words are considered similar
      */
-    public FastTextMeasure(FastTextDataSource dataSource, double similarityThreshold) {
+    public FastTextMeasure(DL4JFastTextDataSource dataSource, double similarityThreshold) {
         this.dataSource = dataSource;
         this.similarityThreshold = similarityThreshold;
     }
 
     @Override public boolean areWordsSimilar(ComparisonContext ctx) {
-        try {
-            double similarity = this.dataSource.getSimilarity(ctx.firstTerm(), ctx.secondTerm()).orElse(Double.NaN);
+        double similarity = this.dataSource.getSimilarity(ctx.firstTerm(), ctx.secondTerm()).orElse(Double.NaN);
 
-            if (Double.isNaN(similarity)) {
-                return false;
-            }
-
-            return similarity >= this.similarityThreshold;
-        } catch (RetrieveVectorException e) {
-            LOGGER.error("Failed to query the fastText data source for word comparison: " + ctx, e);
+        if (Double.isNaN(similarity)) {
             return false;
         }
+
+        return similarity >= this.similarityThreshold;
     }
 
 }
---
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/vector/VectorUtils.java
@@ -0,0 +1,121 @@
+package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.vector;
+
+import java.util.Objects;
+
+/**
+ * Some utility functions for double vectors.
+ */
+public class VectorUtils {
+
+    /**
+     * Calculates the cosine similarity between the two given vectors.
+     *
+     * @param firstVec  the first vector
+     * @param secondVec the second vector
+     * @return the cosine similarity
+     * @throws IllegalArgumentException if the vectors have different lengths
+     */
+    public static double cosineSimilarity(double[] firstVec, double[] secondVec) {
+        Objects.requireNonNull(firstVec);
+        Objects.requireNonNull(secondVec);
+
+        if (firstVec.length != secondVec.length) {
+            throw new IllegalArgumentException("vector length does not match!");
+        }
+
+        double dotProduct = 0.0, firstNorm = 0.0, secondNorm = 0.0;
+
+        for (int i = 0; i < firstVec.length; i++) {
+            dotProduct += firstVec[i] * secondVec[i];
+            firstNorm += Math.pow(firstVec[i], 2);
+            secondNorm += Math.pow(secondVec[i], 2);
+        }
+
+        return dotProduct / (Math.sqrt(firstNorm) * Math.sqrt(secondNorm));
+    }
+
+    /**
+     * Calculates the cosine similarity between the two given vectors.
+     * The two given vectors must have the same length.
+     *
+     * @param firstVec  the first vector
+     * @param secondVec the second vector
+     * @return the cosine similarity
+     * @throws IllegalArgumentException if the vectors have different lengths
+     */
+    public static double cosineSimilarity(float[] firstVec, float[] secondVec) {
+        Objects.requireNonNull(firstVec);
+        Objects.requireNonNull(secondVec);
+
+        if (firstVec.length != secondVec.length) {
+            throw new IllegalArgumentException("vector length does not match!");
+        }
+
+        double dotProduct = 0.0, firstNorm = 0.0, secondNorm = 0.0;
+
+        for (int i = 0; i < firstVec.length; i++) {
+            dotProduct += firstVec[i] * secondVec[i];
+            firstNorm += Math.pow(firstVec[i], 2);
+            secondNorm += Math.pow(secondVec[i], 2);
+        }
+
+        return dotProduct / (Math.sqrt(firstNorm) * Math.sqrt(secondNorm));
+    }
+
+    /**
+     * Checks whether the given vector contains any non-zero numbers.
+     *
+     * @param vector the vector to check
+     * @return {@code true} if the given vector either has no entries or if the only entries are zero
+     */
+    public static boolean isZero(double[] vector) {
+        Objects.requireNonNull(vector);
+
+        if (vector.length <= 0) {
+            return true;
+        }
+
+        for (double entry : vector) {
+            if (entry != 0.0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Adds the given vectors together and stores the resulting vector in the first argument.
+     * The two given vectors must have the same length.
+     *
+     * @param result the first vector that will also be the result vector after the addition
+     * @param toAdd  the second vector
+     */
+    public static void add(double[] result, double[] toAdd) {
+        Objects.requireNonNull(result);
+        Objects.requireNonNull(toAdd);
+
+        if (result.length != toAdd.length) {
+            throw new IllegalArgumentException("vectors of different lengths: " + result.length + " != " + toAdd.length);
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] += toAdd[i];
+        }
+    }
+
+    /**
+     * Scales the given vector by the given scalar.
+     *
+     * @param vector the vector to scale
+     * @param scalar the scalar
+     */
+    public static void scale(double[] vector, double scalar) {
+        Objects.requireNonNull(scalar);
+
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = vector[i] * scalar;
+        }
+    }
+
+}
