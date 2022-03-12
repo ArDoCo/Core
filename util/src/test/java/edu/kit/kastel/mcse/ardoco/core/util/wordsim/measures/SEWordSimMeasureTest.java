34f1abbcc1895e8259914c836abf1f52f8bcb8e3
Kevin Werber <werberkevin@gmail.com>
2022-03-12 20:45:31 +0100

    Add some wsm tests

---
+++ util/src/test/java/edu/kit/kastel/mcse/ardoco/core/util/wordsim/measures/SEWordSimMeasureTest.java
@@ -0,0 +1,10 @@
+package edu.kit.kastel.mcse.ardoco.core.util.wordsim.measures;
+
+/**
+ * Tests for the {@link edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim.SEWordSimMeasure}
+ */
+public class SEWordSimMeasureTest {
+
+    // TODO
+
+}
