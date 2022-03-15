32706e20eb4ff9b5dd0a70cfba098aab540988ec
Kevin Werber <werberkevin@gmail.com>
2022-03-15 11:19:56 +0100

    Replace maven evaluator with custom evaluator

---
+++ tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/integration/tracelinks/eval/n/Evaluator.java
@@ -0,0 +1,115 @@
+package edu.kit.kastel.mcse.ardoco.core.tests.integration.tracelinks.eval.n;
+
+import edu.kit.kastel.mcse.ardoco.core.common.util.CommonTextToolsConfig;
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.jarowinkler.JaroWinklerMeasure;
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.ngram.NgramMeasure;
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim.SEWordSimDataSource;
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim.SEWordSimMeasure;
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.wordnet.WordNetMeasure;
+import edu.mit.jwi.IRAMDictionary;
+import edu.mit.jwi.RAMDictionary;
+import edu.mit.jwi.data.ILoadPolicy;
+import edu.uniba.di.lacam.kdde.lexical_db.ILexicalDatabase;
+import edu.uniba.di.lacam.kdde.lexical_db.MITWordNet;
+import edu.uniba.di.lacam.kdde.ws4j.RelatednessCalculator;
+import edu.uniba.di.lacam.kdde.ws4j.similarity.JiangConrath;
+import edu.uniba.di.lacam.kdde.ws4j.similarity.LeacockChodorow;
+import edu.uniba.di.lacam.kdde.ws4j.similarity.Lesk;
+import edu.uniba.di.lacam.kdde.ws4j.similarity.WuPalmer;
+import edu.uniba.di.lacam.kdde.ws4j.util.WS4JConfiguration;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Evaluator {
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var plans = getPlans();
+        var resultDir = Path.of("eval_results");
+        var evaluator = new MultiPlanEvaluator(plans, resultDir);
+
+        evaluator.execute();
+    }
+
+    public static List<EvalPlan> getPlans() throws IOException, SQLException {
+        var plans = new ArrayList<EvalPlan>();
+
+        boolean jaroWinkler = false;
+        boolean levenshtein = false;
+        boolean ngram = false;
+        boolean sewordsim = false;
+        boolean wordNetWP = false, wordNetLC = true, wordNetJC = true, wordNetLesk = true, wordNetEzzikouri = false;
+
+        for (int b = 1; b <= 2; b++) {
+            for (int t = 10; t <= 90; t += 10) {
+                double threshold = t / 100.0;
+
+                // Jaro Winkler
+                if (jaroWinkler && b == 1) {
+                    plans.add(new EvalPlan("jaroWinkler", b, t, new JaroWinklerMeasure(threshold)));
+                }
+
+                // Levenshtein
+                if (levenshtein) {
+                    throw new UnsupportedOperationException();
+                }
+
+                // Ngram
+                if (ngram) {
+                    plans.add(new EvalPlan("bigram", b, t, new NgramMeasure(NgramMeasure.Variant.LUCENE, 2, threshold)));
+                    //plans.add(new EvalPlan("trigram", b, t, new NgramMeasure(NgramMeasure.Variant.LUCENE, 3, threshold)));
+                }
+
+                // SEWordSim
+                if (sewordsim) {
+                    var dataSource = new SEWordSimDataSource(Path.of(CommonTextToolsConfig.SEWORDSIM_DB_FILE_PATH));
+                    plans.add(new EvalPlan("sewsim", b, t, new SEWordSimMeasure(dataSource, threshold)));
+                }
+
+                // WordNet
+                if (wordNetWP || wordNetLC || wordNetJC || wordNetLesk || wordNetEzzikouri) {
+                    WS4JConfiguration.getInstance().setCache(true);
+                    WS4JConfiguration.getInstance().setStem(true);
+
+                    Path wordNetDirPath = Path.of(CommonTextToolsConfig.WORDNET_DATA_DIR_PATH);
+                    IRAMDictionary dictionary = new RAMDictionary(wordNetDirPath.toFile(), ILoadPolicy.BACKGROUND_LOAD);
+                    dictionary.open();
+
+                    ILexicalDatabase db = new MITWordNet(dictionary);
+
+                    String group = "wordNet_";
+                    var calcThresholdMap = new HashMap<RelatednessCalculator, Double>();
+
+                    if (wordNetWP) {
+                        group += "WP";
+                        calcThresholdMap.put(new WuPalmer(db), threshold);
+                    }
+                    if (wordNetLC) {
+                        group += "LC";
+                        calcThresholdMap.put(new LeacockChodorow(db), threshold);
+                    }
+                    if (wordNetJC) {
+                        group += "JC";
+                        calcThresholdMap.put(new JiangConrath(db), threshold);
+                    }
+                    if (wordNetLesk) {
+                        group += "Lesk";
+                        calcThresholdMap.put(new Lesk(db), threshold);
+                    }
+                    if (wordNetEzzikouri) {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    plans.add(new EvalPlan(group, b, t, new WordNetMeasure(calcThresholdMap)));
+                }
+            }
+        }
+
+        return plans;
+    }
+
+}
---
+++ tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/integration/tracelinks/eval/n/MultiPlanEvaluator.java
@@ -0,0 +1,103 @@
+package edu.kit.kastel.mcse.ardoco.core.tests.integration.tracelinks.eval.n;
+
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.NewSimilarityUtils;
+import edu.kit.kastel.mcse.ardoco.core.pipeline.Pipeline;
+import edu.kit.kastel.mcse.ardoco.core.tests.EvaluationResults;
+import edu.kit.kastel.mcse.ardoco.core.tests.Project;
+import edu.kit.kastel.mcse.ardoco.core.tests.integration.tracelinks.eval.TLProjectEvalResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+/**
+ * Evaluates multiple {@link EvalPlan} instances one after another.
+ * Saves evaluation results to the disk.
+ */
+public class MultiPlanEvaluator {
+
+    private final List<EvalPlan> plans;
+    private final Path resultDir;
+
+    /**
+     * Constructs a new {@link MultiPlanEvaluator} instance.
+     * @param plans the plans to evaluate
+     * @param resultDir in which directory to store the results
+     */
+    public MultiPlanEvaluator(List<EvalPlan> plans, Path resultDir) {
+        this.plans = plans;
+        this.resultDir = resultDir;
+    }
+
+    /**
+     * Executes the evaluation process.
+     * @throws IOException if the evaluation encounters an exception
+     */
+    public void execute() throws IOException {
+        Files.createDirectories(this.resultDir);
+
+        long totalStart = System.currentTimeMillis();
+
+        for (EvalPlan plan : plans) {
+            System.out.printf("\nEvaluating: %s\n\n", plan.getId());
+
+            long evalStart = System.currentTimeMillis();
+
+            var results = evaluatePlan(plan);
+
+            long evalDuration = System.currentTimeMillis() - evalStart;
+
+            System.out.printf("\nFinished %s ✔ (took %s seconds)\n\n", plan.getId(), evalDuration / 1000);
+
+            saveResults(plan, results);
+        }
+
+        long totalDuration = System.currentTimeMillis() - totalStart;
+
+        System.out.printf("\nEntire multi plan evaluation took %s seconds\n", totalDuration / 1000);
+    }
+
+    private EvaluationResults evaluatePlan(EvalPlan plan) throws IOException {
+        var results = new EvaluationResults(0.0, 0.0, 0.0);
+
+        NewSimilarityUtils.setMeasures(plan.getMeasures());
+
+        for (Project project : Project.values()) {
+            File modelFile = project.getTextOntologyFile();
+
+            var data = Pipeline.run("test_" + project.name(), null, modelFile, null);
+
+            var projectResult = new TLProjectEvalResult(project, data);
+
+            results.f1 += projectResult.getF1();
+            results.precision += projectResult.getPrecision();
+            results.recall += projectResult.getRecall();
+
+            System.gc();
+        }
+
+        results.f1 /= Project.values().length;
+        results.precision /= Project.values().length;
+        results.recall /= Project.values().length;
+
+        return results;
+    }
+
+    private void saveResults(EvalPlan plan, EvaluationResults results) throws IOException {
+        var filePrefix = plan.getGroup() + "_b" + plan.getBase();
+
+        var f1File = resultDir.resolve(filePrefix + "_f1.dat");
+        var precisionFile = resultDir.resolve(filePrefix + "_precision.dat");
+        var recallFile = resultDir.resolve(filePrefix + "_recall.dat");
+
+        Files.writeString(f1File, plan.getThreshold() + " " + ((int) (results.getF1() * 100)) + "\n", CREATE, APPEND);
+        Files.writeString(precisionFile, plan.getThreshold() + " " + ((int) (results.getPrecision() * 100)) + "\n", CREATE, APPEND);
+        Files.writeString(recallFile, plan.getThreshold() + " " + ((int) (results.getRecall() * 100)) + "\n", CREATE, APPEND);
+    }
+
+}
