858de63d341a4370e573103f4fb8883d34a697b3
Jan Keim <jan.keim@kit.edu>
2022-03-17 14:30:20 +0100

    add simple example filter check
    Filter if probability for name or type is too low

--- inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/InitialInconsistencyAgent.java
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/InitialInconsistencyAgent.java
@@ -12,11 +12,14 @@ import edu.kit.kastel.mcse.ardoco.core.model.IModelState;
 import edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.IRecommendationState;
 import edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.IRecommendedInstance;
 import edu.kit.kastel.mcse.ardoco.core.text.IText;
+import edu.kit.kastel.mcse.ardoco.core.textextraction.INounMapping;
 import edu.kit.kastel.mcse.ardoco.core.textextraction.ITextState;
 
 @MetaInfServices(InconsistencyAgent.class)
 public class InitialInconsistencyAgent extends InconsistencyAgent {
-    private double threshold = 0.75d;
+    private static final double THRESHOLD_NAME_PROBABILITY = 0.5;
+    private static final double THRESHOLD_TYPE_PROBABILITY = 0.9;
+    private static final double THRESHOLD_RI_PROBABILITY = 0.75d;
 
     public InitialInconsistencyAgent() {
         super(GenericInconsistencyConfig.class);
@@ -46,23 +49,46 @@ public class InitialInconsistencyAgent extends InconsistencyAgent {
     private void filterRecommendedInstances() {
         var filteredRecommendedInstances = Lists.mutable.<IRecommendedInstance> empty();
         for (var recommendedInstance : this.inconsistencyState.getRecommendedInstances()) {
-            if (recommendedInstance.getProbability() > threshold && checkOne(recommendedInstance) && checkTwo(recommendedInstance)) {
+            if (performHeuristicsAndChecks(recommendedInstance)) {
                 filteredRecommendedInstances.add(recommendedInstance);
             }
         }
 
+        System.out.println(this.inconsistencyState.getRecommendedInstances().size());
         this.inconsistencyState.setRecommendedInstances(filteredRecommendedInstances);
+        System.out.println(this.inconsistencyState.getRecommendedInstances().size());
+    }
+
+    private boolean performHeuristicsAndChecks(IRecommendedInstance recommendedInstance) {
+        var checksArePositive = checkProbabilityOfBeingRecommendedInstance(recommendedInstance);
+        checksArePositive = checksArePositive && checkProbabilitiesForNounMappingTypes(recommendedInstance);
+        return checksArePositive && checkTwo(recommendedInstance);
+    }
+
+    private boolean checkProbabilityOfBeingRecommendedInstance(IRecommendedInstance recommendedInstance) {
+        return recommendedInstance.getProbability() > THRESHOLD_RI_PROBABILITY;
     }
 
     /**
-     * Check for ???
+     * Check for probabilities of the types for the {@link INounMapping}s that are contained by the
+     * {@link IRecommendedInstance}. If they exceed a threshold, then the check is positive
      *
      * @param recommendedInstance the {@link IRecommendedInstance} to check
-     * @return
+     * @return true if the probabilities of the types exceed a threshold
      */
-    private boolean checkOne(IRecommendedInstance recommendedInstance) {
-        // TODO
-        return true;
+    private boolean checkProbabilitiesForNounMappingTypes(IRecommendedInstance recommendedInstance) {
+        for (var type : recommendedInstance.getTypeMappings()) {
+            if (type.getProbabilityForType() > THRESHOLD_TYPE_PROBABILITY) {
+                return true;
+            }
+        }
+
+        for (var name : recommendedInstance.getNameMappings()) {
+            if (name.getProbabilityForName() > THRESHOLD_NAME_PROBABILITY) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
