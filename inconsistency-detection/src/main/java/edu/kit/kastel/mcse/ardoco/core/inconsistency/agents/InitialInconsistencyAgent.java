5e3c6adb96347609f36315c8bd90acc1ac9da1c2
Jan Keim <jan.keim@kit.edu>
2022-03-17 14:08:54 +0100

    add possibility to filter RecommendedInstances for Inconsistency
    In the InitialInconsistencyAgent the recommendedInstances from the Rec.State are copied to Incons.state. Then, filtering is done. (TODO: good heuristics/fulter rules).

--- inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/InitialInconsistencyAgent.java
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/InitialInconsistencyAgent.java
@@ -1,6 +1,7 @@
 /* Licensed under MIT 2021. */
 package edu.kit.kastel.mcse.ardoco.core.inconsistency.agents;
 
+import org.eclipse.collections.api.factory.Lists;
 import org.kohsuke.MetaInfServices;
 
 import edu.kit.kastel.mcse.ardoco.core.common.Configuration;
@@ -9,11 +10,13 @@ import edu.kit.kastel.mcse.ardoco.core.inconsistency.IInconsistencyState;
 import edu.kit.kastel.mcse.ardoco.core.inconsistency.InconsistencyAgent;
 import edu.kit.kastel.mcse.ardoco.core.model.IModelState;
 import edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.IRecommendationState;
+import edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.IRecommendedInstance;
 import edu.kit.kastel.mcse.ardoco.core.text.IText;
 import edu.kit.kastel.mcse.ardoco.core.textextraction.ITextState;
 
 @MetaInfServices(InconsistencyAgent.class)
 public class InitialInconsistencyAgent extends InconsistencyAgent {
+    private double threshold = 0.75d;
 
     public InitialInconsistencyAgent() {
         super(GenericInconsistencyConfig.class);
@@ -32,7 +35,45 @@ public class InitialInconsistencyAgent extends InconsistencyAgent {
 
     @Override
     public void exec() {
+        this.inconsistencyState.addRecommendedInstances(this.recommendationState.getRecommendedInstances().toList());
+
+        filterRecommendedInstances();
+    }
+
+    /**
+     * Filter RecommendedInstances based on various heuristics. First, filter unlikely ones (low probability).
+     */
+    private void filterRecommendedInstances() {
+        var filteredRecommendedInstances = Lists.mutable.<IRecommendedInstance> empty();
+        for (var recommendedInstance : this.inconsistencyState.getRecommendedInstances()) {
+            if (recommendedInstance.getProbability() > threshold && checkOne(recommendedInstance) && checkTwo(recommendedInstance)) {
+                filteredRecommendedInstances.add(recommendedInstance);
+            }
+        }
+
+        this.inconsistencyState.setRecommendedInstances(filteredRecommendedInstances);
+    }
+
+    /**
+     * Check for ???
+     *
+     * @param recommendedInstance the {@link IRecommendedInstance} to check
+     * @return
+     */
+    private boolean checkOne(IRecommendedInstance recommendedInstance) {
         // TODO
+        return true;
+    }
+
+    /**
+     * Check for ???
+     *
+     * @param recommendedInstance the {@link IRecommendedInstance} to check
+     * @return
+     */
+    private boolean checkTwo(IRecommendedInstance recommendedInstance) {
+        // TODO
+        return true;
     }
 
 }
