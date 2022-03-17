e97c3257589a57366445c7f174b7ca05c0535694
Kevin Werber <werberkevin@gmail.com>
2022-03-17 10:53:10 +0100

    Load data sources outside of threshold loop for Evaluation

--- tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/integration/tracelinks/eval/Evaluation.java
+++ tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/integration/tracelinks/eval/Evaluation.java
@@ -2,10 +2,13 @@
 package edu.kit.kastel.mcse.ardoco.core.tests.integration.tracelinks.eval;
 
 import edu.kit.kastel.mcse.ardoco.core.common.util.CommonTextToolsConfig;
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.fastText.DL4JFastTextDataSource;
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.fastText.FastTextMeasure;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.jarowinkler.JaroWinklerMeasure;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.ngram.NgramMeasure;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim.SEWordSimDataSource;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim.SEWordSimMeasure;
+import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.wordnet.Ezzikouri;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.wordnet.WordNetMeasure;
 import edu.mit.jwi.IRAMDictionary;
 import edu.mit.jwi.RAMDictionary;
@@ -18,6 +21,8 @@ import edu.uniba.di.lacam.kdde.ws4j.similarity.LeacockChodorow;
 import edu.uniba.di.lacam.kdde.ws4j.similarity.Lesk;
 import edu.uniba.di.lacam.kdde.ws4j.similarity.WuPalmer;
 import edu.uniba.di.lacam.kdde.ws4j.util.WS4JConfiguration;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -28,7 +33,9 @@ import java.util.List;
 
 public class Evaluation {
 
-    public static void main(String[] args) throws IOException, SQLException {
+    @Test
+    @Disabled
+    public void run() throws IOException, SQLException {
         var plans = getPlans();
         var resultDir = Path.of("eval_results");
         var evaluator = new Evaluator(plans, resultDir);
@@ -43,10 +50,31 @@ public class Evaluation {
         boolean levenshtein = false;
         boolean ngram = false;
         boolean sewordsim = false;
-        boolean wordNetWP = false, wordNetLC = true, wordNetJC = true, wordNetLesk = true, wordNetEzzikouri = false;
+        boolean fastText = false;
+        boolean wordNetWP = false, wordNetLC = false, wordNetJC = false, wordNetLesk = false, wordNetEzzikouri = true;
+
+        String fastTextModelName = "";
+        DL4JFastTextDataSource fastTextDataSource = null;
+        if (fastText) {
+            var modelPath = Path.of(CommonTextToolsConfig.FASTTEXT_MODEL_FILE_PATH);
+            fastTextModelName = modelPath.getFileName().toString().replace(".bin", "");
+            fastTextDataSource = new DL4JFastTextDataSource(modelPath);
+        }
+
+        ILexicalDatabase wordNetDB = null;
+        if (wordNetWP || wordNetLC || wordNetJC || wordNetLesk || wordNetEzzikouri) {
+            WS4JConfiguration.getInstance().setCache(true);
+            WS4JConfiguration.getInstance().setStem(true);
+
+            Path wordNetDirPath = Path.of(CommonTextToolsConfig.WORDNET_DATA_DIR_PATH);
+            IRAMDictionary dictionary = new RAMDictionary(wordNetDirPath.toFile(), ILoadPolicy.BACKGROUND_LOAD);
+            dictionary.open();
+
+            wordNetDB = new MITWordNet(dictionary);
+        }
 
         for (int b = 1; b <= 2; b++) {
-            for (int t = 10; t <= 90; t += 10) {
+            for (int t = 0; t <= 100; t += 5) {
                 double threshold = t / 100.0;
 
                 // Jaro Winkler
@@ -61,9 +89,8 @@ public class Evaluation {
 
                 // Ngram
                 if (ngram) {
-                    plans.add(new EvalPlan("bigram", b, t, new NgramMeasure(NgramMeasure.Variant.LUCENE, 2, threshold)));
-                    // plans.add(new EvalPlan("trigram", b, t, new NgramMeasure(NgramMeasure.Variant.LUCENE, 3,
-                    // threshold)));
+                    //plans.add(new EvalPlan("bigram", b, t, new NgramMeasure(NgramMeasure.Variant.LUCENE, 2, threshold)));
+                    plans.add(new EvalPlan("trigram", b, t, new NgramMeasure(NgramMeasure.Variant.LUCENE, 3, threshold)));
                 }
 
                 // SEWordSim
@@ -72,38 +99,35 @@ public class Evaluation {
                     plans.add(new EvalPlan("sewsim", b, t, new SEWordSimMeasure(dataSource, threshold)));
                 }
 
+                // fastText
+                if (fastText) {
+                    plans.add(new EvalPlan("fastText_" + fastTextModelName, b, t, new FastTextMeasure(fastTextDataSource, threshold)));
+                }
+
                 // WordNet
                 if (wordNetWP || wordNetLC || wordNetJC || wordNetLesk || wordNetEzzikouri) {
-                    WS4JConfiguration.getInstance().setCache(true);
-                    WS4JConfiguration.getInstance().setStem(true);
-
-                    Path wordNetDirPath = Path.of(CommonTextToolsConfig.WORDNET_DATA_DIR_PATH);
-                    IRAMDictionary dictionary = new RAMDictionary(wordNetDirPath.toFile(), ILoadPolicy.BACKGROUND_LOAD);
-                    dictionary.open();
-
-                    ILexicalDatabase db = new MITWordNet(dictionary);
-
                     String group = "wordNet_";
                     var calcThresholdMap = new HashMap<RelatednessCalculator, Double>();
 
                     if (wordNetWP) {
                         group += "WP";
-                        calcThresholdMap.put(new WuPalmer(db), threshold);
+                        calcThresholdMap.put(new WuPalmer(wordNetDB), threshold);
                     }
                     if (wordNetLC) {
                         group += "LC";
-                        calcThresholdMap.put(new LeacockChodorow(db), threshold);
+                        calcThresholdMap.put(new LeacockChodorow(wordNetDB), threshold);
                     }
                     if (wordNetJC) {
                         group += "JC";
-                        calcThresholdMap.put(new JiangConrath(db), threshold);
+                        calcThresholdMap.put(new JiangConrath(wordNetDB), threshold);
                     }
                     if (wordNetLesk) {
                         group += "Lesk";
-                        calcThresholdMap.put(new Lesk(db), threshold);
+                        calcThresholdMap.put(new Lesk(wordNetDB), threshold);
                     }
                     if (wordNetEzzikouri) {
-                        throw new UnsupportedOperationException();
+                        group += "Ezzi";
+                        calcThresholdMap.put(new Ezzikouri(wordNetDB), threshold);
                     }
 
                     plans.add(new EvalPlan(group, b, t, new WordNetMeasure(calcThresholdMap)));
