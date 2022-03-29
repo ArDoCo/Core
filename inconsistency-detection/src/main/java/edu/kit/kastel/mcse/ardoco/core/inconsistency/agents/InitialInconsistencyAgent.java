fedcd929e19e75bb980ec6ab2437343972b3842b
Jan Keim <jan.keim@kit.edu>
2022-03-29 19:03:00 +0200

    playing around with probabilities for RIs

--- inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/InitialInconsistencyAgent.java
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/agents/InitialInconsistencyAgent.java
@@ -3,7 +3,10 @@ package edu.kit.kastel.mcse.ardoco.core.inconsistency.agents;
 
 import java.util.Map;
 
+import org.eclipse.collections.api.bag.sorted.MutableSortedBag;
 import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.SortedBags;
+import org.eclipse.collections.api.list.MutableList;
 
 import edu.kit.kastel.mcse.ardoco.core.api.agent.InconsistencyAgent;
 import edu.kit.kastel.mcse.ardoco.core.api.agent.InconsistencyAgentData;
@@ -14,9 +17,13 @@ import edu.kit.kastel.mcse.ardoco.core.api.data.textextraction.INounMapping;
 public class InitialInconsistencyAgent extends InconsistencyAgent {
     private static final double THRESHOLD_NAME_PROBABILITY = 0.5;
     private static final double THRESHOLD_TYPE_PROBABILITY = 0.9;
-    private static final double THRESHOLD_RI_PROBABILITY = 0.75d;
+
+    private double threshold = 0.6d;
+
+    private MutableSortedBag<Double> probabilities = SortedBags.mutable.empty();
 
     public InitialInconsistencyAgent() {
+        // empty
     }
 
     @Override
@@ -34,13 +41,39 @@ public class InitialInconsistencyAgent extends InconsistencyAgent {
      */
     private void filterRecommendedInstances(IInconsistencyState inconsistencyState) {
         var filteredRecommendedInstances = Lists.mutable.<IRecommendedInstance> empty();
-        for (var recommendedInstance : inconsistencyState.getRecommendedInstances()) {
+        var recommendedInstances = inconsistencyState.getRecommendedInstances();
+
+        threshold = analyzeProbabilitiesofRecommendedInstances(recommendedInstances);
+
+        for (var recommendedInstance : recommendedInstances) {
             if (performHeuristicsAndChecks(recommendedInstance)) {
                 filteredRecommendedInstances.add(recommendedInstance);
             }
         }
 
         inconsistencyState.setRecommendedInstances(filteredRecommendedInstances);
+    }
+
+    private double analyzeProbabilitiesofRecommendedInstances(MutableList<IRecommendedInstance> recommendedInstances) {
+        var highestProbability = 0.0d;
+        for (var recommendedInstance : recommendedInstances) {
+            var probability = recommendedInstance.getProbability();
+            if (probability > highestProbability) {
+                highestProbability = probability;
+            }
+            if (logger.isDebugEnabled()) {
+                var roundedProbability = Math.round(probability * 10.0) / 10.0;
+                this.probabilities.addOccurrences(roundedProbability, 1);
+            }
+        }
+
+        if (logger.isDebugEnabled()) {
+            for (var prob : probabilities.toMapOfItemToCount().entrySet()) {
+                logger.debug(prob.getKey() + ": " + prob.getValue());
+            }
+        }
+
+        return highestProbability * 0.8;
     }
 
     private boolean performHeuristicsAndChecks(IRecommendedInstance recommendedInstance) {
@@ -50,7 +83,8 @@ public class InitialInconsistencyAgent extends InconsistencyAgent {
     }
 
     private boolean checkProbabilityOfBeingRecommendedInstance(IRecommendedInstance recommendedInstance) {
-        return recommendedInstance.getProbability() > 0.0;
+        var probability = recommendedInstance.getProbability();
+        return probability > threshold;
     }
 
     /**
@@ -91,5 +125,6 @@ public class InitialInconsistencyAgent extends InconsistencyAgent {
 
     @Override
     protected void delegateApplyConfigurationToInternalObjects(Map<String, String> additionalConfiguration) {
+        // handle additional config
     }
 }
