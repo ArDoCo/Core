496f155bd5cfa5fccf129c92da8bb2a13efacbec
Jan Keim <jan.keim@kit.edu>
2022-03-31 17:14:47 +0200

    update OccasionFilter

--- inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/filters/OccasionFilter.java
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/filters/OccasionFilter.java
@@ -1,20 +1,29 @@
 /* Licensed under MIT 2022. */
 package edu.kit.kastel.mcse.ardoco.core.inconsistency.filters;
 
+import java.util.Comparator;
 import java.util.Map;
 
 import org.eclipse.collections.api.factory.Lists;
 
 import edu.kit.kastel.mcse.ardoco.core.api.agent.AbstractFilter;
 import edu.kit.kastel.mcse.ardoco.core.api.agent.InconsistencyAgentData;
+import edu.kit.kastel.mcse.ardoco.core.api.common.Configurable;
 import edu.kit.kastel.mcse.ardoco.core.api.data.inconsistency.IInconsistencyState;
 import edu.kit.kastel.mcse.ardoco.core.api.data.recommendationgenerator.IRecommendedInstance;
+import edu.kit.kastel.mcse.ardoco.core.api.data.text.IWord;
+import edu.kit.kastel.mcse.ardoco.core.api.data.textextraction.INounMapping;
 
 /**
+ * Filters {@link IRecommendedInstance}s that occur only once in the text, thus are unlikely to be important entities.
+ *
  * @author Jan Keim
  *
  */
 public class OccasionFilter extends AbstractFilter<InconsistencyAgentData> {
+
+    @Configurable
+    private int expectedAppearances = 2;
 
     public OccasionFilter() {
         // empty
@@ -26,13 +35,6 @@ public class OccasionFilter extends AbstractFilter<InconsistencyAgentData> {
             var inconsistencyState = data.getInconsistencyState(model);
             filterRecommendedInstances(inconsistencyState);
         }
-
-        // TODO other idea: go over each word. Find corresponding NounMappings. Find out, if there are "other" words
-        // (that are thus similar).
-        // However, need to find out how to then filter RecommendedInstances out.
-        // Maybe: Find all NounMappings that only refer to one word (/phrase) and get the RecommendedInstances. If they
-        // only have said NounMappings, remove it.
-        // Something like that...
     }
 
     private void filterRecommendedInstances(IInconsistencyState inconsistencyState) {
@@ -50,17 +52,36 @@ public class OccasionFilter extends AbstractFilter<InconsistencyAgentData> {
     }
 
     private boolean recommendedInstanceHasMultipleOccasions(IRecommendedInstance recommendedInstance) {
-        var nameMappings = recommendedInstance.getNameMappings();
+        var counterDifferentTextPositions = countDifferentTextPositions(recommendedInstance);
 
+        return counterDifferentTextPositions >= expectedAppearances;
+    }
+
+    private int countDifferentTextPositions(IRecommendedInstance recommendedInstance) {
+        // phrases exist of words that are directly following each other. Therefore, we look if words appear not
+        // directly after each other to count different text positions
         var counter = 0;
+        var words = recommendedInstance.getNameMappings().flatCollect(INounMapping::getWords).toSortedSet(Comparator.comparingInt(IWord::getPosition));
+        var lastPosition = -1337;
+        for (var word : words) {
+            var position = word.getPosition();
+            if (position > lastPosition + 1) {
+                counter++;
+            }
+            lastPosition = position;
+        }
+        return counter;
+    }
+
+    // Alternative to counting different text positions
+    @SuppressWarnings("unused")
+    private int countSurfaceForms(IRecommendedInstance recommendedInstance) {
+        var counter = 0;
+        var nameMappings = recommendedInstance.getNameMappings();
         for (var nameMapping : nameMappings) {
             counter += nameMapping.getSurfaceForms().size();
         }
-        if (counter < 2) {
-            logger.info("Counter < 2: {}", recommendedInstance.getName());
-        }
-
-        return counter >= 2;
+        return counter;
     }
 
     @Override
