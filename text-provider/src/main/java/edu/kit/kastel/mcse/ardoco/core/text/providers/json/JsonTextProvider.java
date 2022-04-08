db3ab76af58d1d42880e5dfd391bff334daa9929
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-04-08 12:59:53 +0200

    Created JSON Text

---
+++ text-provider/src/main/java/edu/kit/kastel/mcse/ardoco/core/text/providers/json/JsonTextProvider.java
@@ -0,0 +1,67 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.mcse.ardoco.core.text.providers.json;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.kit.kastel.mcse.ardoco.core.api.data.text.IText;
+import edu.kit.kastel.mcse.ardoco.core.text.providers.ITextConnector;
+
+public final class JsonTextProvider implements ITextConnector, Serializable {
+
+    @JsonProperty
+    private Map<String, JsonText> texts = new HashMap<>();
+
+    public IText addNewText(String name, IText text) {
+        JsonText jText = new JsonText(text);
+        texts.put(name, jText);
+        return jText;
+    }
+
+    public void removeExistingTexts() {
+        this.texts.clear();
+    }
+
+    @Override
+    public IText getAnnotatedText() {
+        if (texts.isEmpty())
+            return null;
+        if (texts.size() > 1)
+            throw new IllegalStateException("Multiple texts are defined. Use this::getAnnotatedText(String)");
+        return texts.values().iterator().next();
+    }
+
+    @Override
+    public IText getAnnotatedText(String textName) {
+        return texts.get(textName);
+    }
+
+    public static JsonTextProvider loadFromFile(File f) throws IOException {
+        var texts = getMapper().readValue(f, JsonTextProvider.class);
+        texts.texts.values().forEach(JsonText::init);
+        return texts;
+    }
+
+    public void saveTexts(File destination) throws IOException {
+        getMapper().writeValue(destination, this);
+    }
+
+    private static ObjectMapper getMapper() {
+        ObjectMapper oom = new ObjectMapper();
+        oom.setVisibility(oom.getSerializationConfig()
+                .getDefaultVisibilityChecker() //
+                .withFieldVisibility(JsonAutoDetect.Visibility.ANY)//
+                .withGetterVisibility(JsonAutoDetect.Visibility.NONE)//
+                .withSetterVisibility(JsonAutoDetect.Visibility.NONE)//
+                .withIsGetterVisibility(JsonAutoDetect.Visibility.NONE));
+        return oom;
+    }
+
+}
---
+++ text-provider/src/test/java/edu/kit/kastel/mcse/ardoco/core/text/providers/JSONTextProviderGeneratorTest.java
@@ -0,0 +1,30 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.mcse.ardoco.core.text.providers;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import edu.kit.kastel.mcse.ardoco.core.text.providers.indirect.ParseProvider;
+import edu.kit.kastel.mcse.ardoco.core.text.providers.json.JsonTextProvider;
+
+class JSONTextProviderGeneratorTest {
+    @Test
+    @Disabled("Only a generator")
+    void generateJSONText() throws Exception {
+        ParseProvider pp = new ParseProvider(JSONTextProviderGeneratorTest.class.getResourceAsStream("/teastore.txt"));
+        var text = pp.getAnnotatedText();
+        JsonTextProvider jtp = new JsonTextProvider();
+        var jsonText = jtp.addNewText("teastore", text);
+        Assertions.assertNotNull(jsonText);
+        jtp.saveTexts(new File("./src/test/resources/teastore.json"));
+    }
+
+    @Test
+    void printText() throws Exception {
+        var pp = JsonTextProvider.loadFromFile(new File("./src/test/resources/teastore.json"));
+        System.out.println(pp.getAnnotatedText().getWords().collect(w -> w.getText()).makeString(" "));
+    }
+}
