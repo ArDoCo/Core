b3307cc03c4485595d1bb2a16fc7d32bedd662a0
Kevin Werber <werberkevin@gmail.com>
2022-04-13 15:15:09 +0200

    Cleanup & document babelnet classes

--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/nasari/babelnet/BabelNetDataSource.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/nasari/babelnet/BabelNetDataSource.java
@@ -9,61 +9,66 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * Provides BabelNet related data.
+ */
 public class BabelNetDataSource {
+
+	// TODO: Remove
+	public static void main(String[] args) throws IOException, InterruptedException {
+		var dataSource = new BabelNetDataSource(System.getenv("babelnet_key"), Path.of("babelnet_cache.json"));
+
+		var senses = dataSource.getSensesOfLemma("hood");
+
+		for (BabelNetSynsetId sense : senses) {
+			System.out.println("- " + sense);
+		}
+	}
+
+	private static final Gson GSON = new Gson();
 
     private final BabelNetCache cache;
     private final BabelNetHttpApi httpApi;
-    @Nullable private final Path cacheFilePath;
 
-    public BabelNetDataSource(String apiKey) throws IOException {
-        this.cache = new BabelNetCache(null); // TODO
+	/**
+	 * Constructs a new {@link BabelNetDataSource} instance.
+	 * @param apiKey the api key necessary to communicate with the official BabelNet API
+	 * @param cacheFilePath the path to the file that caches API responses
+	 * @throws IOException if the cache file could not be read
+	 */
+    public BabelNetDataSource(String apiKey, Path cacheFilePath) throws IOException {
+        this.cache = new BabelNetCache(cacheFilePath);
         this.httpApi = new BabelNetHttpApi(apiKey);
-        this.cacheFilePath = null;
     }
 
-    public BabelNetDataSource(String apiKey, @Nullable Path cacheFilePath) throws IOException {
-        this.cache = cacheFilePath == null ? new BabelNetCache() : new BabelNetCache(cacheFilePath);
-        this.httpApi = new BabelNetHttpApi(apiKey);
-        this.cacheFilePath = cacheFilePath;
-    }
-
-    // TODO: Code Cleanup
-
+	/**
+	 * Attempts to retrieve the ids of synsets that are related to the given lemma.
+	 * @param lemma the lemma
+	 * @return the list of synset ids
+	 * @throws IOException if an I/O error occurs while communicating with the BabelNet API or the cache
+	 * @throws InterruptedException if the communication with the BabelNet HTTP API is interrupted
+	 */
     public List<BabelNetSynsetId> getSensesOfLemma(String lemma) throws IOException, InterruptedException {
+	    Objects.requireNonNull(lemma);
+
         String response = this.cache.get(lemma).orElse(null);
 
         if (response == null) {
             response = this.httpApi.querySynsetIdsOfLemma(lemma);
             this.cache.insert(lemma, response);
-
-            if (this.cacheFilePath != null) {
-                this.cache.saveToFile(this.cacheFilePath);
-            }
+			this.cache.saveToFile();
         }
 
-        // Parse response
         var list = new ArrayList<BabelNetSynsetId>();
-        var gson = new Gson();
-        var array = gson.fromJson(response, JsonArray.class);
 
-        for (JsonElement jsonElement : array) {
-            var synsetDescriptor = jsonElement.getAsJsonObject();
-            var synsetIdStr = synsetDescriptor.get("id").getAsString();
-            list.add(new BabelNetSynsetId(synsetIdStr));
-        }
+	    for (JsonElement jsonElement : GSON.fromJson(response, JsonArray.class)) {
+		    var id = new BabelNetSynsetId(jsonElement.getAsJsonObject().get("id").getAsString());
+			list.add(id);
+	    }
 
         return list;
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        var dataSource = new BabelNetDataSource(System.getenv("babelnet_key"), Path.of("babelnet_cache.json"));
-
-        var senses = dataSource.getSensesOfLemma("hood");
-
-        for (BabelNetSynsetId sense : senses) {
-            System.out.println("- " + sense);
-        }
     }
 
 }
