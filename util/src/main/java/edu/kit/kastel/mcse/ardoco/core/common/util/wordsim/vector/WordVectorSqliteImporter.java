6ea0cee535f89e145570c03a763c6f0ce8ff8bc5
Kevin Werber <werberkevin@gmail.com>
2022-04-13 15:16:24 +0200

    Cleanup WordVectorSqliteImporter.java

--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/vector/WordVectorSqliteImporter.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/vector/WordVectorSqliteImporter.java
@@ -1,5 +1,6 @@
 package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.vector;
 
+import com.google.common.collect.ImmutableList;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim.PorterStemmer;
 import org.sqlite.SQLiteConfig;
 import org.sqlite.SQLiteOpenMode;
@@ -11,118 +12,202 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
+/**
+ * Reads a file containing word vector embeddings and inserts them into a sqlite database.
+ * The database must contain a table called {@code words} with two columns: {@code word} and {@code vec}.
+ * The {@code word} column must be a {@code TEXT} column while the {@code vec} column must be a {@code BLOB}.
+ * Vector representations will be inserted as a consecutive sequence of floats.
+ * The amount of floats in a sequence depends on the dimension of the vectors.
+ */
 public class WordVectorSqliteImporter {
 
-    public static void peek() throws IOException {
-        try (var in = Files.newInputStream(Path.of("C:\\dev\\uni\\bachelor\\Notizen\\wordsim\\data\\nasari\\NASARIembed+UMBC_w2v.txt"))) {
-            try (var reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
-                try (var bufferedReader = new BufferedReader(reader)) {
-                    for (int i = 0; i < 4; i++) {
-                        var line = bufferedReader.readLine();
-                        var parts = line.split(" ");
+	/**
+	 * Launches an import process.
+	 * The first string in the args array must be the path to the file containing the vector representations.
+	 * The second string in the args array must be the path to the sqlite database file.
+	 * The third string in the args array must be the dimension of the vectors.
+	 *
+	 * @param args the args array
+	 * @throws IOException  if an io error occurs
+	 * @throws SQLException if a database related error occurs
+	 */
+	public static void main(String[] args) throws SQLException, IOException {
+		ImportResult result = new WordVectorSqliteImporter(
+			Path.of(args[0]),
+			Path.of(args[1]),
+			Integer.parseInt(args[2])
+		).beginImport();
 
-                        var word = parts[0];
-                        var dim = parts.length - 1;
+		System.out.printf("Inserted: %s%n", result.inserted);
+		System.out.println("Skipped:");
 
-                        //System.out.println(word + ": " + dim);
-                        System.out.println(line);
-                    }
-                }
-            }
-        }
+		result.skippedWords.forEach(word -> System.out.printf("%s%n", word));
+	}
 
-        System.exit(0);
-    }
+	private static final int MAX_WORD_LENGTH = 300;
 
-    public static void main(String[] args) throws SQLException, IOException {
-        peek();
+	record ImportResult(long inserted, ImmutableList<String> skippedWords) { }
 
-        Path vectorFile = Path.of(args[0]);
-        Path dbFile = Path.of(args[1]);
-        int dimension = Integer.parseInt(args[2]);
+	private final Path vectorFile;
+	private final Path dbFile;
+	private final int dimension;
 
-        long lineStart = 0;
-        long lineEnd = 3000000;
-        long linesRead = 0;
-        long inserted = 0;
-        boolean stem = false;
-        boolean lowercase = false;
+	private final long startLine;
+	private final long endLine;
+	private final boolean stem;
+	private final boolean lowercase;
+	private final boolean dryRun;
 
-        var cfg = new SQLiteConfig();
-        cfg.setLockingMode(SQLiteConfig.LockingMode.EXCLUSIVE);
-        cfg.setJournalMode(SQLiteConfig.JournalMode.OFF);
-        cfg.setSynchronous(SQLiteConfig.SynchronousMode.OFF);
-        cfg.setOpenMode(SQLiteOpenMode.NOMUTEX);
+	/**
+	 * Constructs a new {@link WordVectorSqliteImporter} instance.
+	 *
+	 * @param vectorFile the path to the file that contains the vector representations for each word
+	 * @param dbFile     the path to the sqlite database into which the vector representations will be inserted
+	 * @param dimension  the dimension of the vectors
+	 */
+	public WordVectorSqliteImporter(Path vectorFile, Path dbFile, int dimension) {
+		this(vectorFile, dbFile, dimension, false, false, 0L, -1L, false);
+	}
 
-        var skipped = new ArrayList<String>();
+	/**
+	 * Constructs a new {@link WordVectorSqliteImporter} instance.
+	 *
+	 * @param vectorFile the path ot the file that contains the vector representations for each word
+	 * @param dbFile     the path to the sqlite database into which the vector representations will be inserted
+	 * @param dimension  the dimension of the vectors
+	 * @param stem       whether to stem a word before inserting it into the database
+	 * @param lowercase  whether to lowercase a word before inserting it into the database
+	 */
+	public WordVectorSqliteImporter(Path vectorFile, Path dbFile, int dimension, boolean stem, boolean lowercase) {
+		this(vectorFile, dbFile, dimension, stem, lowercase, 0L, -1L, false);
+	}
 
-        try (var connection = cfg.createConnection("jdbc:sqlite:" + dbFile.toAbsolutePath())) {
-            try (var statement = connection.prepareStatement("INSERT INTO `words` (`word`, `vec`) VALUES (?, ?);")) {
-                try (var in = Files.newInputStream(vectorFile)) {
-                    try (var reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
-                        try (var bufferedReader = new BufferedReader(reader)) {
-                            ByteBuffer buffer = ByteBuffer.allocate(dimension * 4);
+	/**
+	 * Constructs a new {@link WordVectorSqliteImporter} instance.
+	 * To start the import process, call {@link #beginImport()}.
+	 *
+	 * @param vectorFile the path ot the file that contains the vector representations for each word
+	 * @param dbFile     the path to the sqlite database into which the vector representations will be inserted
+	 * @param dimension  the dimension of the vectors
+	 * @param stem       whether to stem a word before inserting it into the database
+	 * @param lowercase  whether to lowercase a word before inserting it into the database
+	 * @param startLine  at which line of the {@code vectorFile} this importer will start inserting
+	 * @param endLine    at which line of the {@code vectorFile} this importer will stop inserting
+	 * @param dryRun     whether this importer should actually insert. Use {@code false} to run this importer without
+	 *                   actually inserting anything
+	 */
+	public WordVectorSqliteImporter(Path vectorFile, Path dbFile, int dimension, boolean stem, boolean lowercase,
+	                                long startLine, long endLine, boolean dryRun) {
+		this.vectorFile = vectorFile;
+		this.dbFile = dbFile;
+		this.dimension = dimension;
+		this.stem = stem;
+		this.lowercase = lowercase;
+		this.startLine = startLine;
+		this.endLine = endLine;
+		this.dryRun = dryRun;
 
-                            while (bufferedReader.ready()) {
-                                var line = bufferedReader.readLine();
-                                var parts = line.split(" ");
+		if (!Files.exists(vectorFile)) {
+			throw new IllegalStateException("vectorFile does not exist: " + vectorFile);
+		}
 
-                                linesRead++;
+		if (!Files.exists(dbFile)) {
+			throw new IllegalStateException("dbFile does not exist: " + dbFile);
+		}
+	}
 
-                                if (linesRead < lineStart) {
-                                    continue;
-                                }
+	/**
+	 * Starts the import process.
+	 * @return the result of the import process
+	 * @throws SQLException if an error occurs while interacting with the database
+	 * @throws IOException if an error occurs while interacting with the vector file
+	 */
+	public ImportResult beginImport() throws SQLException, IOException {
+		final List<String> skippedWords = new ArrayList<>();
+		long linesRead = 0;
+		long inserted = 0;
 
-                                if (linesRead > lineEnd) {
-                                    break;
-                                }
+		try (Connection connection = connect()) {
+			try (PreparedStatement statement = prepareSelect(connection)) {
+				try (var in = Files.newInputStream(vectorFile, StandardOpenOption.READ)) {
+					try (var reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
+						try (var bufferedReader = new BufferedReader(reader)) {
+							ByteBuffer buffer = ByteBuffer.allocate(dimension * 4);
 
-                                String word = parts[0];
+							while (bufferedReader.ready()) {
+								var line = bufferedReader.readLine();
+								var parts = line.split(" ");
 
-                                if (word.length() > 300) {
-                                    // Filter out weird words from dataset
-                                    skipped.add(word);
-                                    continue;
-                                }
+								linesRead++;
 
-                                if (lowercase) {
-                                    word = word.toLowerCase(Locale.ROOT);
-                                }
+								if (linesRead < startLine) {
+									continue;
+								}
 
-                                if (stem) {
-                                    word = PorterStemmer.stem(word);
-                                }
+								if (linesRead > endLine) {
+									break;
+								}
 
-                                buffer.clear();
+								String word = parts[0];
 
-                                for (int i = 0; i < parts.length - 1; i++) {
-                                    float value = Float.parseFloat(parts[i + 1]);
-                                    buffer.putFloat(value);
-                                }
+								if (word.length() > MAX_WORD_LENGTH) {
+									// Filter out weird words from dataset
+									skippedWords.add(word);
+									continue;
+								}
 
-                                statement.setString(1, word);
-                                statement.setBytes(2, buffer.array());
-                                statement.execute();
+								if (lowercase) {
+									word = word.toLowerCase(Locale.ROOT);
+								}
 
-                                inserted++;
-                            }
-                        }
-                    }
-                }
-            }
+								if (stem) {
+									word = PorterStemmer.stem(word);
+								}
 
-            System.out.println("==============================================");
-            System.out.printf("Inserted: %s%n", inserted);
-            System.out.println("Skipped:");
+								buffer.clear();
 
-            for (String skippedWord : skipped) {
-                System.out.printf("%s: %s%n", skippedWord.length(), skippedWord);
-            }
-        }
-    }
+								for (int i = 0; i < parts.length - 1; i++) {
+									float value = Float.parseFloat(parts[i + 1]);
+									buffer.putFloat(value);
+								}
+
+								if (!dryRun) {
+									statement.setString(1, word);
+									statement.setBytes(2, buffer.array());
+									statement.execute();
+								}
+
+								inserted++;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return new ImportResult(inserted, ImmutableList.copyOf(skippedWords));
+	}
+
+	private Connection connect() throws SQLException {
+		var cfg = new SQLiteConfig();
+		cfg.setLockingMode(SQLiteConfig.LockingMode.EXCLUSIVE);
+		cfg.setJournalMode(SQLiteConfig.JournalMode.OFF);
+		cfg.setSynchronous(SQLiteConfig.SynchronousMode.OFF);
+		cfg.setOpenMode(SQLiteOpenMode.NOMUTEX);
+
+		return cfg.createConnection("jdbc:sqlite:" + this.dbFile.toAbsolutePath());
+	}
+
+	private PreparedStatement prepareSelect(Connection conn) throws SQLException {
+		return conn.prepareStatement("INSERT INTO `words` (`word`, `vec`) VALUES (?, ?);");
+	}
 
 }
