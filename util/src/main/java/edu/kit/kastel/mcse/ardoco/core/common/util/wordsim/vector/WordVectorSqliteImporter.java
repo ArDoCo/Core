6175078544423b87825af99435564e95116824ef
Kevin Werber <werberkevin@gmail.com>
2022-04-18 15:11:52 +0200

    Cleanup WordVectorSqliteImporter

--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/vector/WordVectorSqliteImporter.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/vector/WordVectorSqliteImporter.java
@@ -2,6 +2,8 @@ package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.vector;
 
 import com.google.common.collect.ImmutableList;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.sewordsim.PorterStemmer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sqlite.SQLiteConfig;
 import org.sqlite.SQLiteOpenMode;
 
@@ -22,16 +24,22 @@ import java.util.Locale;
 
 /**
  * Reads a file containing word vector embeddings and inserts them into a sqlite database.
+ * <p>
  * The database must contain a table called {@code words} with two columns: {@code word} and {@code vec}.
  * The {@code word} column must be a {@code TEXT} column while the {@code vec} column must be a {@code BLOB}.
  * Vector representations will be inserted as a consecutive sequence of floats.
  * The amount of floats in a sequence depends on the dimension of the vectors.
+ * <p>
+ * This class is customizable by extending it and overriding the {@link #processWord(String)} method.
+ * This method will be called for each word and allows modifying the word before it is inserted into the database.
  */
 public class WordVectorSqliteImporter {
 
-    // TODO: Make this class more customizable (through extending)
-    // TODO: Use logger
-    // and move this class to tests?
+	private static final int DEFAULT_MAX_WORD_LENGTH = 300;
+	private static final Logger LOGGER = LoggerFactory.getLogger(WordVectorSqliteImporter.class);
+
+	// TODO: Make this class more customizable (through extending)
+	// and move this class to tests?
 
 	/**
 	 * Launches an import process.
@@ -50,13 +58,10 @@ public class WordVectorSqliteImporter {
 			Integer.parseInt(args[2])
 		).beginImport();
 
-		System.out.printf("Inserted: %s%n", result.inserted);
-		System.out.println("Skipped:");
-
-		result.skippedWords.forEach(word -> System.out.printf("%s%n", word));
+		LOGGER.info("Inserted: {}\n", result.inserted);
+		LOGGER.info("Skipped: ({})", result.skippedWords.size());
+		result.skippedWords.forEach(word -> LOGGER.info("{}\n", word));
 	}
-
-	private static final int MAX_WORD_LENGTH = 300;
 
 	record ImportResult(long inserted, ImmutableList<String> skippedWords) { }
 
@@ -66,6 +71,7 @@ public class WordVectorSqliteImporter {
 
 	private final long startLine;
 	private final long endLine;
+	private final int maxWordLength;
 	private final boolean stem;
 	private final boolean lowercase;
 	private final boolean dryRun;
@@ -78,7 +84,7 @@ public class WordVectorSqliteImporter {
 	 * @param dimension  the dimension of the vectors
 	 */
 	public WordVectorSqliteImporter(Path vectorFile, Path dbFile, int dimension) {
-		this(vectorFile, dbFile, dimension, false, false, 0L, -1L, false);
+		this(vectorFile, dbFile, dimension, DEFAULT_MAX_WORD_LENGTH, false, false, 0L, -1L, false);
 	}
 
 	/**
@@ -91,28 +97,30 @@ public class WordVectorSqliteImporter {
 	 * @param lowercase  whether to lowercase a word before inserting it into the database
 	 */
 	public WordVectorSqliteImporter(Path vectorFile, Path dbFile, int dimension, boolean stem, boolean lowercase) {
-		this(vectorFile, dbFile, dimension, stem, lowercase, 0L, -1L, false);
+		this(vectorFile, dbFile, dimension, DEFAULT_MAX_WORD_LENGTH, stem, lowercase, 0L, -1L, false);
 	}
 
 	/**
 	 * Constructs a new {@link WordVectorSqliteImporter} instance.
 	 * To start the import process, call {@link #beginImport()}.
 	 *
-	 * @param vectorFile the path ot the file that contains the vector representations for each word
-	 * @param dbFile     the path to the sqlite database into which the vector representations will be inserted
-	 * @param dimension  the dimension of the vectors
-	 * @param stem       whether to stem a word before inserting it into the database
-	 * @param lowercase  whether to lowercase a word before inserting it into the database
-	 * @param startLine  at which line of the {@code vectorFile} this importer will start inserting
-	 * @param endLine    at which line of the {@code vectorFile} this importer will stop inserting
-	 * @param dryRun     whether this importer should actually insert. Use {@code false} to run this importer without
-	 *                   actually inserting anything
+	 * @param vectorFile    the path ot the file that contains the vector representations for each word
+	 * @param dbFile        the path to the sqlite database into which the vector representations will be inserted
+	 * @param dimension     the dimension of the vectors
+	 * @param maxWordLength the maximum length a word is allowed to have to be inserted into the database
+	 * @param stem          whether to stem a word before inserting it into the database
+	 * @param lowercase     whether to lowercase a word before inserting it into the database
+	 * @param startLine     at which line of the {@code vectorFile} this importer will start inserting
+	 * @param endLine       at which line of the {@code vectorFile} this importer will stop inserting
+	 * @param dryRun        whether this importer should actually insert. Use {@code false} to run this importer without
+	 *                      actually inserting anything
 	 */
-	public WordVectorSqliteImporter(Path vectorFile, Path dbFile, int dimension, boolean stem, boolean lowercase,
+	public WordVectorSqliteImporter(Path vectorFile, Path dbFile, int dimension, int maxWordLength, boolean stem, boolean lowercase,
 	                                long startLine, long endLine, boolean dryRun) {
 		this.vectorFile = vectorFile;
 		this.dbFile = dbFile;
 		this.dimension = dimension;
+		this.maxWordLength = maxWordLength;
 		this.stem = stem;
 		this.lowercase = lowercase;
 		this.startLine = startLine;
@@ -130,11 +138,13 @@ public class WordVectorSqliteImporter {
 
 	/**
 	 * Starts the import process.
+	 *
 	 * @return the result of the import process
 	 * @throws SQLException if an error occurs while interacting with the database
-	 * @throws IOException if an error occurs while interacting with the vector file
+	 * @throws IOException  if an error occurs while interacting with the vector file
+	 * @throws IllegalStateException if a vector with an invalid dimension is found
 	 */
-	public ImportResult beginImport() throws SQLException, IOException {
+	public ImportResult beginImport() throws SQLException, IOException, IllegalStateException {
 		final List<String> skippedWords = new ArrayList<>();
 		long linesRead = 0;
 		long inserted = 0;
@@ -148,7 +158,6 @@ public class WordVectorSqliteImporter {
 
 							while (bufferedReader.ready()) {
 								var line = bufferedReader.readLine();
-								var parts = line.split(" ");
 
 								linesRead++;
 
@@ -160,23 +169,31 @@ public class WordVectorSqliteImporter {
 									break;
 								}
 
-                                if (parts.length - 1 != this.dimension) {
-                                    throw new IllegalStateException("importer has read line with invalid vector dimension: \"" + line + "\"");
-                                }
+								var parts = line.split(" ");
 
-                                String word = parts[0];
+								if (parts.length - 1 != this.dimension) {
+									throw new IllegalStateException("importer has read line with invalid vector dimension: \"" + line + "\"");
+								}
 
-								if (word.length() > MAX_WORD_LENGTH) {
-									// Filter out weird words from dataset
+								// Process the word
+								String word = parts[0];
+
+								if (word.length() > this.maxWordLength) { // Filter out weird words from dataset
 									skippedWords.add(word);
 									continue;
 								}
 
-								if (lowercase) {
+								if (this.lowercase) {
 									word = word.toLowerCase(Locale.ROOT);
 								}
 
-                                // Insert into database
+								if (this.stem) {
+									word = PorterStemmer.stem(word);
+								}
+
+								word = processWord(word);
+
+								// Process the vector
 								buffer.clear();
 
 								for (int i = 0; i < parts.length - 1; i++) {
@@ -184,14 +201,15 @@ public class WordVectorSqliteImporter {
 									buffer.putFloat(value);
 								}
 
+								// Insert into database
 								if (!dryRun) {
 									statement.setString(1, word);
 									statement.setBytes(2, buffer.array());
 									statement.execute();
 								}
-                                else {
-                                    System.out.println("Would have inserted: " + word);
-                                }
+								else {
+									LOGGER.debug("Would have inserted: {}", word);
+								}
 
 								inserted++;
 							}
@@ -218,16 +236,15 @@ public class WordVectorSqliteImporter {
 		return conn.prepareStatement("INSERT INTO `words` (`word`, `vec`) VALUES (?, ?);");
 	}
 
-    protected String processWord(String word) {
-        if (this.lowercase) {
-            word = word.toLowerCase(Locale.ROOT);
-        }
-
-        if (this.stem) {
-            word = PorterStemmer.stem(word);
-        }
-
-        return word;
-    }
+	/**
+	 * This method is called for each word that is read from the vector file.
+	 * The string that is returned will then be used for insertion.
+	 *
+	 * @param word the word to process
+	 * @return the processed version of the word
+	 */
+	protected String processWord(String word) {
+		return word;
+	}
 
 }
