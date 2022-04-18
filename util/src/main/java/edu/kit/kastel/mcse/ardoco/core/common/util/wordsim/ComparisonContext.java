3d7cd9ab905eb0beee02fc50f2bcf58bfd36ab12
Kevin Werber <werberkevin@gmail.com>
2022-04-18 15:39:59 +0200

    Implement canonical constructor for ComparisonContext
    This allows us to guarantee that firstString and secondString cannot be null

--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/ComparisonContext.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/ComparisonContext.java
@@ -10,53 +10,66 @@ import java.util.Objects;
 
 /**
  * A ComparisonContext contains all information that can be used for comparing similarity between objects that occur
- * within ArDoCo.
+ * within ArDoCo. The fields {@link #firstString} and {@link #secondString} are always not null.
+ * The field {@link #lemmatize} decides whether the lemmatized version of both words should be used for comparison.
  */
-public record ComparisonContext(@Nonnull String firstString, @Nonnull String secondString, @Nullable IWord firstWord, @Nullable IWord secondWord,
-        boolean lemmatize) {
+public record ComparisonContext(@Nonnull String firstString, @Nonnull String secondString,
+                                @Nullable IWord firstWord, @Nullable IWord secondWord,
+                                boolean lemmatize) {
 
-    public ComparisonContext(@Nonnull String firstString, @Nonnull String secondString) {
-        this(firstString, secondString, null, null, false);
-    }
+	public ComparisonContext(@Nonnull String firstString, @Nonnull String secondString) {
+		this(firstString, secondString, null, null, false);
+	}
 
-    public ComparisonContext(@Nonnull String firstString, @Nonnull String secondString, boolean lemmatize) {
-        this(firstString, secondString, null, null, lemmatize);
-    }
+	public ComparisonContext(@Nonnull String firstString, @Nonnull String secondString, boolean lemmatize) {
+		this(firstString, secondString, null, null, lemmatize);
+	}
 
-    public ComparisonContext(IWord firstWord, IWord secondWord, boolean lemmatize) {
-        this(firstWord.getText(), secondWord.getText(), firstWord, secondWord, lemmatize);
-    }
+	public ComparisonContext(@Nonnull IWord firstWord, @Nonnull IWord secondWord, boolean lemmatize) {
+		this(firstWord.getText(), secondWord.getText(), firstWord, secondWord, lemmatize);
+	}
 
-    /**
-     * Finds the most appropriate string representation by the first object in this comparison object. This method can
-     * be used as a shorthand to avoid going through all variables that could possibly represent the first object.
-     *
-     * @return the most appropriate string presentation of the first object in this comparison
-     */
-    @Nonnull
-    public String firstTerm() {
-        return findAppropriateTerm(firstString, firstWord);
-    }
+	public ComparisonContext(@Nonnull String firstString, @Nonnull String secondString,
+	                         @Nullable IWord firstWord, @Nullable IWord secondWord,
+	                         boolean lemmatize) {
+		this.firstString = Objects.requireNonNull(firstString);
+		this.secondString = Objects.requireNonNull(secondString);
+		this.firstWord = firstWord;
+		this.secondWord = secondWord;
+		this.lemmatize = lemmatize;
+	}
 
-    /**
-     * Finds the most appropriate string representation by the second object in this comparison object. This method can
-     * be used as a shorthand to avoid going through all variables that could possibly represent the second object.
-     *
-     * @return the most appropriate string presentation of the second object in this comparison
-     */
-    @Nonnull
-    public String secondTerm() {
-        return findAppropriateTerm(secondString, secondWord);
-    }
+	/**
+	 * Finds the most appropriate string representation by the first object in this comparison object. This method can
+	 * be used as a shorthand to avoid going through all variables that could possibly represent the first object.
+	 *
+	 * @return the most appropriate string presentation of the first object in this comparison
+	 */
+	@Nonnull
+	public String firstTerm() {
+		return findAppropriateTerm(firstString, firstWord);
+	}
 
-    private String findAppropriateTerm(@Nonnull String string, @Nullable IWord word) {
-        Objects.requireNonNull(string);
+	/**
+	 * Finds the most appropriate string representation by the second object in this comparison object. This method can
+	 * be used as a shorthand to avoid going through all variables that could possibly represent the second object.
+	 *
+	 * @return the most appropriate string presentation of the second object in this comparison
+	 */
+	@Nonnull
+	public String secondTerm() {
+		return findAppropriateTerm(secondString, secondWord);
+	}
 
-        if (word != null) {
-            return lemmatize ? word.getLemma() : word.getText();
-        } else {
-            return string;
-        }
-    }
+	private String findAppropriateTerm(@Nonnull String string, @Nullable IWord word) {
+		Objects.requireNonNull(string);
+
+		if (word != null) {
+			return lemmatize ? word.getLemma() : word.getText();
+		}
+		else {
+			return string;
+		}
+	}
 
 }
