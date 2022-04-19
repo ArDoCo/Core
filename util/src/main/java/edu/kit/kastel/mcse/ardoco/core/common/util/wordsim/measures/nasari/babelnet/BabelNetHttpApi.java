d9fd96ca9de18f5fb4a61e4ce88a3e24b8ff3600
Kevin Werber <werberkevin@gmail.com>
2022-04-19 14:44:57 +0200

    BabelNetHttpApi retries request if first attempt fails

--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/nasari/babelnet/BabelNetHttpApi.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/measures/nasari/babelnet/BabelNetHttpApi.java
@@ -1,6 +1,5 @@
 package edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.nasari.babelnet;
 
-import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.nasari.babelnet.exception.BabelNetException;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.nasari.babelnet.exception.BabelNetInvalidKeyException;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.measures.nasari.babelnet.exception.BabelNetRequestLimitException;
 
@@ -11,6 +10,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -19,79 +19,79 @@ import java.util.Objects;
  */
 public class BabelNetHttpApi {
 
-    // TODO: Explicitly specify result language
+	// Sadly, there is no official documentation from BabelNet. These error messages have to suffice. Must be lowercase!
+	private static final String INVALID_KEY_ERROR = "your key is not valid";
+	private static final String REQUEST_LIMIT_REACHED_ERROR = "the daily requests limit has been reached";
+	private static final Duration FIRST_ATTEMPT_TIMEOUT = Duration.ofSeconds(2);
 
-    // Sadly, there is no official documentation from BabelNet. So these error messages have to suffice. Must be lowercase!
-    private static final String INVALID_KEY_ERROR = "your key is not valid";
-    private static final String REQUEST_LIMIT_REACHED_ERROR = "the daily requests limit has been reached";
+	private static final String LANGUAGE = "EN";
+	private static final String WORD_SENSE_QUERY = "https://babelnet.io/v6/getSynsetIds" +
+		"?lemma={lemma}" +
+		"&searchLang={searchLang}" +
+		"&targetLang={targetLang}" +
+		"&key={key}";
 
-    private static final String LANGUAGE = "EN";
-    private static final String WORD_SENSE_QUERY = "https://babelnet.io/v6/getSynsetIds?lemma={lemma}&searchLang={searchLang}&key={key}";
-
-    private final String apiKey;
-    private long lastQuery = System.currentTimeMillis();
-    private long start = System.currentTimeMillis();
+	private final String apiKey;
 
 	/**
 	 * Constructs a new instance of {@link BabelNetHttpApi}.
+	 *
 	 * @param apiKey the api key necessary to interact with BabelNet
 	 */
-    public BabelNetHttpApi(String apiKey) {
-        this.apiKey = Objects.requireNonNull(apiKey);
-    }
+	public BabelNetHttpApi(String apiKey) {
+		this.apiKey = Objects.requireNonNull(apiKey);
+	}
 
 	/**
 	 * Queries the BabelNet API for ids of synsets that are related to the given lemma.
+	 *
 	 * @param lemma the lemma
 	 * @return the response body of this query
-	 * @throws IOException if an I/O error occurs when sending or receiving
+	 * @throws IOException          if an I/O error occurs when sending or receiving
 	 * @throws InterruptedException if the operation is interrupted
 	 */
-    public String querySynsetIdsOfLemma(String lemma) throws IOException, InterruptedException, BabelNetRequestLimitException, BabelNetInvalidKeyException {
-        if (System.currentTimeMillis() - lastQuery < 5000) { // TODO: Find better solution
-            Thread.sleep(5000);
-        }
+	public String querySynsetIdsOfLemma(String lemma) throws IOException, InterruptedException, BabelNetRequestLimitException, BabelNetInvalidKeyException {
+		lemma = URLEncoder.encode(lemma, StandardCharsets.UTF_8);
 
-        lastQuery = System.currentTimeMillis();
+		String url = WORD_SENSE_QUERY
+			.replace("{lemma}", lemma)
+			.replace("{searchLang}", LANGUAGE)
+			.replace("{targetLang}", LANGUAGE)
+			.replace("{key}", this.apiKey);
 
-        lemma = URLEncoder.encode(lemma, StandardCharsets.UTF_8);
+		var request = HttpRequest.newBuilder()
+			.GET()
+			.uri(URI.create(url))
+			.build();
 
-        // Return type maybe should already be List<BabelNetSynsetId>
-        String url = WORD_SENSE_QUERY
-                .replace("{lemma}", lemma)
-                .replace("{searchLang}", LANGUAGE)
-                .replace("{key}", this.apiKey);
+		try {
+			return sendGetRequest(request);
+		}
+		catch (IOException | InterruptedException e) {
+			// Exception could be caused by too many queries in a short timespan
+			// => wait a bit and try again
+			Thread.sleep(FIRST_ATTEMPT_TIMEOUT.toMillis());
+			return sendGetRequest(request);
+		}
+	}
 
-        var httpClient = HttpClient.newHttpClient();
+	private String sendGetRequest(HttpRequest request) throws IOException, InterruptedException,
+		BabelNetInvalidKeyException, BabelNetRequestLimitException {
 
-        var request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(url))
-                .build();
+		HttpClient client = HttpClient.newHttpClient();
 
-        try {
-            var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+		var response = client.send(request, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+		String body = response.body();
 
-            String body = response.body();
+		// Do some basic error checking with known error messages
+		if (body.toLowerCase(Locale.ROOT).contains(INVALID_KEY_ERROR)) {
+			throw new BabelNetInvalidKeyException(body);
+		}
+		else if (body.toLowerCase(Locale.ROOT).contains(REQUEST_LIMIT_REACHED_ERROR)) {
+			throw new BabelNetRequestLimitException(body);
+		}
 
-            // Do some basic error checking with known error messages
-            if (body.toLowerCase(Locale.ROOT).contains(INVALID_KEY_ERROR)) {
-                throw new BabelNetInvalidKeyException(body);
-            } else if (body.toLowerCase(Locale.ROOT).contains(REQUEST_LIMIT_REACHED_ERROR)) {
-                throw new BabelNetRequestLimitException(body);
-            }
-
-            lastQuery = System.currentTimeMillis();
-
-            System.out.println("[" + (System.currentTimeMillis() - start) + "] Worked: " + url); // TODO: REMOVE
-
-            return body;
-        }
-        catch (IOException | BabelNetException e) {
-            System.err.println("[" + (System.currentTimeMillis() - start) + "] Failed: " + url); // TODO: REMOVE & CLEANUP
-            e.printStackTrace();
-            throw e;
-        }
-    }
+		return body;
+	}
 
 }
