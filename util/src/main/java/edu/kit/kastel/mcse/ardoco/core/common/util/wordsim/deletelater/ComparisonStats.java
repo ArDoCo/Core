4fc895711a84c0fe0d4f12c1e5545af591f3e9b2
Kevin Werber <werberkevin@gmail.com>
2022-05-16 20:26:44 +0200

    Clean up ComparisonStats

--- util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/deletelater/ComparisonStats.java
+++ util/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/wordsim/deletelater/ComparisonStats.java
@@ -5,61 +5,91 @@ import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.ComparisonContext;
 import edu.kit.kastel.mcse.ardoco.core.common.util.wordsim.WordSimMeasure;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * This class will probably be deleted in the future.
  */
 public class ComparisonStats {
 
-    public static boolean ENABLED = true;
+	public static boolean ENABLED = true;
 
-    private static final List<Comparison> COMPARISONS = new ArrayList<>();
-    private static WordPair PENDING_WORD_PAIR = null;
-    private static final List<MeasureResult> PENDING_MEASURES = new ArrayList<>();
-    private static double PENDING_SCORE = Double.NaN;
+	private static final Set<WordPair> KNOWN_WORD_PAIRS = new HashSet<>();
+	private static final Set<Comparison> COMPARISONS = new HashSet<>();
 
-    public static void begin(ComparisonContext ctx) {
-        if (!ENABLED) {
-            return;
-        }
-        PENDING_WORD_PAIR = new WordPair(ctx.firstTerm(), ctx.secondTerm());
-        PENDING_MEASURES.clear();
-    }
+	private static class CURRENT {
+		static boolean SKIP = false;
+		static WordPair WORD_PAIR = null;
+		static ComparisonContext CONTEXT = null;
+		static double SCORE = Double.NaN;
+		static List<MeasureResult> RESULTS = new ArrayList<>();
+	}
 
-    public static void record(WordSimMeasure measure, boolean accepted) {
-        if (!ENABLED) {
-            return;
-        }
-        var measureResult = new MeasureResult(PENDING_WORD_PAIR, measure, accepted, PENDING_SCORE);
-        PENDING_MEASURES.add(measureResult);
-        PENDING_SCORE = Double.NaN;
-    }
+	public static void begin(ComparisonContext ctx) {
+		if (!ENABLED) {
+			return;
+		}
 
-    public static void recordScore(double score) {
-        PENDING_SCORE = score;
-    }
+		CURRENT.WORD_PAIR = new WordPair(ctx.firstTerm(), ctx.secondTerm());
+		CURRENT.CONTEXT = ctx;
 
-    public static void end(boolean finalResult) {
-        if (!ENABLED) {
-            return;
-        }
-        var comparison = new Comparison(PENDING_WORD_PAIR, new ArrayList<>(PENDING_MEASURES), finalResult);
+		if (KNOWN_WORD_PAIRS.contains(CURRENT.WORD_PAIR)) {
+			// A previous comparison with the same word pair has already been made
+			// => results for the current comparison are already known, this can be skipped
+			CURRENT.SKIP = true;
+		}
+		else {
+			KNOWN_WORD_PAIRS.add(CURRENT.WORD_PAIR);
+		}
+	}
 
-        COMPARISONS.add(comparison);
+	public static void record(WordSimMeasure measure, boolean accepted) {
+		if (!ENABLED || CURRENT.SKIP) {
+			return;
+		}
 
-        PENDING_WORD_PAIR = null;
-        PENDING_MEASURES.clear();
-    }
+		var measureResult = new MeasureResult(CURRENT.WORD_PAIR, measure, accepted, CURRENT.SCORE);
 
-    public static void reset() {
-        PENDING_WORD_PAIR = null;
-        PENDING_MEASURES.clear();
-        COMPARISONS.clear();
-    }
+		CURRENT.RESULTS.add(measureResult);
+		CURRENT.SCORE = Double.NaN;
+	}
 
-    public static List<Comparison> getComparisons() {
-        return new ArrayList<>(COMPARISONS);
-    }
+	public static void recordScore(double score) {
+		if (!ENABLED || CURRENT.SKIP) {
+			return;
+		}
+		CURRENT.SCORE = score;
+	}
+
+	public static void end(boolean finalResult) {
+		if (!ENABLED) {
+			return;
+		}
+
+		if (!CURRENT.SKIP) {
+			var comparison = new Comparison(CURRENT.WORD_PAIR, CURRENT.CONTEXT, CURRENT.RESULTS, finalResult);
+			COMPARISONS.add(comparison);
+		}
+
+		CURRENT.SKIP = false;
+		CURRENT.WORD_PAIR = null;
+		CURRENT.SCORE = Double.NaN;
+		CURRENT.RESULTS.clear();
+	}
+
+	public static void reset() {
+		COMPARISONS.clear();
+		KNOWN_WORD_PAIRS.clear();
+		CURRENT.SKIP = false;
+		CURRENT.WORD_PAIR = null;
+		CURRENT.SCORE = Double.NaN;
+		CURRENT.RESULTS.clear();
+	}
+
+	public static Set<Comparison> getComparisons() {
+		return new HashSet<>(COMPARISONS);
+	}
 
 }
