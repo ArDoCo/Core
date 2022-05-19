a3c54339f8ae7232df43c2269654904f3892b12c
Jan Keim <jan.keim@kit.edu>
2022-05-19 08:28:55 +0300

    add phrase class for coreNLP provider

---
+++ common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/data/text/IPhrase.java
@@ -0,0 +1,23 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.mcse.ardoco.core.api.data.text;
+
+import java.util.List;
+
+public interface IPhrase {
+    int getSentenceNo();
+
+    ISentence getSentence();
+
+    String getText();
+
+    // TODO create an enum with label types?
+    PhraseType getPhraseType();
+
+    List<IWord> getContainedWords();
+
+    List<IPhrase> getSubPhrases();
+
+    boolean isSuperPhraseOf(IPhrase other);
+
+    boolean isSubPhraseOf(IPhrase other);
+}
