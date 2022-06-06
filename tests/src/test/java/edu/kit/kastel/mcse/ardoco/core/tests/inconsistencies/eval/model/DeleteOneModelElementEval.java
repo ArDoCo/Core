fd61b491f8044b1cd88d3c185de1aac192e2b72f
Jan Keim <jan.keim@kit.edu>
2022-06-06 16:47:10 +0300

    Add detailed result info that is written to file
    For debug and improvement reasons, there will now be files created that give details about the different results of the inconsistency step

--- tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/inconsistencies/eval/model/DeleteOneModelElementEval.java
+++ tests/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/inconsistencies/eval/model/DeleteOneModelElementEval.java
@@ -1,13 +1,19 @@
 /* Licensed under MIT 2021-2022. */
 package edu.kit.kastel.mcse.ardoco.core.tests.inconsistencies.eval.model;
 
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.kit.kastel.mcse.ardoco.core.api.data.DataStructure;
 import edu.kit.kastel.mcse.ardoco.core.api.data.inconsistency.IInconsistency;
@@ -26,6 +32,10 @@ import edu.kit.kastel.mcse.ardoco.core.tests.inconsistencies.mod.ModifiedElement
 import edu.kit.kastel.mcse.ardoco.core.tests.inconsistencies.mod.model.DeleteOneElementEach;
 
 public class DeleteOneModelElementEval extends AbstractEvalStrategy {
+    private static final Logger logger = LoggerFactory.getLogger(DeleteOneModelElementEval.class);
+    private static final String OUTPUT = "src/test/resources/testout";
+
+    private static final boolean detailedDebug = true;
 
     public DeleteOneModelElementEval() {
         // empty
@@ -35,6 +45,10 @@ public class DeleteOneModelElementEval extends AbstractEvalStrategy {
     public EvaluationResult evaluate(Project project, IModelConnector originalModel, IText originalText, GoldStandard gs, PrintStream os) {
         IModificationStrategy strategy = new DeleteOneElementEach(originalModel);
         var result = process(originalModel, originalText, strategy);
+
+        if (detailedDebug) {
+            writeOutResultInfo(project, result);
+        }
 
         var evaluator = new PRF1Evaluator();
 
@@ -50,6 +64,64 @@ public class DeleteOneModelElementEval extends AbstractEvalStrategy {
         return evaluator.getWeightedAveragePRF1();
     }
 
+    private void writeOutResultInfo(Project project, Map<ModifiedElement<IModelConnector, IModelInstance>, DataStructure> result) {
+        var evalDir = Path.of(OUTPUT).resolve("id_eval");
+        Path targetFile = null;
+        try {
+            Files.createDirectories(evalDir);
+            var filename = "results_" + project.name().toLowerCase() + ".md";
+            targetFile = evalDir.resolve(filename);
+            Files.writeString(targetFile, "", StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            logger.warn(e.getMessage(), e.getCause());
+            return;
+        }
+
+        var outputBuilder = new StringBuilder("# Inconsistency Detection\n");
+        outputBuilder.append("## ");
+        outputBuilder.append(project.name());
+        outputBuilder.append("\n");
+        for (var resultEntry : result.entrySet()) {
+            var modifiedElement = resultEntry.getKey();
+            if (modifiedElement != null) {
+                // only look at the base version, not when an element was deleted
+                return;
+            }
+            var data = resultEntry.getValue();
+            for (var modelId : data.getModelIds()) {
+                outputBuilder.append("### Model-ID: ");
+                outputBuilder.append(modelId);
+                outputBuilder.append("\n\n");
+
+                var text = data.getText();
+                var sentences = text.getSentences();
+                ImmutableList<IInconsistency> inconsistencies = data.getInconsistencyState(modelId).getInconsistencies();
+                var mmInconsistencies = inconsistencies.select(MissingModelInstanceInconsistency.class::isInstance)
+                        .collect(MissingModelInstanceInconsistency.class::cast)
+                        .toSortedList((i1, i2) -> Integer.compare(i1.sentence(), i2.sentence()));
+                for (var mmi : mmInconsistencies) {
+                    var sentenceNo = mmi.sentence();
+                    var sentenceText = sentences.get(sentenceNo - 1).getText();
+                    var name = mmi.name();
+
+                    outputBuilder.append("s");
+                    outputBuilder.append(String.format("%02d", sentenceNo));
+                    outputBuilder.append("; name=\"" + name + "\"");
+                    outputBuilder.append("; sentence=\"" + sentenceText + "\"");
+                    outputBuilder.append("\n");
+
+                }
+                outputBuilder.append("\n");
+            }
+
+            try {
+                Files.writeString(targetFile, outputBuilder.toString(), StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     private Map<ModifiedElement<IModelConnector, IModelInstance>, DataStructure> process(IModelConnector pcmModel, IText annotatedText,
             IModificationStrategy strategy) {
         var configurations = new HashMap<String, String>();
@@ -61,9 +133,9 @@ public class DeleteOneModelElementEval extends AbstractEvalStrategy {
         var original = runRecommendationConnectionInconsistency(originalData);
         results.put(null, original);
 
-        var iter = strategy.getModifiedModelInstances();
-        while (iter.hasNext()) {
-            var modification = iter.next();
+        var modifiedElementIterator = strategy.getModifiedModelInstances();
+        while (modifiedElementIterator.hasNext()) {
+            var modification = modifiedElementIterator.next();
             var model = modification.getArtifact();
             var data = new DataStructure(annotatedText, Map.of(model.getModelId(), runModelExtractor(model, configurations)));
             runTextExtractor(data, configurations);
