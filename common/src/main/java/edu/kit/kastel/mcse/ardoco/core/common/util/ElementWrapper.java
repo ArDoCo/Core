1d7c28b5c3152b9e9c4cab46ff2193dfc162a096
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-07-27 16:22:01 +0200

    Fixed lookup of phrases in textState

--- common/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/ElementWrapper.java
+++ common/src/main/java/edu/kit/kastel/mcse/ardoco/core/common/util/ElementWrapper.java
@@ -1,3 +1,4 @@
+/* Licensed under MIT 2022. */
 package edu.kit.kastel.mcse.ardoco.core.common.util;
 
 import java.util.Objects;
@@ -6,39 +7,39 @@ import java.util.function.Function;
 
 public final class ElementWrapper<E> {
 
-	private final Class<E> elementType;
+    private final Class<E> elementType;
 
-	private final E element;
+    private final E element;
 
-	private final Function<E, Integer> elementToHash;
+    private final Function<E, Integer> elementToHash;
 
-	private final BiPredicate<E, E> elementEquals;
+    private final BiPredicate<E, E> elementEquals;
 
-	public ElementWrapper(Class<E> elementType, E element, Function<E, Integer> elementToHash, BiPredicate<E, E> elementEquals) {
+    public ElementWrapper(Class<E> elementType, E element, Function<E, Integer> elementToHash, BiPredicate<E, E> elementEquals) {
 
-		this.elementType = Objects.requireNonNull(elementType);
-		this.element = element;
-		this.elementEquals = Objects.requireNonNull(elementEquals);
-		this.elementToHash = Objects.requireNonNull(elementToHash);
+        this.elementType = Objects.requireNonNull(elementType);
+        this.element = element;
+        this.elementEquals = Objects.requireNonNull(elementEquals);
+        this.elementToHash = Objects.requireNonNull(elementToHash);
 
-	}
+    }
 
-	@Override
-	public int hashCode() {
-		return elementToHash.apply(element);
-	}
+    @Override
+    public int hashCode() {
+        return elementToHash.apply(element);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		ElementWrapper<?> that = (ElementWrapper<?>) o;
-		return elementType.equals(that.elementType) && elementEquals.test(element, elementType.cast(that.element));
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        ElementWrapper<?> that = (ElementWrapper<?>) o;
+        return elementType.equals(that.elementType) && elementEquals.test(element, elementType.cast(that.element));
+    }
 
-	public E getElement() {
-		return element;
-	}
+    public E getElement() {
+        return element;
+    }
 }
