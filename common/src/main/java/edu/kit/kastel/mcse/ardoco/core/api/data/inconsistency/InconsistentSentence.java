e634667143cf4a3656416b53fd1cc020eae0b191
Jan Keim <jan.keim@kit.edu>
2022-08-11 16:19:39 +0200

    Add missing JavaDocs to InconsistentSentence and ArDoCoResult

--- common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/data/inconsistency/InconsistentSentence.java
+++ common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/data/inconsistency/InconsistentSentence.java
@@ -8,13 +8,41 @@ import org.eclipse.collections.api.factory.Lists;
 
 import edu.kit.kastel.mcse.ardoco.core.api.data.text.Sentence;
 
+/**
+ * This record represents an inconsistent sentence consisting of a sentence and all the inconsistencies that were found
+ * within this sentence.
+ * 
+ * @param sentence        The sentence
+ * @param inconsistencies the list of inconsistencies within the sentence
+ */
 public record InconsistentSentence(Sentence sentence, List<Inconsistency> inconsistencies) {
-    // TODO
 
+    /**
+     * Creates a new instance with only one inconsistency. The underlying list is populated with the given
+     * inconsistency.
+     * 
+     * @param sentence      the sentence
+     * @param inconsistency the inconsistency
+     */
     public InconsistentSentence(Sentence sentence, Inconsistency inconsistency) {
         this(sentence, Lists.mutable.of(inconsistency));
     }
 
+    /**
+     * Adds an inconsistency to the list of inconsistencies of this sentence.
+     * 
+     * @param inconsistency the inconsistency
+     */
+    public boolean addInconsistency(Inconsistency inconsistency) {
+        return inconsistencies.add(inconsistency);
+    }
+
+    /**
+     * Creates and returns an info string that contains the sentence number, the text of the sentence, and the reasons
+     * of the inconsistencies
+     * 
+     * @return an info string
+     */
     public String getInfoString() {
         StringBuilder reasonsBuilder = new StringBuilder();
         for (var inconsistency : inconsistencies) {
@@ -25,9 +53,4 @@ public record InconsistentSentence(Sentence sentence, List<Inconsistency> incons
         String formatString = "S%3d: \"%s\"%n\tInconsistent due to the following reasons:%n%s";
         return String.format(Locale.ENGLISH, formatString, sentence.getSentenceNumberForOutput(), sentence.getText(), reasonsBuilder);
     }
-
-    public void addInconsistency(Inconsistency inconsistency) {
-        inconsistencies.add(inconsistency);
-    }
-
 }
