4664edb459638d09f8851276d72fbe3a4792478a
Dominik Fuchß <dominik.fuchss@kit.edu>
2022-08-12 10:09:13 +0200

    Add some javadoc to sentence

--- common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/data/text/Sentence.java
+++ common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/data/text/Sentence.java
@@ -5,19 +5,27 @@ import org.eclipse.collections.api.list.ImmutableList;
 
 /**
  * Represents a sentence within the document.
- *
- *
  */
 public interface Sentence {
 
     /**
-     * Returns the sentence number
+     * Returns the sentence number (starting at {@code 0}.
      *
      * @return the sentence number
      */
     int getSentenceNumber();
 
-    int getSentenceNumberForOutput();
+    /**
+     * Return the sentence number used for human readably output.
+     * Therefore, this method calculates the sentence number starting with {@code 1}.
+     *
+     * @return the sentence number starting at one
+     */
+    default int getSentenceNumberForOutput() {
+        return getSentenceNumber() + 1;
+    }
+
+    ;
 
     /**
      * Returns the words contained by this sentence
