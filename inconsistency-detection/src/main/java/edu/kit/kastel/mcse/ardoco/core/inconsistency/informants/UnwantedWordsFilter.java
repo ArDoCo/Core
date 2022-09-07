8ad89c632b97c7d5adb7ed67d608f821035a13f5
Jan Keim <jan.keim@kit.edu>
2022-09-07 09:43:38 +0200

    Update heurisics for UnwantedWordsFilter

--- inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/informants/UnwantedWordsFilter.java
+++ inconsistency-detection/src/main/java/edu/kit/kastel/mcse/ardoco/core/inconsistency/informants/UnwantedWordsFilter.java
@@ -1,6 +1,8 @@
 package edu.kit.kastel.mcse.ardoco.core.inconsistency.informants;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.ImmutableList;
@@ -8,10 +10,17 @@ import org.eclipse.collections.api.list.ImmutableList;
 import edu.kit.kastel.informalin.data.DataRepository;
 import edu.kit.kastel.mcse.ardoco.core.api.data.inconsistency.InconsistencyState;
 import edu.kit.kastel.mcse.ardoco.core.api.data.recommendationgenerator.RecommendedInstance;
+import edu.kit.kastel.mcse.ardoco.core.api.data.textextraction.NounMapping;
 
+// TODO this is a prototype. It should be improved, e.g, using following ideas:
+// - use ComputerScienceWords
+// - improve handling: instead of filtering them completely, reduce probability? Idea: we don't want to remove super high probability RIs
+// - maybe consider phrases
+// - maybe check if the word is a named entity or starts with capital letter in the middle of a sentence (sign of being a named entity)
 public class UnwantedWordsFilter extends Filter {
 
-    private final ImmutableList<String> filterWords = Lists.immutable.with("meta", "log", "server", "browser", "task", "operation", "case", "instance");
+    private final ImmutableList<String> unwantedWords = Lists.immutable.with("meta", "log", "browser", "task", "operation", "case", "instance", "log",
+            "script");
 
     public UnwantedWordsFilter(DataRepository dataRepository) {
         super(UnwantedWordsFilter.class.getSimpleName(), dataRepository);
@@ -33,20 +42,34 @@ public class UnwantedWordsFilter extends Filter {
 
     private boolean checkRecommendedInstance(RecommendedInstance recommendedInstance) {
         for (var nounMapping : recommendedInstance.getNameMappings()) {
-
-            // filter unwanted words from word list
-            var reference = nounMapping.getReference().toLowerCase();
-            if (filterWords.contains(reference)) {
+            // apply heuristics
+            if (referenceContainsUnwantedWord(nounMapping) || referenceContainsPluralWord(nounMapping) || referenceContainsOnlyNumbers(nounMapping))
                 return true;
-            }
-
-            // filter plural words (heuristic)
-            for (var word : nounMapping.getReferenceWords()) {
-                if (word.getPosTag().name().toLowerCase().contains("plural"))
-                    return true;
-            }
         }
         return false;
+    }
+
+    private boolean referenceContainsUnwantedWord(NounMapping nounMapping) {
+        var reference = nounMapping.getReference().toLowerCase();
+        if (unwantedWords.contains(reference)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean referenceContainsPluralWord(NounMapping nounMapping) {
+        for (var word : nounMapping.getReferenceWords()) {
+            if (word.getPosTag().name().toLowerCase().contains("plural"))
+                return true;
+        }
+        return false;
+    }
+
+    private boolean referenceContainsOnlyNumbers(NounMapping nounMapping) {
+        var reference = nounMapping.getReference().toLowerCase();
+        Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+        Matcher matcher = pattern.matcher(reference);
+        return matcher.matches();
     }
 
     @Override
