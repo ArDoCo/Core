12cdf9b1579ae39e36004cf81acbaf4afb923f2f
Jan Keim <jan.keim@kit.edu>
2023-02-09 17:42:51 +0100

    Refactor text-provider into text-preprocessor pipeline
    Using pipeline and data in the same fashion as other pipeline steps.

---
+++ text-preprocessing/src/test/java/edu/kit/kastel/mcse/ardoco/core/text/providers/base/NlpInformantTest.java
@@ -0,0 +1,31 @@
+/* Licensed under MIT 2022. */
+package edu.kit.kastel.mcse.ardoco.core.text.providers.base;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.kit.kastel.mcse.ardoco.core.api.data.text.NlpInformant;
+import edu.kit.kastel.mcse.ardoco.core.text.providers.informants.corenlp.CoreNLPProvider;
+
+public abstract class NlpInformantTest {
+    private static final Logger logger = LoggerFactory.getLogger(NlpInformantTest.class);
+    protected static String inputText = "src/test/resources/teastore.txt";
+
+    private NlpInformant provider = null;
+
+    @BeforeEach
+    void beforeEach() {
+        provider = getProvider();
+    }
+
+    protected abstract CoreNLPProvider getProvider();
+
+    @Test
+    void getTextTest() {
+        var text = provider.getAnnotatedText();
+        Assertions.assertNotNull(text);
+    }
+}
