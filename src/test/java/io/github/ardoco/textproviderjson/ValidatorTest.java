efbb525cd84e7073a03018c922a80a12bd5a1c1b
laura <laura.erb1@gmx.de>
2023-02-21 16:12:23 +0100

    added tests and networknt dependency

---
+++ src/test/java/io/github/ardoco/textproviderjson/ValidatorTest.java
@@ -0,0 +1,19 @@
+package io.github.ardoco.textproviderjson;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class ValidatorTest {
+
+    @Test
+    void testValidating () throws IOException {
+        Assertions.assertTrue(Converter.validateJson(Files.readString(Path.of("./src/test/resources/valid-example-text.json"))));
+        Assertions.assertFalse(Converter.validateJson(Files.readString(Path.of("./src/test/resources/invalid-example-text.json"))));
+
+    }
+
+}
