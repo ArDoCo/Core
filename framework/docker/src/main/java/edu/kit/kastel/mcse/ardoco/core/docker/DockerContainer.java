bad2aab74c3c7b3c423ca5f22d82afc4fff3cc06
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-04-12 12:58:11 +0200

    Remove docker and java model extractor

--- framework/docker/src/main/java/edu/kit/kastel/mcse/ardoco/core/docker/DockerContainer.java
+++
@@ -1,8 +0,0 @@
-/* Licensed under MIT 2022-2023. */
-package edu.kit.kastel.mcse.ardoco.core.docker;
-
-public record DockerContainer(String id, String image, String status, String name) {
-    public boolean isRunning() {
-        return status().startsWith("Up");
-    }
-}
--- framework/docker/src/main/java/edu/kit/kastel/mcse/ardoco/core/docker/DockerPortBind.java
+++
@@ -1,8 +0,0 @@
-/* Licensed under MIT 2022-2023. */
-package edu.kit.kastel.mcse.ardoco.core.docker;
-
-public record DockerPortBind(int hostPort, int containerPort, boolean wildcard) {
-    public boolean valid() {
-        return hostPort > 0 && containerPort > 0;
-    }
-}
