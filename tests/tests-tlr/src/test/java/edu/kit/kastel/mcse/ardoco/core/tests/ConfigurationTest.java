a32549bdda08d8b62397127e336d88ca2e30e55c
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-04-14 10:54:35 +0200

    Split into profiles

---
+++ tests/tests-tlr/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/ConfigurationTest.java
@@ -0,0 +1,41 @@
+/* Licensed under MIT 2022-2023. */
+package edu.kit.kastel.mcse.ardoco.core.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import edu.kit.kastel.mcse.ardoco.core.configuration.AbstractConfigurable;
+import edu.kit.kastel.mcse.ardoco.core.tests.eval.ConfigurationTestBase;
+
+/**
+ * This test class deals with the configurations.
+ *
+ * @see AbstractConfigurable
+ */
+class ConfigurationTest extends ConfigurationTestBase {
+    @Override
+    protected void assertFalse(boolean result, String message) {
+        Assertions.assertFalse(result, message);
+    }
+
+    @Override
+    protected void fail(String message) {
+        Assertions.fail(message);
+    }
+
+    /**
+     * This test verifies that all configurable values are able to be configured. It also prints all configurable values
+     * as they should be contained in a configuration file.
+     *
+     * @throws Exception if anything goes wrong
+     */
+    @Test
+    public void showCurrentConfiguration() throws Exception {
+        super.showCurrentConfiguration();
+    }
+
+    @Test
+    public void testValidityOfConfigurableFields() throws Exception {
+        super.testValidityOfConfigurableFields();
+    }
+}
