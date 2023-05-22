cd80e516b26fd646d5b3165cf8bf31238b2770e0
Jan Keim <jan.keim@kit.edu>
2023-05-22 11:26:57 +0200

    Remove dependency from ArDoCoResult to StateImpls

--- tests/tests-misc/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/integration/statehelper/files/RecommendationStateFile.java
+++ tests/tests-misc/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/integration/statehelper/files/RecommendationStateFile.java
@@ -52,9 +52,12 @@ public class RecommendationStateFile {
         builder.append(String.join(VALUE_SEPARATOR, "Name", "Type", "Probability", "Names", "Types", "Sentences", "Claimants", LINE_SEPARATOR));
         builder.append(LINE_SEPARATOR);
 
-        for (RecommendedInstance recommendation : recommendationState.getRecommendedInstances()
-                .toSortedListBy(RecommendedInstance::getType)
-                .sortThisBy(RecommendedInstance::getName)) {
+        ImmutableList<RecommendedInstance> recommendedInstances = Lists.immutable.empty();
+        if (recommendationState != null) {
+            recommendedInstances = recommendationState.getRecommendedInstances();
+        }
+
+        for (RecommendedInstance recommendation : recommendedInstances.toSortedListBy(RecommendedInstance::getType).sortThisBy(RecommendedInstance::getName)) {
             builder.append(recommendation.getName());
             builder.append(VALUE_SEPARATOR);
             builder.append(recommendation.getType());
@@ -82,9 +85,11 @@ public class RecommendationStateFile {
         var builder = new StringBuilder();
 
         var currentRecommendationState = arDoCoResult.getRecommendationState(metaModel);
-        var currentRecommendedInstances = currentRecommendationState.getRecommendedInstances()
-                .toSortedListBy(RecommendedInstance::getType)
-                .sortThisBy(RecommendedInstance::getName);
+        ImmutableList<RecommendedInstance> recommendedInstances = Lists.immutable.empty();
+        if (currentRecommendationState != null) {
+            recommendedInstances = currentRecommendationState.getRecommendedInstances();
+        }
+        var currentRecommendedInstances = recommendedInstances.toSortedListBy(RecommendedInstance::getType).sortThisBy(RecommendedInstance::getName);
 
         builder.append("# ").append(arDoCoResult.getProjectName());
         builder.append(LINE_SEPARATOR).append(LINE_SEPARATOR);
@@ -124,13 +129,13 @@ public class RecommendationStateFile {
                 ImmutableList<String> names = currentRecommendedInstance.getNameMappings().collect(NounMapping::getReference).toSet().toImmutableList();
                 ImmutableList<String> types = currentRecommendedInstance.getTypeMappings().collect(NounMapping::getReference).toSet().toImmutableList();
                 ImmutableList<String> sentences = currentRecommendedInstance.getSentenceNumbers()
-                        .toSortedList()
-                        .collect(no -> Integer.toString(no))
-                        .toImmutable();
+                                                                            .toSortedList()
+                                                                            .collect(no -> Integer.toString(no))
+                                                                            .toImmutable();
                 ImmutableSet<String> claimants = currentRecommendedInstance.getClaimants().collect(c -> c.getClass().getSimpleName());
 
-                String currentLine = String.join(VALUE_SEPARATOR, name, type, probability, String.join(LIST_SEPARATOR, names), String.join(LIST_SEPARATOR,
-                        types), String.join(LIST_SEPARATOR, sentences), String.join(LIST_SEPARATOR, claimants));
+                String currentLine = String.join(VALUE_SEPARATOR, name, type, probability, String.join(LIST_SEPARATOR, names),
+                        String.join(LIST_SEPARATOR, types), String.join(LIST_SEPARATOR, sentences), String.join(LIST_SEPARATOR, claimants));
                 if (nameOrder == 0 && typeOrder == 0) {
 
                     if (!parts[2].equals(probability) ||//
