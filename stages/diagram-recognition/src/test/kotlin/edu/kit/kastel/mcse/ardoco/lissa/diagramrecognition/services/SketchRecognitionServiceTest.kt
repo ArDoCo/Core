cc2dc6b75b4010a5facd29b457375706e1254a80
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-05-22 17:04:23 +0200

    Skipping Docker related JUnit tests if "NO_DOCKER" environment variable is set to true or if no docker is present

--- stages/diagram-recognition/src/test/kotlin/edu/kit/kastel/mcse/ardoco/lissa/diagramrecognition/services/SketchRecognitionServiceTest.kt
+++ stages/diagram-recognition/src/test/kotlin/edu/kit/kastel/mcse/ardoco/lissa/diagramrecognition/services/SketchRecognitionServiceTest.kt
@@ -7,33 +7,56 @@ import edu.kit.kastel.mcse.ardoco.core.data.DataRepository
 import edu.kit.kastel.mcse.ardoco.lissa.DiagramRecognition
 import edu.kit.kastel.mcse.ardoco.lissa.diagramrecognition.visualize
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.awt.Color
 import java.awt.Desktop
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.util.concurrent.TimeUnit
 import kotlin.test.assertNotNull
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisabledIfEnvironmentVariable(named = "NO_DOCKER", matches = "true")
 class SketchRecognitionServiceTest {
     companion object {
         const val PATH = "src/test/resources/bbb-arch-overview.png"
         const val PATH_TO_HL_ARCHITECTURE = "src/test/resources/highlevelArchitecture.png"
+
+        private val logger: Logger = LoggerFactory.getLogger(SketchRecognitionServiceTest::class.java)
     }
 
     private lateinit var dataRepository: DataRepository
 
     @BeforeAll
     fun setup() {
+        assumeDocker()
         dataRepository = DataRepository()
         dataRepository.addData(InputDiagramData.ID, InputDiagramData("src/test/resources/"))
         val stage = DiagramRecognition(dataRepository)
         stage.run()
         assertNotNull(getState())
         File("target/testout").mkdirs()
+    }
+
+    private fun assumeDocker() {
+        val remoteDocker = System.getenv("REMOTE_DOCKER_IP") != null && System.getenv("REMOTE_DOCKER_PORT") != null
+        var localDocker = true
+        try {
+            val result = Runtime.getRuntime().exec("docker ps")
+            result.waitFor(3, TimeUnit.SECONDS)
+            localDocker = result.exitValue() == 0
+        } catch (e: Exception) {
+            localDocker = false
+            logger.error(e.message, e)
+        }
+        Assumptions.assumeTrue(remoteDocker || localDocker, "Docker is not available")
     }
 
     private fun getState() = dataRepository.getData(DiagramRecognitionState.ID, DiagramRecognitionState::class.java).orElse(null)
