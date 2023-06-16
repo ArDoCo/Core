ece7312991eaa4dee4d20a385501b564b82e1d8d
Gram21 <Gram21@users.noreply.github.com>
2023-06-16 08:59:30 +0000

    Apply formatting changes

--- framework/common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/models/BasicEntity.java
+++ framework/common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/models/BasicEntity.java
@@ -1,2 +1,5 @@
-package edu.kit.kastel.mcse.ardoco.core.api.models;public class BasicEntity {
+/* Licensed under MIT 2023. */
+package edu.kit.kastel.mcse.ardoco.core.api.models;
+
+public class BasicEntity {
 }
