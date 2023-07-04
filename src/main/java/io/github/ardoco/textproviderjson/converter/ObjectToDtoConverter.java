077f479d4edcf97227b1cec5065b1b36f09637ad
Laxraa <99870553+Laxraa@users.noreply.github.com>
2023-07-04 15:38:28 +0200

    Feature/refactor jsonconverter (#6)
    * moved the schema text.json into the right directory
    
    * adjusted the indices of words and sentences
    
    * moved text.json into resource folder
    
    * Update src/main/java/io/github/ardoco/textproviderjson/converter/DtoToObjectConverter.java
    
    1L instead of (long) 1
    
    Co-authored-by: Dominik Fuchß <develop@fuchss.org>
    
    * Update src/main/java/io/github/ardoco/textproviderjson/converter/ObjectToDtoConverter.java
    
    1L instead of (long) 1
    
    Co-authored-by: Dominik Fuchß <develop@fuchss.org>
    
    * Update src/main/java/io/github/ardoco/textproviderjson/converter/ObjectToDtoConverter.java
    
    1L instead of (long) 1
    
    Co-authored-by: Dominik Fuchß <develop@fuchss.org>
    
    * Update src/main/java/io/github/ardoco/textproviderjson/converter/DtoToObjectConverter.java
    
    1L instead of (long) 1
    
    Co-authored-by: Dominik Fuchß <develop@fuchss.org>
    
    * replaced FileInputStream with getResourceAsStream
    
    * Apply formatting changes
    
    ---------
    
    Co-authored-by: Dominik Fuchß <develop@fuchss.org>
    Co-authored-by: Laxraa <Laxraa@users.noreply.github.com>

--- src/main/java/io/github/ardoco/textproviderjson/converter/ObjectToDtoConverter.java
+++ src/main/java/io/github/ardoco/textproviderjson/converter/ObjectToDtoConverter.java
@@ -138,14 +138,14 @@ public class ObjectToDtoConverter {
     private IncomingDependencyDTO convertToDepInDTO(DependencyImpl dependency) {
         IncomingDependencyDTO dependencyDTO = new IncomingDependencyDTO();
         dependencyDTO.setDependencyTag(dependency.getDependencyTag());
-        dependencyDTO.setSourceWordId(dependency.getWordId());
+        dependencyDTO.setSourceWordId(dependency.getWordId() + 1L);
         return dependencyDTO;
     }
 
     private OutgoingDependencyDTO convertToDepOutDTO(DependencyImpl dependency) {
         OutgoingDependencyDTO dependencyDTO = new OutgoingDependencyDTO();
         dependencyDTO.setDependencyTag(dependency.getDependencyTag());
-        dependencyDTO.setTargetWordId(dependency.getWordId());
+        dependencyDTO.setTargetWordId(dependency.getWordId() + 1L);
         return dependencyDTO;
     }
 }
