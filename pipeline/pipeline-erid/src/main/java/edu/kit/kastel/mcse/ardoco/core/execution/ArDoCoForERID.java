24579959d97cb74da90a2ae52e2d2df11e6141ee
Fabian Reinbold <fabian.reinbold@outlook.com>
2023-08-06 13:25:36 +0200

    Fix ArDoCoForErid Runner

--- pipeline/pipeline-erid/src/main/java/edu/kit/kastel/mcse/ardoco/core/execution/ArDoCoForERID.java
+++ pipeline/pipeline-erid/src/main/java/edu/kit/kastel/mcse/ardoco/core/execution/ArDoCoForERID.java
@@ -2,67 +2,58 @@ package edu.kit.kastel.mcse.ardoco.core.execution;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import edu.kit.kastel.mcse.ardoco.core.api.models.ArchitectureModelType;
 import edu.kit.kastel.mcse.ardoco.core.common.util.CommonUtilities;
 import edu.kit.kastel.mcse.ardoco.core.common.util.DataRepositoryHelper;
 import edu.kit.kastel.mcse.ardoco.core.connectiongenerator.ConnectionGenerator;
-import edu.kit.kastel.mcse.ardoco.core.diagramconnectiongenerator.DiagramConnectionGenerator;
-import edu.kit.kastel.mcse.ardoco.core.execution.runner.ArDoCoRunnerExt;
+import edu.kit.kastel.mcse.ardoco.core.execution.runner.ParameterizedRunner;
 import edu.kit.kastel.mcse.ardoco.core.inconsistency.InconsistencyChecker;
 import edu.kit.kastel.mcse.ardoco.core.models.ModelProviderAgent;
+import edu.kit.kastel.mcse.ardoco.core.pipeline.AbstractPipelineStep;
 import edu.kit.kastel.mcse.ardoco.core.recommendationgenerator.RecommendationGenerator;
 import edu.kit.kastel.mcse.ardoco.core.text.providers.TextPreprocessingAgent;
 import edu.kit.kastel.mcse.ardoco.core.textextraction.TextExtraction;
-import edu.kit.kastel.mcse.ardoco.lissa.DiagramRecognition;
+import edu.kit.kastel.mcse.ardoco.erid.diagramconnectiongenerator.DiagramConnectionGenerator;
+import edu.kit.kastel.mcse.ardoco.erid.diagraminconsistency.DiagramInconsistencyChecker;
+import edu.kit.kastel.mcse.ardoco.erid.diagramrecognitionmock.DiagramRecognitionMock;
 
-public class ArDoCoForERID extends ArDoCoRunnerExt<ArDoCoForERID.Parameters> {
+public class ArDoCoForERID extends ParameterizedRunner<ArDoCoForERID.Parameters> {
     public record Parameters(File inputText, File inputModelArchitecture, ArchitectureModelType inputArchitectureModelType,
                              Map<String, String> additionalConfigs, File outputDir) {
     }
 
-    private static final Logger logger = LoggerFactory.getLogger(ArDoCoForERID.class);
-
-    public ArDoCoForERID(String projectName) {
-        super(projectName);
+    public ArDoCoForERID(String projectName, Parameters parameters) {
+        super(projectName, parameters);
     }
 
     @Override
-    public boolean setUp(Parameters p) {
-        try {
-            definePipeline(p);
-        } catch (IOException e) {
-            logger.error("Problem in initialising pipeline when loading data (IOException)", e.getCause());
-            isSetUp = false;
-            return false;
-        }
-        setOutputDirectory(p.outputDir);
-        isSetUp = true;
-        return true;
-    }
+    public List<AbstractPipelineStep> initializePipelineSteps(Parameters p) throws IOException {
+        var pipelineSteps = new ArrayList<AbstractPipelineStep>();
 
-    private void definePipeline(Parameters p) throws IOException {
         ArDoCo arDoCo = getArDoCo();
         var dataRepository = arDoCo.getDataRepository();
 
-        arDoCo.addPipelineStep(DiagramRecognition.get(p.additionalConfigs, dataRepository));
+        pipelineSteps.add(new DiagramRecognitionMock(p.additionalConfigs, dataRepository));
 
         var text = CommonUtilities.readInputText(p.inputText);
         if (text.isBlank()) {
             throw new IllegalArgumentException("Cannot deal with empty input text. Maybe there was an error reading the file.");
         }
         DataRepositoryHelper.putInputText(dataRepository, text);
-        arDoCo.addPipelineStep(TextPreprocessingAgent.get(p.additionalConfigs, dataRepository));
+        pipelineSteps.add(TextPreprocessingAgent.get(p.additionalConfigs, dataRepository));
 
-        arDoCo.addPipelineStep(ModelProviderAgent.get(p.inputModelArchitecture, p.inputArchitectureModelType, dataRepository));
-        arDoCo.addPipelineStep(TextExtraction.get(p.additionalConfigs, dataRepository));
-        arDoCo.addPipelineStep(RecommendationGenerator.get(p.additionalConfigs, dataRepository));
-        arDoCo.addPipelineStep(DiagramConnectionGenerator.get(p.additionalConfigs, dataRepository));
-        arDoCo.addPipelineStep(ConnectionGenerator.get(p.additionalConfigs, dataRepository));
-        arDoCo.addPipelineStep(InconsistencyChecker.get(p.additionalConfigs, dataRepository));
+        pipelineSteps.add(ModelProviderAgent.get(p.inputModelArchitecture, p.inputArchitectureModelType, dataRepository));
+        pipelineSteps.add(TextExtraction.get(p.additionalConfigs, dataRepository));
+        pipelineSteps.add(RecommendationGenerator.get(p.additionalConfigs, dataRepository));
+        pipelineSteps.add(new DiagramConnectionGenerator(p.additionalConfigs, dataRepository));
+        pipelineSteps.add(new DiagramInconsistencyChecker(p.additionalConfigs, dataRepository));
+        pipelineSteps.add(ConnectionGenerator.get(p.additionalConfigs, dataRepository));
+        pipelineSteps.add(InconsistencyChecker.get(p.additionalConfigs, dataRepository));
+
+        return pipelineSteps;
     }
 }
