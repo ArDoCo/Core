67e01c92171db0d62687a3afeaf20add2d5a7684
Jan Keim <jan.keim@kit.edu>
2023-09-19 09:25:46 +0200

    Exchange GET with POST for microservice

--- stages/text-preprocessing/src/main/java/edu/kit/kastel/mcse/ardoco/core/text/providers/informants/corenlp/textprocessor/TextProcessorService.java
+++ stages/text-preprocessing/src/main/java/edu/kit/kastel/mcse/ardoco/core/text/providers/informants/corenlp/textprocessor/TextProcessorService.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.kit.kastel.mcse.ardoco.core.api.text.Text;
 import edu.kit.kastel.mcse.ardoco.core.text.providers.informants.corenlp.config.ConfigManager;
 import edu.kit.kastel.mcse.ardoco.core.textproviderjson.converter.DtoToObjectConverter;
@@ -17,6 +21,7 @@ import edu.kit.kastel.mcse.ardoco.core.textproviderjson.error.NotConvertableExce
  * This text processor processes texts by sending requests to a microservice, which provides text processing using CoreNLP.
  */
 public class TextProcessorService {
+    private static final Logger logger = LoggerFactory.getLogger(TextProcessorService.class);
 
     /**
      * processes and annotates a given text by sending requests to a microservice
@@ -32,14 +37,30 @@ public class TextProcessorService {
     }
 
     private String sendCorenlpRequest(String inputText) throws IOException {
-        inputText = URLEncoder.encode(inputText, StandardCharsets.UTF_8);
+        String encodedText = encodeText(inputText);
         ConfigManager configManager = ConfigManager.INSTANCE;
-        String requestUrl = configManager.getMicroserviceUrl() + configManager.getCorenlpService() + inputText;
-        return sendAuthenticatedGetRequest(requestUrl);
+        String requestUrl = configManager.getMicroserviceUrl() + configManager.getCorenlpService();
+        return sendAuthenticatedPostRequest(requestUrl, encodedText);
+    }
+
+    private static String encodeText(String inputText) {
+        String encodedText = URLEncoder.encode(inputText, StandardCharsets.UTF_8);
+        return encodedText;
     }
 
     private String sendAuthenticatedGetRequest(String requestUrl) throws IOException {
         HttpCommunicator httpCommunicator = new HttpCommunicator();
         return httpCommunicator.sendAuthenticatedGetRequest(requestUrl);
+    }
+
+    private String sendAuthenticatedPostRequest(String requestUrl, String encodedText) throws IOException {
+        HttpCommunicator httpCommunicator = new HttpCommunicator();
+        String body = getRequestBodyString(encodedText);
+        return httpCommunicator.sendAuthenticatedPostRequest(requestUrl, body);
+    }
+
+    @NotNull
+    private static String getRequestBodyString(String encodedText) {
+        return "{\"text\": \"" + encodedText + "\"}";
     }
 }
