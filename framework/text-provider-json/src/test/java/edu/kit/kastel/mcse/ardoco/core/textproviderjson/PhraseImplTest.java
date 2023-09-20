f06ac36f95ae65d7c633d89e94255fb008b351a5
Jan Keim <jan.keim@kit.edu>
2023-09-20 11:22:12 +0200

    Add tests for PhraseImpl and TextImpl

---
+++ framework/text-provider-json/src/test/java/edu/kit/kastel/mcse/ardoco/core/textproviderjson/PhraseImplTest.java
@@ -0,0 +1,81 @@
+/* Licensed under MIT 2023. */
+package edu.kit.kastel.mcse.ardoco.core.textproviderjson;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.kit.kastel.mcse.ardoco.core.api.text.Phrase;
+import edu.kit.kastel.mcse.ardoco.core.api.text.Text;
+import edu.kit.kastel.mcse.ardoco.core.textproviderjson.converter.DtoToObjectConverter;
+import edu.kit.kastel.mcse.ardoco.core.textproviderjson.error.NotConvertableException;
+
+class PhraseImplTest {
+
+    private static final DtoToObjectConverter CONVERTER = new DtoToObjectConverter();
+    private static Phrase baselinePhrase;
+    private Phrase phraseImplInstance;
+
+    @BeforeAll
+    static void initAll() {
+        Text baselineText = TestUtil.generateTextWithMultipleSentences();
+        baselinePhrase = baselineText.getSentences().get(1).getPhrases().get(0);
+    }
+
+    @BeforeEach
+    void init() {
+        try {
+            Text textImplInstance = CONVERTER.convertText(TestUtil.generateDTOWithMultipleSentences());
+            phraseImplInstance = textImplInstance.getSentences().get(1).getPhrases().get(0);
+        } catch (NotConvertableException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testGetSentenceNo() {
+        Assertions.assertEquals(baselinePhrase.getSentenceNo(), phraseImplInstance.getSentenceNo());
+    }
+
+    @Test
+    void testGetText() {
+        Assertions.assertEquals(baselinePhrase.getText(), phraseImplInstance.getText());
+    }
+
+    @Test
+    void testGetPhraseType() {
+        Assertions.assertEquals(baselinePhrase.getPhraseType(), phraseImplInstance.getPhraseType());
+    }
+
+    @Test
+    void testGetContainedWords() {
+        Assertions.assertEquals(baselinePhrase.getContainedWords().size(), phraseImplInstance.getContainedWords().size());
+    }
+
+    @Test
+    void testGetSubPhrases() {
+        Assertions.assertEquals(baselinePhrase.getSubPhrases().size(), phraseImplInstance.getSubPhrases().size());
+    }
+
+    @Test
+    void testIsSuperPhraseOf() {
+        Phrase subphrase = phraseImplInstance.getSubPhrases().get(0);
+        Assertions.assertAll(//
+                () -> Assertions.assertTrue(phraseImplInstance.isSuperPhraseOf(subphrase)), () -> Assertions.assertFalse(phraseImplInstance.isSuperPhraseOf(
+                        phraseImplInstance)), () -> Assertions.assertFalse(subphrase.isSuperPhraseOf(phraseImplInstance))//
+        );
+    }
+
+    @Test
+    void testIsSubPhraseOf() {
+        Phrase subphrase = phraseImplInstance.getSubPhrases().get(0);
+        Assertions.assertAll(//
+                () -> Assertions.assertFalse(phraseImplInstance.isSubPhraseOf(subphrase)), () -> Assertions.assertFalse(phraseImplInstance.isSubPhraseOf(
+                        phraseImplInstance)), () -> Assertions.assertTrue(subphrase.isSubPhraseOf(phraseImplInstance))//
+        );
+    }
+
+}
