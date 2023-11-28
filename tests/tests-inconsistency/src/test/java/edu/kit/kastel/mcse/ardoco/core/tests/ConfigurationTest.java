1e00ae39254348c3f9663553c5cd51f374a1f524
Dominik Fuchß <dominik.fuchss@kit.edu>
2023-11-28 14:12:31 +0100

    Bump versions

--- tests/tests-inconsistency/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/ConfigurationTest.java
+++ tests/tests-inconsistency/src/test/java/edu/kit/kastel/mcse/ardoco/core/tests/ConfigurationTest.java
@@ -37,7 +37,7 @@ class ConfigurationTest extends ConfigurationTestBase {
 
     @Override
     @Test
-    public void testValidityOfConfigurableFields() throws Exception {
+    public void testValidityOfConfigurableFields() {
         super.testValidityOfConfigurableFields();
     }
 }
