489196071d8689869916cc343343485640d839d8
Jan Keim <jan.keim@kit.edu>
2023-12-06 18:16:09 +0100

    Beautify CodeModel file output

--- framework/common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/models/ModelElement.java
+++ framework/common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/models/ModelElement.java
@@ -2,17 +2,24 @@
 package edu.kit.kastel.mcse.ardoco.core.api.models;
 
 import java.util.Objects;
-import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A model element. Has an identifier. Two model elements are equal if and only if they have the same identifier.
  */
 public abstract class ModelElement implements Comparable<ModelElement> {
 
+    private static final AtomicInteger COUNTER = new AtomicInteger(0);
+
     private final String id;
 
     protected ModelElement() {
-        this.id = UUID.randomUUID().toString();
+        this.id = createId();
+    }
+
+    private static String createId() {
+        int currentCounter = COUNTER.getAndIncrement();
+        return "acm%06djsd".formatted(currentCounter);
     }
 
     protected ModelElement(String id) {
