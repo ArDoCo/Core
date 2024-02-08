166b536c8e881f6c9d92401f501654df5d4fdf0d
Dominik Fuchß <dominik.fuchss@kit.edu>
2024-02-08 16:51:25 +0100

    Bugfix: Set outputs of test to target folder (remove from resources) (#342)
    * Set outputs of test to target folder (remove from resources)
    
    * Fix build
    
    * More bugfixes
    
    * different output dirs

--- stages/diagram-consistency/src/test/java/edu/kit/kastel/mcse/ardoco/core/diagramconsistency/evaluation/EvaluationTestBase.java
+++ stages/diagram-consistency/src/test/java/edu/kit/kastel/mcse/ardoco/core/diagramconsistency/evaluation/EvaluationTestBase.java
@@ -1,4 +1,4 @@
-/* Licensed under MIT 2023. */
+/* Licensed under MIT 2023-2024. */
 package edu.kit.kastel.mcse.ardoco.core.diagramconsistency.evaluation;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -51,8 +51,8 @@ public class EvaluationTestBase {
     protected static final double PARTIAL_SELECTION_MAX = 0.25;
     protected static final double REFACTORING_RATIO = 0.25;
 
-    protected static final String PIPELINE_OUTPUT = "src/test/resources/pipeline_out";
-    protected static final String TEST_OUTPUT = "src/test/resources/test_out";
+    protected static final String PIPELINE_OUTPUT = "target/pipeline_out";
+    protected static final String TEST_OUTPUT = "target/test_out";
     protected static final Logger logger = LoggerFactory.getLogger(EvaluationTestBase.class);
 
     protected FileWriter writer;
