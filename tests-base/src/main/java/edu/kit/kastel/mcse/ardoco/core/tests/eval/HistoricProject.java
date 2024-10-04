b15d488c4cde8a3e66b707a3233141f648e6ff89
Dominik Fuchß <dominik.fuchss@kit.edu>
2024-10-04 11:08:28 +0200

    Squashed 'tests/integration-tests/' changes from d6720d3..5e73165
    5e73165 Add RAW Model File Type
    
    git-subtree-dir: tests/integration-tests
    git-subtree-split: 5e73165dbd8a5be835bbe2344493cd90a04e03fc

--- tests-base/src/main/java/edu/kit/kastel/mcse/ardoco/core/tests/eval/HistoricProject.java
+++ tests-base/src/main/java/edu/kit/kastel/mcse/ardoco/core/tests/eval/HistoricProject.java
@@ -111,6 +111,7 @@ public enum HistoricProject implements GoldStandardProject {
         return switch (modelType) {
         case PCM -> getModelFile();
         case UML -> ProjectHelper.loadFileFromResources(model.replace("/pcm/", "/uml/").replace(".repository", ".uml"));
+        case RAW -> throw new IllegalArgumentException("Raw model is not supported for this project.");
         };
     }
 
@@ -119,6 +120,7 @@ public enum HistoricProject implements GoldStandardProject {
         return switch (modelType) {
         case PCM -> model;
         case UML -> model.replace("/pcm/", "/uml/").replace(".repository", ".uml");
+        case RAW -> throw new IllegalArgumentException("Raw model is not supported for this project.");
         };
     }
 
