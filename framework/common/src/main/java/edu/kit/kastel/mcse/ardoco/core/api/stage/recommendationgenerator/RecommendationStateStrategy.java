5faf1fd9e30ca2ad7e7c9153b5eed8c6fa23d225
Dominik Fuchß <dominik.fuchss@kit.edu>
2024-12-06 13:31:25 +0100

    Remove RecommendationStateStrategy, since not used

--- framework/common/src/main/java/edu/kit/kastel/mcse/ardoco/core/api/stage/recommendationgenerator/RecommendationStateStrategy.java
+++
@@ -1,8 +0,0 @@
-/* Licensed under MIT 2024. */
-package edu.kit.kastel.mcse.ardoco.core.api.stage.recommendationgenerator;
-
-public interface RecommendationStateStrategy {
-    boolean areRecommendedInstanceTypesSimilar(String typeA, String typeB);
-
-    boolean areRecommendedInstanceNamesSimilar(String nameA, String nameB);
-}
