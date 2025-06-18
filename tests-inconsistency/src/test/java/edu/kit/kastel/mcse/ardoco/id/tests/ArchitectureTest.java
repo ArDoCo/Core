c22fa75fe826759201d73f6126208b747d49447a
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-06-18 15:26:34 +0200

    Fix architecture tests

--- tests-inconsistency/src/test/java/edu/kit/kastel/mcse/ardoco/id/tests/ArchitectureTest.java
+++ tests-inconsistency/src/test/java/edu/kit/kastel/mcse/ardoco/id/tests/ArchitectureTest.java
@@ -1,8 +1,9 @@
-/* Licensed under MIT 2024. */
+/* Licensed under MIT 2024-2025. */
 package edu.kit.kastel.mcse.ardoco.id.tests;
 
-// TODO Enable
-// @AnalyzeClasses(packages = "edu.kit.kastel.mcse.ardoco")
+import com.tngtech.archunit.junit.AnalyzeClasses;
+
+@AnalyzeClasses(packages = "edu.kit.kastel.mcse.ardoco")
 public class ArchitectureTest extends edu.kit.kastel.mcse.ardoco.core.tests.architecture.ArchitectureTest {
     // Has to be executed in this module
 }
