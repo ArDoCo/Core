b209c3fbf06b230146efd9e300afccfdf7b1e832
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-06-18 18:13:03 +0200

    Add constants to architecture tests

--- tests-base/src/main/java/edu/kit/kastel/mcse/ardoco/core/tests/architecture/ArchitectureTest.java
+++ tests-base/src/main/java/edu/kit/kastel/mcse/ardoco/core/tests/architecture/ArchitectureTest.java
@@ -10,75 +10,106 @@ import com.tngtech.archunit.lang.ArchRule;
 
 @AnalyzeClasses(packages = "edu.kit.kastel.mcse.ardoco")
 public class ArchitectureTest {
+
+    // Package constants
+    private static final String PACKAGE_EXECUTION = "..execution..";
+    private static final String PACKAGE_TESTS = "..tests..";
+    private static final String PACKAGE_MODELS = "..models..";
+    private static final String PACKAGE_RECOMMENDATION_GENERATOR = "..recommendationgenerator..";
+    private static final String PACKAGE_CONNECTION_GENERATOR = "..connectiongenerator..";
+    private static final String PACKAGE_INCONSISTENCY = "..inconsistency..";
+    private static final String PACKAGE_INCONSISTENCY_DETECTION = "..id.."; // InconsistencyDetection
+    private static final String PACKAGE_PIPELINE = "..pipeline..";
+    private static final String PACKAGE_COMMON = "..common..";
+    private static final String PACKAGE_OUTPUT = "..output..";
+    private static final String PACKAGE_CODE_TRACEABILITY = "..codetraceability..";
+    private static final String PACKAGE_TRACE_LINKS = "..tracelinks..";
+    private static final String PACKAGE_API = "..api..";
+    private static final String PACKAGE_TRACE_LINK = "..tracelink..";
+    private static final String PACKAGE_DATA = "..data..";
+    private static final String PACKAGE_TEXT_EXTRACTION = "..textextraction..";
+
+    // Layer name constants
+    private static final String LAYER_COMMON = "Common";
+    private static final String LAYER_TEXT_EXTRACTOR = "TextExtractor";
+    private static final String LAYER_MODEL_EXTRACTOR = "ModelExtractor";
+    private static final String LAYER_RECOMMENDATION_GENERATOR = "RecommendationGenerator";
+    private static final String LAYER_CONNECTION_GENERATOR = "ConnectionGenerator";
+    private static final String LAYER_INCONSISTENCY_DETECTION = "InconsistencyDetection";
+    private static final String LAYER_CODE_TRACEABILITY = "CodeTraceability";
+    private static final String LAYER_PIPELINE = "Pipeline";
+    private static final String LAYER_EXECUTION = "Execution";
+
     @ArchTest
     public static final ArchRule noDependencyOnExecution = classes().that()
-            .resideInAPackage("..execution..")
+            .resideInAPackage(PACKAGE_EXECUTION)
             .should()
             .onlyHaveDependentClassesThat()
-            .resideInAnyPackage("..execution..", "..tests..");
+            .resideInAnyPackage(PACKAGE_EXECUTION, PACKAGE_TESTS);
     @ArchTest
     public static final ArchRule modelInstancesOnlyAfterModelExtraction = classes().that()
             .haveSimpleName("Model")
             .should()
             .onlyHaveDependentClassesThat()
-            .resideInAnyPackage("..models..", "..recommendationgenerator..", "..connectiongenerator..", "..inconsistency..", "..id..", "..pipeline..",
-                    "..common..", "..output..", "..tests..");
+            .resideInAnyPackage(PACKAGE_MODELS, PACKAGE_RECOMMENDATION_GENERATOR, PACKAGE_CONNECTION_GENERATOR, PACKAGE_INCONSISTENCY,
+                    PACKAGE_INCONSISTENCY_DETECTION, PACKAGE_PIPELINE, PACKAGE_COMMON, PACKAGE_OUTPUT, PACKAGE_TESTS);
 
     @ArchTest
     public static final ArchRule linksOnlyAfterConnectionGenerator = classes().that()
             .haveSimpleNameEndingWith("Link")
             .should()
             .onlyHaveDependentClassesThat()
-            .resideInAnyPackage("..connectiongenerator..", "..codetraceability..", "..tracelinks..", "..inconsistency..", "..id..", "..pipeline..",
-                    "..common..", "..api..", "..tests..");
+            .resideInAnyPackage(PACKAGE_CONNECTION_GENERATOR, PACKAGE_CODE_TRACEABILITY, PACKAGE_TRACE_LINKS, PACKAGE_INCONSISTENCY,
+                    PACKAGE_INCONSISTENCY_DETECTION, PACKAGE_PIPELINE, PACKAGE_COMMON, PACKAGE_API, PACKAGE_TESTS);
 
     @ArchTest
     public static final ArchRule usingLinkAsNamingOnlyInConnectionGenerator = classes().that()
             .haveSimpleNameEndingWith("Link")
             .should()
-            .resideInAnyPackage("..tracelink..", "..codetraceability..", "..connectiongenerator..");
+            .resideInAnyPackage(PACKAGE_TRACE_LINK, PACKAGE_CODE_TRACEABILITY, PACKAGE_CONNECTION_GENERATOR);
 
     @ArchTest
     public static final ArchRule inconsistencyOnlyAfterInconsistencyDetection = classes().that()
             .haveSimpleNameContaining("Inconsistency")
             .should()
             .onlyHaveDependentClassesThat()
-            .resideInAnyPackage("..inconsistency..", "..id..", "..execution..", "..api..", "..common..", "..tests..");
+            .resideInAnyPackage(PACKAGE_INCONSISTENCY, PACKAGE_INCONSISTENCY_DETECTION, PACKAGE_EXECUTION, PACKAGE_API, PACKAGE_COMMON, PACKAGE_TESTS);
 
     @ArchTest
     public static final ArchRule layerRule = layeredArchitecture().consideringAllDependencies()
             // Layer definition
-            .layer("Common")
-            .definedBy("..common..", "..data..", "..api..", "..tests..")
-            .layer("TextExtractor")
-            .definedBy("..textextraction..")
-            .layer("ModelExtractor")
-            .definedBy("..models..")
-            .layer("RecommendationGenerator")
-            .definedBy("..recommendationgenerator..")
-            .layer("ConnectionGenerator")
-            .definedBy("..connectiongenerator..")
-            .layer("InconsistencyDetection")
-            .definedBy("..inconsistency..", "..id..")
-            .layer("CodeTraceability")
-            .definedBy("..codetraceability..")
-            .layer("Pipeline")
-            .definedBy("..pipeline..")
-            .layer("Execution")
-            .definedBy("..execution..")
+            .layer(LAYER_COMMON)
+            .definedBy(PACKAGE_COMMON, PACKAGE_DATA, PACKAGE_API, PACKAGE_TESTS)
+            .layer(LAYER_TEXT_EXTRACTOR)
+            .definedBy(PACKAGE_TEXT_EXTRACTION)
+            .layer(LAYER_MODEL_EXTRACTOR)
+            .definedBy(PACKAGE_MODELS)
+            .layer(LAYER_RECOMMENDATION_GENERATOR)
+            .definedBy(PACKAGE_RECOMMENDATION_GENERATOR)
+            .layer(LAYER_CONNECTION_GENERATOR)
+            .definedBy(PACKAGE_CONNECTION_GENERATOR)
+            .layer(LAYER_INCONSISTENCY_DETECTION)
+            .definedBy(PACKAGE_INCONSISTENCY, PACKAGE_INCONSISTENCY_DETECTION)
+            .layer(LAYER_CODE_TRACEABILITY)
+            .definedBy(PACKAGE_CODE_TRACEABILITY)
+            .layer(LAYER_PIPELINE)
+            .definedBy(PACKAGE_PIPELINE)
+            .layer(LAYER_EXECUTION)
+            .definedBy(PACKAGE_EXECUTION)
             // rule definition
-            .whereLayer("Execution")
-            .mayOnlyBeAccessedByLayers("Common") // Needed for tests
-            .whereLayer("InconsistencyDetection")
-            .mayOnlyBeAccessedByLayers("Pipeline", "Common", "Execution")
-            .whereLayer("ConnectionGenerator")
-            .mayOnlyBeAccessedByLayers("CodeTraceability", "InconsistencyDetection", "Pipeline", "Common", "Execution")
-            .whereLayer("RecommendationGenerator")
-            .mayOnlyBeAccessedByLayers("ConnectionGenerator", "InconsistencyDetection", "Pipeline", "Common", "Execution")
-            .whereLayer("TextExtractor")
-            .mayOnlyBeAccessedByLayers("RecommendationGenerator", "ConnectionGenerator", "InconsistencyDetection", "Pipeline", "Common", "Execution")
-            .whereLayer("ModelExtractor")
-            .mayOnlyBeAccessedByLayers("RecommendationGenerator", "ConnectionGenerator", "CodeTraceability", "InconsistencyDetection", "Pipeline", "Common",
-                    "Execution");
+            .whereLayer(LAYER_EXECUTION)
+            .mayOnlyBeAccessedByLayers(LAYER_COMMON) // Needed for tests
+            .whereLayer(LAYER_INCONSISTENCY_DETECTION)
+            .mayOnlyBeAccessedByLayers(LAYER_PIPELINE, LAYER_COMMON, LAYER_EXECUTION)
+            .whereLayer(LAYER_CONNECTION_GENERATOR)
+            .mayOnlyBeAccessedByLayers(LAYER_CODE_TRACEABILITY, LAYER_INCONSISTENCY_DETECTION, LAYER_PIPELINE, LAYER_COMMON, LAYER_EXECUTION)
+            .whereLayer(LAYER_RECOMMENDATION_GENERATOR)
+            .mayOnlyBeAccessedByLayers(LAYER_CONNECTION_GENERATOR, LAYER_INCONSISTENCY_DETECTION, LAYER_PIPELINE, LAYER_COMMON, LAYER_EXECUTION)
+            .whereLayer(LAYER_TEXT_EXTRACTOR)
+            .mayOnlyBeAccessedByLayers(LAYER_RECOMMENDATION_GENERATOR, LAYER_CONNECTION_GENERATOR, LAYER_INCONSISTENCY_DETECTION, LAYER_PIPELINE, LAYER_COMMON,
+                    LAYER_EXECUTION)
+            .whereLayer(LAYER_MODEL_EXTRACTOR)
+            .mayOnlyBeAccessedByLayers(LAYER_RECOMMENDATION_GENERATOR, LAYER_CONNECTION_GENERATOR, LAYER_CODE_TRACEABILITY, LAYER_INCONSISTENCY_DETECTION,
+                    LAYER_PIPELINE, LAYER_COMMON, LAYER_EXECUTION);
 
 }
