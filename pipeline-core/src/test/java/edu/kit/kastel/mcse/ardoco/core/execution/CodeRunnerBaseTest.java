1f7e6943bb50242070be71e5584f3b2383cecbee
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-07-21 15:27:33 +0200

    Remove legacy support to get code configuration

--- pipeline-core/src/test/java/edu/kit/kastel/mcse/ardoco/core/execution/CodeRunnerBaseTest.java
+++ pipeline-core/src/test/java/edu/kit/kastel/mcse/ardoco/core/execution/CodeRunnerBaseTest.java
@@ -1,4 +1,4 @@
-/* Licensed under MIT 2023-2024. */
+/* Licensed under MIT 2023-2025. */
 package edu.kit.kastel.mcse.ardoco.core.execution;
 
 import java.io.File;
@@ -10,33 +10,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.kit.kastel.mcse.ardoco.core.common.RepositoryHandler;
+import edu.kit.kastel.mcse.ardoco.tlr.models.agents.CodeConfiguration;
 
 public class CodeRunnerBaseTest extends RunnerBaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(CodeRunnerBaseTest.class);
     protected static final String inputCodeRepository = "https://github.com/ArDoCo/TeaStore.git";
 
-    protected String inputCodeModel = null;
+    protected CodeConfiguration codeConfiguration = null;
     protected static String commitHash = "bdc49020a55cfa97eaabbb25744fefbc2697defa";
 
     @BeforeEach
     void setupCodeDirectories() throws Exception {
-        if (inputCodeModel != null) {
+        if (codeConfiguration != null) {
             logger.debug("Already initialized");
             return;
         }
 
         var inputCodeModelDirectory = new File(directory.toFile(), "code");
         inputCodeModelDirectory.mkdir();
-        var inputCodeModel = new File(inputCodeModelDirectory, "codeModel.acm");
-        this.getClass().getResourceAsStream("/code/teastore/codeModel.acm").transferTo(Files.newOutputStream(inputCodeModel.toPath()));
-        this.inputCodeModel = inputCodeModelDirectory.getAbsolutePath();
+        var acmFile = new File(inputCodeModelDirectory, "codeModel.acm");
+        this.getClass().getResourceAsStream("/code/teastore/codeModel.acm").transferTo(Files.newOutputStream(acmFile.toPath()));
+        this.codeConfiguration = new CodeConfiguration(acmFile, CodeConfiguration.CodeConfigurationType.ACM_FILE);
 
         if (System.getenv("testCodeFull") != null) {
             var inputCodeModelDirectoryFull = new File(directory.toFile(), "code-full");
             inputCodeModelDirectoryFull.mkdir();
-            this.inputCodeModel = inputCodeModelDirectoryFull.getAbsolutePath();
-            var successfulClone = RepositoryHandler.shallowCloneRepository(inputCodeRepository, this.inputCodeModel, commitHash);
+            this.codeConfiguration = new CodeConfiguration(inputCodeModelDirectoryFull, CodeConfiguration.CodeConfigurationType.DIRECTORY);
+            var successfulClone = RepositoryHandler.shallowCloneRepository(inputCodeRepository, inputCodeModelDirectoryFull.getAbsolutePath(), commitHash);
             if (!successfulClone) {
                 Assertions.fail("Could not clone repository.");
             }
